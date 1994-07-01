414e5b1d06c7fdca1691be1914adac4ed83f34f7
Jim Meyering <jim@meyering.net>
1994-07-01 18:42:52 +0000

    .

--- lib/full-write.c
+++ lib/full-write.c
@@ -1,5 +1,5 @@
 /* full-write.c -- an interface to write that retries after interrupts
-   Copyright (C) 1993 Free Software Foundation, Inc.
+   Copyright (C) 1993, 1994 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
