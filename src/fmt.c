10c7a38d84908e603d971f8a0d030f90760f2894
Jim Meyering <jim@meyering.net>
1994-09-30 00:50:23 +0000

    .

--- src/fmt.c
+++ src/fmt.c
@@ -1,4 +1,4 @@
-* GNU fmt -- simple text formatter.
+/* GNU fmt -- simple text formatter.
    Copyright (C) 1994 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
