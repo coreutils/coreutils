e9bbe5be67443c2989b2a00e3b6d49583a639ab7
Jim Meyering <jim@meyering.net>
1994-10-01 04:37:18 +0000

    merge with 1.10n4

--- lib/putenv.c
+++ lib/putenv.c
@@ -1,76 +1,46 @@
 /* Copyright (C) 1991 Free Software Foundation, Inc.
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+NOTE: The canonical source of this file is maintained with the GNU C Library.
+Bugs can be reported to bug-glibc@prep.ai.mit.edu.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2, or (at your option) any
+later version.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
-#include <sys/types.h>
+#include <ansidecl.h>
 #include <errno.h>
-#ifndef errno
-extern int errno;
-#endif
-
-/* Don't include stdlib.h for non-GNU C libraries because some of them
-   contain conflicting prototypes for getopt.
-   This needs to come after some library #include
-   to get __GNU_LIBRARY__ defined.  */
-#ifdef	__GNU_LIBRARY__
 #include <stdlib.h>
-#else
-char *malloc ();
-#endif	/* GNU C library.  */
-
-#if defined(STDC_HEADERS) || defined(HAVE_STRING_H)
 #include <string.h>
-#else
-#include <strings.h>
-#ifndef strchr
-#define strchr index
-#endif
-#ifndef memcpy
-#define memcpy(d, s, n) bcopy((s), (d), (n))
-#endif
-#endif
-
-#ifdef HAVE_UNISTD_H
 #include <unistd.h>
-#endif
 
-#ifndef NULL
-#define NULL 0
+#ifndef	HAVE_GNU_LD
+#define	__environ	environ
 #endif
-
-extern char **environ;
 
 /* Put STRING, which is of the form "NAME=VALUE", in the environment.  */
 int
-putenv (string)
-     const char *string;
+DEFUN(putenv, (string), CONST char *string)
 {
-  char *name_end = strchr (string, '=');
+  CONST char *CONST name_end = strchr(string, '=');
   register size_t size;
   register char **ep;
 
   if (name_end == NULL)
     {
       /* Remove the variable from the environment.  */
-      size = strlen (string);
-      for (ep = environ; *ep != NULL; ++ep)
-	if (!strncmp (*ep, string, size) && (*ep)[size] == '=')
+      size = strlen(string);
+      for (ep = __environ; *ep != NULL; ++ep)
+	if (!strncmp(*ep, string, size) && (*ep)[size] == '=')
 	  {
 	    while (ep[1] != NULL)
 	      {
@@ -83,8 +53,8 @@ putenv (string)
     }
 
   size = 0;
-  for (ep = environ; *ep != NULL; ++ep)
-    if (!strncmp (*ep, string, name_end - string) &&
+  for (ep = __environ; *ep != NULL; ++ep)
+    if (!strncmp(*ep, string, name_end - string) &&
 	(*ep)[name_end - string] == '=')
       break;
     else
@@ -93,16 +63,16 @@ putenv (string)
   if (*ep == NULL)
     {
       static char **last_environ = NULL;
-      char **new_environ = (char **) malloc ((size + 2) * sizeof (char *));
+      char **new_environ = (char **) malloc((size + 2) * sizeof(char *));
       if (new_environ == NULL)
 	return -1;
-      memcpy ((char *) new_environ, (char *) environ, size * sizeof (char *));
+      (void) memcpy((PTR) new_environ, (PTR) __environ, size * sizeof(char *));
       new_environ[size] = (char *) string;
       new_environ[size + 1] = NULL;
       if (last_environ != NULL)
-	free ((char *) last_environ);
+	free((PTR) last_environ);
       last_environ = new_environ;
-      environ = new_environ;
+      __environ = new_environ;
     }
   else
     *ep = (char *) string;
--- lib/strcspn.c
+++ lib/strcspn.c
@@ -1,43 +1,36 @@
 /* Copyright (C) 1991 Free Software Foundation, Inc.
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+NOTE: The canonical source of this file is maintained with the GNU C Library.
+Bugs can be reported to bug-glibc@prep.ai.mit.edu.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2, or (at your option) any
+later version.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
-#ifdef HAVE_STRING_H
+#include <ansidecl.h>
 #include <string.h>
-#else
-#include <strings.h>
-#ifndef strchr
-#define strchr index
-#endif
-#endif
+
 
 /* Return the length of the maximum inital segment of S
    which contains no characters from REJECT.  */
-int
-strcspn (s, reject)
-     register char *s;
-     register char *reject;
+size_t
+DEFUN(strcspn, (s, reject),
+      register CONST char *s AND register CONST char *reject)
 {
-  register int count = 0;
+  register size_t count = 0;
 
   while (*s != '\0')
-    if (strchr (reject, *s++) == 0)
+    if (strchr(reject, *s++) == NULL)
       ++count;
     else
       return count;
