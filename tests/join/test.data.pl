da098ddd8a46ff7655710f8e49c9f97685af2f39
Jim Meyering <jim@meyering.net>
1994-11-07 12:55:05 +0000

    .

--- tests/join/test.data.pl
+++ tests/join/test.data.pl
@@ -1,4 +1,4 @@
 # test name
 #     flags       file-1 file-2    expected output   expected return code
 #
-("1", '-a1 -a2',  "a 1\n", "\n",   "a 1\n",              0);
+("1", '-a1 -a2',  "a 1\n", "b\n",   "a 1\nb\n",              0);
