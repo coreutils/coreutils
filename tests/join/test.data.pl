75d902ef016c2966129ceb1f53165e46e11d1063
Jim Meyering <jim@meyering.net>
1994-11-08 22:40:52 +0000

    .

--- tests/join/test.data.pl
+++ tests/join/test.data.pl
@@ -1,4 +1,11 @@
+# TODO: make sure all test_names are unique!!!!
 # test name
 #     flags       file-1 file-2    expected output   expected return code
 #
-("1", '-a1 -a2',  "a 1\n", "b\n",   "a 1\nb\n",              0);
+('1a', '-a1',      "a 1\n", "b\n",   "a 1\n",          0);
+('1b', '-a2',      "a 1\n", "b\n",   "b\n",            0); # Got "\n"
+('1c', '-a1 -a2',  "a 1\n", "b\n",   "a 1\nb\n",       0); # Got "a 1\n\n"
+
+('2a', '-a1 -e .',  "a\nb\nc\n", "a x y\nb\nc\n", "a x y\nb\nc\n", 0);
+('2b', '-a1 -e . -o 2.1,2.2,2.3',  "a\nb\nc\n", "a x y\nb\nc\n", "a x y\nb . .\nc . .\n", 0);
+('2c', '-a1 -e . -o 2.1,2.2,2.3',  "a\nb\nc\nd\n", "a x y\nb\nc\n", "a x y\nb . .\nc . .\nd\n", 0);
