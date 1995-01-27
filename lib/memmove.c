19765460626c97d68cf9f7399891c453d91249b0
Jim Meyering <jim@meyering.net>
1995-01-27 19:32:16 +0000

    .

---
+++ lib/memmove.c
@@ -0,0 +1,19 @@
+/* memmove.c -- copy memory.
+   Copy LENGTH bytes from SOURCE to DEST.  Does not null-terminate.
+   In the public domain.
+   By David MacKenzie <djm@gnu.ai.mit.edu>.  */
+
+void
+memmove (dest, source, length)
+     char *dest, *source;
+     unsigned length;
+{
+  if (source < dest)
+    /* Moving from low mem to hi mem; start at end.  */
+    for (source += length, dest += length; length; --length)
+      *--dest = *--source;
+  else if (source != dest)
+    /* Moving from hi mem to low mem; start at beginning.  */
+    for (; length; --length)
+      *dest++ = *source++;
+}
