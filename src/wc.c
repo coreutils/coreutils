40f6e920679bdadd5f391d8043347489d89993d1
Jim Meyering <jim@meyering.net>
1995-02-28 12:50:35 +0000

    (wc): oops.  Add missing dcl of stats.

--- src/wc.c
+++ src/wc.c
@@ -217,6 +217,7 @@ wc (fd, file)
   if (print_chars && !print_words && !print_lines)
     {
       off_t current_pos, end_pos;
+      struct stat stats;
 
       if (fstat (fd, &stats) == 0 && S_ISREG (stats.st_mode)
 	  && (current_pos = lseek (fd, (off_t) 0, SEEK_CUR)) != -1
