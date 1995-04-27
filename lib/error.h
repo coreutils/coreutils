843193374f8ef180c6512874584390f95feb0468
Jim Meyering <jim@meyering.net>
1995-04-27 17:04:23 +0000

    Use __-protected versions of `format' and `printf' only with gcc-2.7 and later.

--- lib/error.h
+++ lib/error.h
@@ -19,8 +19,15 @@
 #define _error_h_
 
 #ifndef __attribute__
+/* This feature is available in gcc versions 2.5 and later.  */
 # if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 5) || __STRICT_ANSI__
 #  define __attribute__(Spec) /* empty */
+# endif
+/* The __-protected variants of `format' and `printf' attributes
+   are accepted by gcc versions 2.6.4 (effectively 2.7) and later.  */
+# if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 7)
+#  define __format__ format
+#  define __printf__ printf
 # endif
 #endif
 
