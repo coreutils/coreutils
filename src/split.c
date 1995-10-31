11a013bf870e1e56ecd8c05d76aa6ff306b6fcab
Jim Meyering <jim@meyering.net>
1995-10-31 12:34:48 +0000

    Reorder functions to obviate forward dcls. Remove forward dcls.

--- src/split.c
+++ src/split.c
@@ -34,15 +34,6 @@ char *xmalloc ();
 int full_write ();
 int safe_read ();
 
-static int convint ();
-static int isdigits ();
-static int stdread ();
-static void line_bytes_split ();
-static void bytes_split ();
-static void cwrite ();
-static void lines_split ();
-static void next_file_name ();
-
 /* The name this program was run with. */
 char *program_name;
 
@@ -115,6 +106,297 @@ SIZE may have a multiplier suffix: b for 512, k for 1K, m for 1 Meg.\n\
   exit (status);
 }
 
+/* Return nonzero if the string STR is composed entirely of decimal digits.  */
+
+static int
+isdigits (str)
+     char *str;
+{
+  do
+    {
+      if (!ISDIGIT (*str))
+	return 0;
+      str++;
+    }
+  while (*str);
+  return 1;
+}
+
+/* Put the value of the number in STR into *VAL.
+   STR can specify a positive integer, optionally ending in `k'
+   to mean kilo or `m' to mean mega.
+   Return 0 if STR is valid, -1 if not. */
+
+static int
+convint (str, val)
+     char *str;
+     int *val;
+{
+  int multiplier = 1;
+  int arglen = strlen (str);
+
+  if (arglen > 1)
+    {
+      switch (str[arglen - 1])
+	{
+	case 'b':
+	  multiplier = 512;
+	  str[arglen - 1] = '\0';
+	  break;
+	case 'k':
+	  multiplier = 1024;
+	  str[arglen - 1] = '\0';
+	  break;
+	case 'm':
+	  multiplier = 1048576;
+	  str[arglen - 1] = '\0';
+	  break;
+	}
+    }
+  if (!isdigits (str))
+    return -1;
+  *val = atoi (str) * multiplier;
+  return 0;
+}
+
+/* Compute the next sequential output file name suffix and store it
+   into the string `outfile' at the position pointed to by `outfile_mid'.  */
+
+static void
+next_file_name ()
+{
+  int x;
+  char *ne;
+  unsigned int i;
+
+  static int first_call = 1;
+
+  /* Status for outfile name generation.  */
+  static unsigned outfile_count = 0;
+  static unsigned outfile_name_limit = 25 * 26;
+  static unsigned outfile_name_generation = 1;
+
+  if (!first_call)
+    outfile_count++;
+  first_call = 0;
+  if (outfile_count < outfile_name_limit)
+    {
+      for (ne = outfile_end - 1; ; ne--)
+	{
+	  x = *ne;
+	  if (x != 'z')
+	    break;
+	  *ne = 'a';
+	}
+      *ne = x + 1;
+      return;
+    }
+
+  outfile_count = 0;
+  outfile_name_limit *= 26;
+  outfile_name_generation++;
+  *outfile_mid++ = 'z';
+  for (i = 0; i <= outfile_name_generation; i++)
+    outfile_mid[i] = 'a';
+  outfile_end += 2;
+}
+
+/* Write BYTES bytes at BP to an output file.
+   If NEW_FILE_FLAG is nonzero, open the next output file.
+   Otherwise add to the same output file already in use.  */
+
+static void
+cwrite (new_file_flag, bp, bytes)
+     int new_file_flag;
+     char *bp;
+     int bytes;
+{
+  if (new_file_flag)
+    {
+      if (output_desc >= 0 && close (output_desc) < 0)
+	error (1, errno, "%s", outfile);
+
+      next_file_name ();
+      output_desc = open (outfile, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+      if (output_desc < 0)
+	error (1, errno, "%s", outfile);
+    }
+  if (full_write (output_desc, bp, bytes) < 0)
+    error (1, errno, "%s", outfile);
+}
+
+/* Read NCHARS bytes from the input file into BUF.
+   Return the number of bytes successfully read.
+   If this is less than NCHARS, do not call `stdread' again.  */
+
+static int
+stdread (buf, nchars)
+     char *buf;
+     int nchars;
+{
+  int n_read;
+  int to_be_read = nchars;
+
+  while (to_be_read)
+    {
+      n_read = safe_read (input_desc, buf, to_be_read);
+      if (n_read < 0)
+	return -1;
+      if (n_read == 0)
+	break;
+      to_be_read -= n_read;
+      buf += n_read;
+    }
+  return nchars - to_be_read;
+}
+
+/* Split into pieces of exactly NCHARS bytes.
+   Use buffer BUF, whose size is BUFSIZE.  */
+
+static void
+bytes_split (nchars, buf, bufsize)
+     int nchars;
+     char *buf;
+     int bufsize;
+{
+  int n_read;
+  int new_file_flag = 1;
+  int to_read;
+  int to_write = nchars;
+  char *bp_out;
+
+  do
+    {
+      n_read = stdread (buf, bufsize);
+      if (n_read < 0)
+        error (1, errno, "%s", infile);
+      bp_out = buf;
+      to_read = n_read;
+      for (;;)
+	{
+	  if (to_read < to_write)
+	    {
+	      if (to_read)	/* do not write 0 bytes! */
+		{
+		  cwrite (new_file_flag, bp_out, to_read);
+		  to_write -= to_read;
+		  new_file_flag = 0;
+		}
+	      break;
+	    }
+	  else
+	    {
+	      cwrite (new_file_flag, bp_out, to_write);
+	      bp_out += to_write;
+	      to_read -= to_write;
+	      new_file_flag = 1;
+	      to_write = nchars;
+	    }
+	}
+    }
+  while (n_read == bufsize);
+}
+
+/* Split into pieces of exactly NLINES lines.
+   Use buffer BUF, whose size is BUFSIZE.  */
+
+static void
+lines_split (nlines, buf, bufsize)
+     int nlines;
+     char *buf;
+     int bufsize;
+{
+  int n_read;
+  char *bp, *bp_out, *eob;
+  int new_file_flag = 1;
+  int n = 0;
+
+  do
+    {
+      n_read = stdread (buf, bufsize);
+      if (n_read < 0)
+	error (1, errno, "%s", infile);
+      bp = bp_out = buf;
+      eob = bp + n_read;
+      *eob = '\n';
+      for (;;)
+	{
+	  while (*bp++ != '\n')
+	    ;			/* this semicolon takes most of the time */
+	  if (bp > eob)
+	    {
+	      if (eob != bp_out) /* do not write 0 bytes! */
+		{
+		  cwrite (new_file_flag, bp_out, eob - bp_out);
+		  new_file_flag = 0;
+		}
+	      break;
+	    }
+	  else
+	    if (++n >= nlines)
+	      {
+		cwrite (new_file_flag, bp_out, bp - bp_out);
+		bp_out = bp;
+		new_file_flag = 1;
+		n = 0;
+	      }
+	}
+    }
+  while (n_read == bufsize);
+}
+
+/* Split into pieces that are as large as possible while still not more
+   than NCHARS bytes, and are split on line boundaries except
+   where lines longer than NCHARS bytes occur. */
+
+static void
+line_bytes_split (nchars)
+     int nchars;
+{
+  int n_read;
+  char *bp;
+  int eof = 0;
+  int n_buffered = 0;
+  char *buf = (char *) xmalloc (nchars);
+
+  do
+    {
+      /* Fill up the full buffer size from the input file.  */
+
+      n_read = stdread (buf + n_buffered, nchars - n_buffered);
+      if (n_read < 0)
+	error (1, errno, "%s", infile);
+
+      n_buffered += n_read;
+      if (n_buffered != nchars)
+	eof = 1;
+
+      /* Find where to end this chunk.  */
+      bp = buf + n_buffered;
+      if (n_buffered == nchars)
+	{
+	  while (bp > buf && bp[-1] != '\n')
+	    bp--;
+	}
+
+      /* If chunk has no newlines, use all the chunk.  */
+      if (bp == buf)
+	bp = buf + n_buffered;
+
+      /* Output the chars as one output file.  */
+      cwrite (1, buf, bp - buf);
+
+      /* Discard the chars we just output; move rest of chunk
+	 down to be the start of the next chunk.  Source and
+	 destination probably overlap.  */
+      n_buffered -= bp - buf;
+      if (n_buffered > 0)
+	memmove (buf, bp, n_buffered);
+    }
+  while (!eof);
+  free (buf);
+}
+
 void
 main (argc, argv)
      int argc;
@@ -158,6 +440,7 @@ main (argc, argv)
 	  if (split_type != type_undef)
 	    usage (2, _("cannot split in more than one way"));
 	  split_type = type_bytes;
+	  /* FIXME: use xstrtoul */
 	  if (convint (optarg, &accum) == -1)
 	    usage (2, _("invalid number of bytes"));
 	  break;
@@ -168,6 +451,7 @@ main (argc, argv)
 	  split_type = type_lines;
 	  if (!isdigits (optarg))
 	    usage (2, _("invalid number of lines"));
+	  /* FIXME: use xstrtoul */
 	  accum = atoi (optarg);
 	  break;
 
@@ -175,6 +459,7 @@ main (argc, argv)
 	  if (split_type != type_undef)
 	    usage (2, _("cannot split in more than one way"));
 	  split_type = type_byteslines;
+	  /* FIXME: use xstrtoul */
 	  if (convint (optarg, &accum) == -1)
 	    usage (2, _("invalid number of bytes"));
 	  break;
@@ -291,295 +576,4 @@ main (argc, argv)
     error (1, errno, "%s", outfile);
 
   exit (0);
-}
-
-/* Return nonzero if the string STR is composed entirely of decimal digits.  */
-
-static int
-isdigits (str)
-     char *str;
-{
-  do
-    {
-      if (!ISDIGIT (*str))
-	return 0;
-      str++;
-    }
-  while (*str);
-  return 1;
-}
-
-/* Put the value of the number in STR into *VAL.
-   STR can specify a positive integer, optionally ending in `k'
-   to mean kilo or `m' to mean mega.
-   Return 0 if STR is valid, -1 if not. */
-
-static int
-convint (str, val)
-     char *str;
-     int *val;
-{
-  int multiplier = 1;
-  int arglen = strlen (str);
-
-  if (arglen > 1)
-    {
-      switch (str[arglen - 1])
-	{
-	case 'b':
-	  multiplier = 512;
-	  str[arglen - 1] = '\0';
-	  break;
-	case 'k':
-	  multiplier = 1024;
-	  str[arglen - 1] = '\0';
-	  break;
-	case 'm':
-	  multiplier = 1048576;
-	  str[arglen - 1] = '\0';
-	  break;
-	}
-    }
-  if (!isdigits (str))
-    return -1;
-  *val = atoi (str) * multiplier;
-  return 0;
-}
-
-/* Split into pieces of exactly NCHARS bytes.
-   Use buffer BUF, whose size is BUFSIZE.  */
-
-static void
-bytes_split (nchars, buf, bufsize)
-     int nchars;
-     char *buf;
-     int bufsize;
-{
-  int n_read;
-  int new_file_flag = 1;
-  int to_read;
-  int to_write = nchars;
-  char *bp_out;
-
-  do
-    {
-      n_read = stdread (buf, bufsize);
-      if (n_read < 0)
-        error (1, errno, "%s", infile);
-      bp_out = buf;
-      to_read = n_read;
-      for (;;)
-	{
-	  if (to_read < to_write)
-	    {
-	      if (to_read)	/* do not write 0 bytes! */
-		{
-		  cwrite (new_file_flag, bp_out, to_read);
-		  to_write -= to_read;
-		  new_file_flag = 0;
-		}
-	      break;
-	    }
-	  else
-	    {
-	      cwrite (new_file_flag, bp_out, to_write);
-	      bp_out += to_write;
-	      to_read -= to_write;
-	      new_file_flag = 1;
-	      to_write = nchars;
-	    }
-	}
-    }
-  while (n_read == bufsize);
-}
-
-/* Split into pieces of exactly NLINES lines.
-   Use buffer BUF, whose size is BUFSIZE.  */
-
-static void
-lines_split (nlines, buf, bufsize)
-     int nlines;
-     char *buf;
-     int bufsize;
-{
-  int n_read;
-  char *bp, *bp_out, *eob;
-  int new_file_flag = 1;
-  int n = 0;
-
-  do
-    {
-      n_read = stdread (buf, bufsize);
-      if (n_read < 0)
-	error (1, errno, "%s", infile);
-      bp = bp_out = buf;
-      eob = bp + n_read;
-      *eob = '\n';
-      for (;;)
-	{
-	  while (*bp++ != '\n')
-	    ;			/* this semicolon takes most of the time */
-	  if (bp > eob)
-	    {
-	      if (eob != bp_out) /* do not write 0 bytes! */
-		{
-		  cwrite (new_file_flag, bp_out, eob - bp_out);
-		  new_file_flag = 0;
-		}
-	      break;
-	    }
-	  else
-	    if (++n >= nlines)
-	      {
-		cwrite (new_file_flag, bp_out, bp - bp_out);
-		bp_out = bp;
-		new_file_flag = 1;
-		n = 0;
-	      }
-	}
-    }
-  while (n_read == bufsize);
-}
-
-/* Split into pieces that are as large as possible while still not more
-   than NCHARS bytes, and are split on line boundaries except
-   where lines longer than NCHARS bytes occur. */
-
-static void
-line_bytes_split (nchars)
-     int nchars;
-{
-  int n_read;
-  char *bp;
-  int eof = 0;
-  int n_buffered = 0;
-  char *buf = (char *) xmalloc (nchars);
-
-  do
-    {
-      /* Fill up the full buffer size from the input file.  */
-
-      n_read = stdread (buf + n_buffered, nchars - n_buffered);
-      if (n_read < 0)
-	error (1, errno, "%s", infile);
-
-      n_buffered += n_read;
-      if (n_buffered != nchars)
-	eof = 1;
-
-      /* Find where to end this chunk.  */
-      bp = buf + n_buffered;
-      if (n_buffered == nchars)
-	{
-	  while (bp > buf && bp[-1] != '\n')
-	    bp--;
-	}
-
-      /* If chunk has no newlines, use all the chunk.  */
-      if (bp == buf)
-	bp = buf + n_buffered;
-
-      /* Output the chars as one output file.  */
-      cwrite (1, buf, bp - buf);
-
-      /* Discard the chars we just output; move rest of chunk
-	 down to be the start of the next chunk.  Source and
-	 destination probably overlap.  */
-      n_buffered -= bp - buf;
-      if (n_buffered > 0)
-	memmove (buf, bp, n_buffered);
-    }
-  while (!eof);
-  free (buf);
-}
-
-/* Write BYTES bytes at BP to an output file.
-   If NEW_FILE_FLAG is nonzero, open the next output file.
-   Otherwise add to the same output file already in use.  */
-
-static void
-cwrite (new_file_flag, bp, bytes)
-     int new_file_flag;
-     char *bp;
-     int bytes;
-{
-  if (new_file_flag)
-    {
-      if (output_desc >= 0 && close (output_desc) < 0)
-	error (1, errno, "%s", outfile);
-
-      next_file_name ();
-      output_desc = open (outfile, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-      if (output_desc < 0)
-	error (1, errno, "%s", outfile);
-    }
-  if (full_write (output_desc, bp, bytes) < 0)
-    error (1, errno, "%s", outfile);
-}
-
-/* Read NCHARS bytes from the input file into BUF.
-   Return the number of bytes successfully read.
-   If this is less than NCHARS, do not call `stdread' again.  */
-
-static int
-stdread (buf, nchars)
-     char *buf;
-     int nchars;
-{
-  int n_read;
-  int to_be_read = nchars;
-
-  while (to_be_read)
-    {
-      n_read = safe_read (input_desc, buf, to_be_read);
-      if (n_read < 0)
-	return -1;
-      if (n_read == 0)
-	break;
-      to_be_read -= n_read;
-      buf += n_read;
-    }
-  return nchars - to_be_read;
-}
-
-/* Compute the next sequential output file name suffix and store it
-   into the string `outfile' at the position pointed to by `outfile_mid'.  */
-
-static void
-next_file_name ()
-{
-  int x;
-  char *ne;
-  unsigned int i;
-
-  static int first_call = 1;
-
-  /* Status for outfile name generation.  */
-  static unsigned outfile_count = 0;
-  static unsigned outfile_name_limit = 25 * 26;
-  static unsigned outfile_name_generation = 1;
-
-  if (!first_call)
-    outfile_count++;
-  first_call = 0;
-  if (outfile_count < outfile_name_limit)
-    {
-      for (ne = outfile_end - 1; ; ne--)
-	{
-	  x = *ne;
-	  if (x != 'z')
-	    break;
-	  *ne = 'a';
-	}
-      *ne = x + 1;
-      return;
-    }
-
-  outfile_count = 0;
-  outfile_name_limit *= 26;
-  outfile_name_generation++;
-  *outfile_mid++ = 'z';
-  for (i = 0; i <= outfile_name_generation; i++)
-    outfile_mid[i] = 'a';
-  outfile_end += 2;
 }
