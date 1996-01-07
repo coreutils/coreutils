00b953713e7689062727be6b17b34fc1436936f7
Jim Meyering <jim@meyering.net>
1996-01-07 04:04:48 +0000

    Fix typo in __P use.

--- src/date.c
+++ src/date.c
@@ -46,7 +46,7 @@ time_t get_date ();
 time_t posixtime ();
 
 static void show_date __P ((const char *format, time_t when));
-static void usage __P (int status));
+static void usage __P ((int status));
 
 /* The name this program was run with, for error messages. */
 char *program_name;
