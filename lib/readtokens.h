a5c268a9f954de1772ccfb3f8ab4523e6726e134
Jim Meyering <jim@meyering.net>
1996-02-01 06:13:54 +0000

    __P-protect dcls.

--- lib/readtokens.h
+++ lib/readtokens.h
@@ -16,14 +16,21 @@ typedef struct tokenbuffer token_buffer;
 
 #endif /* not TOKENBUFFER_DEFINED */
 
-void init_tokenbuffer (token_buffer *tokenbuffer);
+#undef __P
+#if defined (__STDC__) && __STDC__
+#define	__P(x) x
+#else
+#define	__P(x) ()
+#endif
+
+void init_tokenbuffer __P ((token_buffer *tokenbuffer));
 
 long
-  readtoken (FILE *stream, const char *delim, int n_delim,
-	     token_buffer *tokenbuffer);
+  readtoken __P ((FILE *stream, const char *delim, int n_delim,
+	     token_buffer *tokenbuffer));
 int
-  readtokens (FILE *stream, int projected_n_tokens,
+  readtokens __P ((FILE *stream, int projected_n_tokens,
 	      const char *delim, int n_delim,
-	      char ***tokens_out, long **token_lengths);
+	      char ***tokens_out, long **token_lengths));
 
 #endif /* not H_READTOKENS_H */
