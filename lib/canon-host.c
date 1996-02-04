2d882a72007b918bd51076d72424fb9cd3b78c59
Jim Meyering <jim@meyering.net>
1996-02-04 12:47:33 +0000

    .

---
+++ lib/canon-host.c
@@ -0,0 +1,79 @@
+/* Host name canonicalization
+
+   Copyright (C) 1995 Free Software Foundation, Inc.
+
+   Written by Miles Bader <miles@gnu.ai.mit.edu>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif
+#ifdef HAVE_NETDB_H
+#include <netdb.h>
+#endif
+
+#ifdef HAVE_NETINET_IN_H
+#include <netinet/in.h>
+#endif
+#ifdef HAVE_ARPA_INET_H
+#include <arpa/inet.h>
+#endif
+
+/* Returns the canonical hostname associated with HOST (allocated in a static
+   buffer), or 0 if it can't be determined.  */
+char *
+canon_host (host)
+     char *host;
+{
+#ifdef HAVE_GETHOSTBYNAME
+  struct hostent *he = gethostbyname (host);
+
+  if (he)
+    {
+#ifdef HAVE_GETHOSTBYADDR
+      char *addr = 0;
+
+      /* Try and get an ascii version of the numeric host address.  */
+      switch (he->h_addrtype)
+	{
+#ifdef HAVE_INET_NTOA
+	case AF_INET:
+	  addr = inet_ntoa (*(struct in_addr *)he->h_addr);
+	  break;
+#endif /* HAVE_INET_NTOA */
+	}
+
+      if (addr && strcmp (he->h_name, addr) == 0)
+	/* gethostbyname() cheated!  Lookup the host name via the address
+	   this time to get the actual host name.  */
+	he = gethostbyaddr (he->h_addr, he->h_length, he->h_addrtype);
+#endif HAVE_GETHOSTBYADDR
+
+      if (he)
+	return he->h_name;
+    }
+
+#else /* ! HAVE_GETHOSTBYNAME */
+  return 0;
+#endif /* HAVE_GETHOSTBYNAME */
+}
