b28595b5022c48a0b4f8e8e7ccfb5f8563ea001b
Jim Meyering <jim@meyering.net>
1996-03-24 18:20:25 +0000

    .

--- lib/xstrtod.c
+++ lib/xstrtod.c
@@ -1,3 +1,22 @@
+/* xstrtod.c - error-checking interface to strtod
+   Copyright (C) 1996 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+/* Written by Jim Meyering.  */
+
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
@@ -11,13 +30,12 @@ double strtod ();
 #include <errno.h>
 #include <stdio.h>
 #include <limits.h>
-#include <ctype.h>
 #include "xstrtod.h"
 
 /* An interface to strtod that encapsulates all the error checking
-   one should usually perform.  Like strtod, but return zero upon
-   successful conversion and put the result in *RESULT.  Return
-   non-zero upon any failure.  */
+   one should usually perform.  Like strtod, but upon successful
+   conversion put the result in *RESULT and return zero.  Return
+   non-zero and don't modify *RESULT upon any failure.  */
 
 int
 xstrtod (str, ptr, result)
