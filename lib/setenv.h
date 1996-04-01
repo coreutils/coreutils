651e543fd678c550054c0f93a0e5a9d5495a70dc
Jim Meyering <jim@meyering.net>
1996-04-01 00:16:11 +0000

    .

---
+++ lib/setenv.h
@@ -0,0 +1,17 @@
+#ifndef SETENV_H
+#define SETENV_H 1
+
+#undef __P
+#if defined (__STDC__) && __STDC__
+#define	__P(x) x
+#else
+#define	__P(x) ()
+#endif
+
+int
+  setenv __P ((const char *name, const char *value, int replace));
+
+void
+  unsetenv __P ((const char *name));
+
+#endif /* SETENV_H */
