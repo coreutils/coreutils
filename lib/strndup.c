16c5d19ce527505012489be47182e0816c34e0fb
Jim Meyering <jim@meyering.net>
1996-06-25 05:06:53 +0000

    (strndup): Deansify. [!STDC_HEADERS]: Include string.h and stdlib.h.

--- lib/strndup.c
+++ lib/strndup.c
@@ -1,31 +1,40 @@
 /* Copyright (C) 1996 Free Software Foundation, Inc.
 
-The GNU C Library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Library General Public License as
-published by the Free Software Foundation; either version 2 of the
-License, or (at your option) any later version.
+NOTE: The canonical source of this file is maintained with the GNU C Library.
+Bugs can be reported to bug-glibc@prep.ai.mit.edu.
 
-The GNU C Library is distributed in the hope that it will be useful,
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Library General Public License for more details.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-You should have received a copy of the GNU Library General Public
-License along with the GNU C Library; see the file COPYING.LIB.  If
-not, write to the Free Software Foundation, Inc., 675 Mass Ave,
-Cambridge, MA 02139, USA.  */
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
-#include <config.h>
-#include <ansidecl.h>
-#include "xstring.h"
-#include "xmalloc.h"
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
 
+#ifdef STDC_HEADERS
+# include <string.h>
+# include <stdlib.h>
+#else
+char *malloc ();
+#endif
 
 /* Duplicate S, returning an identical malloc'd string.  */
 char *
-DEFUN(strndup, (s, n), const char *s AND size_t n)
+strndup (s, n)
+     const char *s;
+     size_t n;
 {
-  char *new = malloc(n + 1);
+  char *new = malloc (n + 1);
 
   if (new == NULL)
     return NULL;
