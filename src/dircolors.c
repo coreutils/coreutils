b467d6d51338a84cbfc7ad36f11ea045fd9c116b
Jim Meyering <jim@meyering.net>
1996-07-03 22:59:18 +0000

    Include sys/types.h before system.h. From Jim Blandy (jimb@cyclic.com).

--- src/dircolors.c
+++ src/dircolors.c
@@ -20,6 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 # include <config.h>
 #endif
 
+#include <sys/types.h>
 #include <ctype.h>
 #include <getopt.h>
 #include <stdio.h>
