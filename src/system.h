7b2cea5ab25eaa08c4172d2a792717857c289743
Jim Meyering <jim@meyering.net>
1996-07-03 04:00:36 +0000

    [!EXIT_SUCCESS]: Define it. [!EXIT_FAILURE]: Define it.

--- src/system.h
+++ src/system.h
@@ -179,6 +179,18 @@ extern int errno;
 char *getenv ();
 #endif /* STDC_HEADERS */
 
+/* The following test is to work around the gross typo in
+   systems like Sony NEWS-OS Release 4.0C, whereby EXIT_FAILURE
+   is defined to 0, not 1.  */
+#if !EXIT_FAILURE
+# undef EXIT_FAILURE
+# define EXIT_FAILURE 1
+#endif
+
+#ifndef EXIT_SUCCESS
+# define EXIT_SUCCESS 0
+#endif
+
 #ifdef HAVE_FCNTL_H
 #include <fcntl.h>
 #else
