23684f8383437667dfffbf6f56a72ce7672e30e9
Jim Meyering <jim@meyering.net>
1996-07-14 19:09:41 +0000

    formatting...

--- lib/makepath.h
+++ lib/makepath.h
@@ -1,15 +1,15 @@
 #if __STDC__
-#undef __P
-#define __P(args) args
+# undef __P
+# define __P(args) args
 #else
-#define __P(args) ()
+# define __P(args) ()
 #endif
 
 int
-  make_path __P ((const char *_argpath,
-		  int _mode,
-		  int _parent_mode,
-		  uid_t _owner,
-		  gid_t _group,
-		  int _preserve_existing,
-		  const char *_verbose_fmt_string));
+make_path __P ((const char *_argpath,
+		int _mode,
+		int _parent_mode,
+		uid_t _owner,
+		gid_t _group,
+		int _preserve_existing,
+		const char *_verbose_fmt_string));
