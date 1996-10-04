3400ffe3f22fa55d22be79288075a3de2238a6d1
Jim Meyering <jim@meyering.net>
1996-10-04 13:42:20 +0000

    Add assertions.

--- lib/memcpy.c
+++ lib/memcpy.c
@@ -2,6 +2,15 @@
    if the source overlaps with the destination.
    Return DESTADDR. */
 
+#include <assert.h>
+
+/* FIXME: uncomment the following for releases.  */
+#define NDEBUG 1
+
+#ifndef ABS
+# define ABS(x) ((x) < 0 ? (-(x)) : (x))
+#endif
+
 char *
 memcpy (destaddr, srcaddr, len)
      char *destaddr;
@@ -9,6 +18,10 @@ memcpy (destaddr, srcaddr, len)
      int len;
 {
   char *dest = destaddr;
+
+  assert (len >= 0);
+  /* Make sure they don't overlap.  */
+  assert (ABS (destaddr - srcaddr) >= len);
 
   while (len-- > 0)
     *destaddr++ = *srcaddr++;
