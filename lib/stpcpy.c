1eb3a8c9bad66510d0c927112ff6e8eb9d54ddbe
Jim Meyering <jim@meyering.net>
1996-11-04 18:07:38 +0000

    libitize

--- lib/stpcpy.c
+++ lib/stpcpy.c
@@ -15,8 +15,8 @@
     along with this program; if not, write to the Free Software Foundation,
     Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
+#if HAVE_CONFIG_H
+# include <config.h>
 #endif
 
 /* Copy SRC to DEST, returning the address of the terminating '\0' in DEST.  */
