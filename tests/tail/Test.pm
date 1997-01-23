b22469537b8b46b1de3186c9c1716afb832c2bab
Jim Meyering <jim@meyering.net>
1997-01-23 03:55:50 +0000

    .

--- tests/tail/Test.pm
+++ tests/tail/Test.pm
@@ -13,6 +13,7 @@ my @tv = (
 ['obs-c4', '-9c', 'abcd', 'abcd', 0],
 ['obs-c5', '-12c', 'x' . ('y' x 12) . 'z', ('y' x 11) . 'z', 0],
 
+
 ['obs-l1', '-1l', 'x', 'x', 0],
 ['obs-l2', '-1l', "x\ny\n", "y\n", 0],
 ['obs-l3', '-1l', "x\ny", "y", 0],
@@ -30,6 +31,8 @@ my @tv = (
 ['obsx-1', '+c', 'x' . ('y' x 10) . 'z', 'yyz', 0],
 # This is equivalent to +10l
 ['obsx-2', '+l', "x\n" . ("y\n" x 10) . 'z', "y\ny\nz", 0],
+# With no number, this is like -10l
+['obs-l', '-l', "x\n" . ("y\n" x 10) . 'z', ("y\n" x 9) . 'z', 0],
 
 # This should get `tail: +cl: No such file or directory'
 ['err-1', '+cl', '', '', 1],
@@ -39,14 +42,13 @@ my @tv = (
 
 # Since the number is larger than 2^64, this should provoke
 # the diagnostic: `tail: 99999999999999999999: number of bytes is so large \
-# that it is not representable'
+# that it is not representable' on all systems... for now, probably, maybe.
 ['err-3', '-c99999999999999999999', '', '', 1],
 ['err-4', '-c', '', '', 1],
 
 # Same as -l 10
 ['stdin-1', '-', '', '', 0],
 ['stdin-2', '-', "x\n" . ("y\n" x 10) . 'z', ("y\n" x 9) . 'z', 0],
-['stdin-3', '-l 10', "x\n" . ("y\n" x 10) . 'z', ("y\n" x 9) . 'z', 0],
 );
 
 sub test_vector
