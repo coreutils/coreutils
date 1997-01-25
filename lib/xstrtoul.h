0894db7b3f81d7b1dcacf021e9ef1ee77829aefe
Jim Meyering <jim@meyering.net>
1997-01-25 06:17:13 +0000

    (XSTRTOL_H): Undefine it.

--- lib/xstrtoul.h
+++ lib/xstrtoul.h
@@ -2,6 +2,12 @@
 # define XSTRTOUL_H_ 1
 
 # define STRING_TO_UNSIGNED 1
+
+/* Undefine this symbol so we can include xstrtol.h a second time.
+   Otherwise, a program that wanted both xstrtol.h and xstrtoul.h
+   would never get the declaration corresponding to the header file 
+   included after the first one.  */
+# undef XSTRTOL_H_
 # include "xstrtol.h"
 
 #endif /* not XSTRTOUL_H_ */
