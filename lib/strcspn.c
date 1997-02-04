1235c944d20211cf23027bc58dc9f6122be86290
Jim Meyering <jim@meyering.net>
1997-02-04 03:26:31 +0000

    libitize

--- lib/strcspn.c
+++ lib/strcspn.c
@@ -14,11 +14,11 @@
    along with this program; if not, write to the Free Software Foundation,
    Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
-#ifdef HAVE_CONFIG_H
+#if HAVE_CONFIG_H
 # include <config.h>
 #endif
 
-#ifdef HAVE_STRING_H
+#if HAVE_STRING_H
 # include <string.h>
 #else
 # include <strings.h>
