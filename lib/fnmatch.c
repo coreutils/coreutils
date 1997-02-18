1b1bca8a638f96e87ca03391000eba450193ae1a
Jim Meyering <jim@meyering.net>
1997-02-18 15:13:12 +0000

    .

--- lib/fnmatch.c
+++ lib/fnmatch.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993, 1997 Free Software Foundation, Inc.
 
    NOTE: The canonical source of this file is maintained with the GNU C
    Library.  Bugs can be reported to bug-glibc@prep.ai.mit.edu.
@@ -33,18 +33,6 @@
 
 #define ISUPPER(c) (ISASCII (c) && isupper (c))
 
-
-/* Comment out all this code if we are using the GNU C Library, and are not
-   actually compiling the library itself.  This code is part of the GNU C
-   Library, but also included in many other GNU distributions.  Compiling
-   and linking in this code is a waste when using the GNU C library
-   (especially if it is a shared library).  Rather than having every GNU
-   program understand `configure --with-gnu-libc' and omit the object files,
-   it is simpler to just do this in the source for each such file.  */
-
-#if defined (_LIBC) || !defined (__GNU_LIBRARY__)
-
-
 # ifndef errno
 extern int errno;
 # endif
@@ -60,7 +48,7 @@ fnmatch (pattern, string, flags)
   register const char *p = pattern, *n = string;
   register char c;
 
-/* Note that this evalutes C many times.  */
+/* Note that this evaluates C many times.  */
 # define FOLD(c)	((flags & FNM_CASEFOLD) && ISUPPER (c) ? tolower (c) : (c))
 
   while ((c = *p++) != '\0')
@@ -208,5 +196,3 @@ fnmatch (pattern, string, flags)
 
 # undef FOLD
 }
-
-#endif	/* _LIBC or not __GNU_LIBRARY__.  */
