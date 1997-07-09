a9309ad8726621cd900c52ec3b2f04fcfd62018c
Jim Meyering <jim@meyering.net>
1997-07-09 15:31:29 +0000

    .

---
+++ lib/strverscmp.h
@@ -0,0 +1,8 @@
+/* strverscmp.h -- compare strings holding indices/version numbers */
+
+#ifndef STRVERSCMP_H_
+# define STRVERSCMP_H_
+
+int strverscmp (const char*, const char*);
+
+#endif /* not STRVERSCMP_H_ */
