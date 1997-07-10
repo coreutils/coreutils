2d1baaeb315f2785b0633f21289af0679faa7de5
Jim Meyering <jim@meyering.net>
1997-07-10 18:30:08 +0000

    *** empty log message ***

--- tests/head/Test.pm
+++ tests/head/Test.pm
@@ -29,8 +29,8 @@ my @tv = (
 ['obs-4', '-2b', [\'in'], [\'in-1024'], 0], #'
 ['obs-5', '-1k', [\'in'], [\'in-1024'], 0], #'
 
-# This test fails for textutils-1.22, because head let 4096m overflow to 0.
-# Now it fails with a diagnostic.
+# This test fails for textutils-1.22, because head let 4096m overflow to 0
+# and did not fail.  Now head fails with a diagnostic.
 ['fail-0', '-n 4096m', "a\n", "", 1],
 
 # In spite of its name, this test passes -- just to contrast with the above.
