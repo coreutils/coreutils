3e8068282b31fd470a1727171452ce5883e8f81f
Jim Meyering <jim@meyering.net>
1997-09-14 03:12:35 +0000

    Include config.h.

--- lib/stpcpy.c
+++ lib/stpcpy.c
@@ -1,6 +1,6 @@
 /* Copyright (C) 1992, 1995, 1997 Free Software Foundation, Inc.
 
-NOTE: The canonical source of this file is maintained with the 
+NOTE: The canonical source of this file is maintained with the
 GNU C Library.  Bugs can be reported to bug-glibc@prep.ai.mit.edu.
 
 This program is free software; you can redistribute it and/or modify it
@@ -14,8 +14,12 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software Foundation, 
+along with this program; if not, write to the Free Software Foundation,
 Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
 
 #include <string.h>
 
