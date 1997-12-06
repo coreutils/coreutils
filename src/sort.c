20e1875a322c2966d053e5fa871244f2cf3370f8
Jim Meyering <jim@meyering.net>
1997-12-06 21:09:58 +0000

    revert last change

--- src/sort.c
+++ src/sort.c
@@ -2808,14 +2808,6 @@ but lacks following character offset"));
     outer:;
     }
 
-  {
-    char *x = getenv ("SORT_INITIAL_LINE_LENGTH");
-    if (x != NULL && *x)
-      {
-	linelength = atoi (x);
-      }
-  }
-
   if (key)
     insertkey (key);
 
