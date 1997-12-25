bb48c492c63d7d639199ba31a711758391ac8ba9
Jim Meyering <jim@meyering.net>
1997-12-25 17:44:08 +0000

    (_GNU_SOURCE): Define only if not already defined.

--- lib/getline.c
+++ lib/getline.c
@@ -1,6 +1,6 @@
 /* getline.c -- Replacement for GNU C library function getline
 
-Copyright (C) 1993, 1996 Free Software Foundation, Inc.
+Copyright (C) 1993, 1996, 1997 Free Software Foundation, Inc.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License as
@@ -24,7 +24,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
 
 /* The `getdelim' function is only declared if the following symbol
    is defined.  */
-#define _GNU_SOURCE	1
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+
 #include <stdio.h>
 #include <sys/types.h>
 
