08c3f260a19a5dd4eaafe14f48dcc66f979946fd
Jim Meyering <jim@meyering.net>
1997-12-28 09:48:26 +0000

    define PARAMS

--- lib/group-member.h
+++ lib/group-member.h
@@ -1,6 +1,14 @@
 #ifndef GROUP_MEMBER_H_
 # define GROUP_MEMBER_H_ 1
 
+# ifndef PARAMS
+#  if defined PROTOTYPES || (defined __STDC__ && __STDC__)
+#   define PARAMS(Args) Args
+#  else
+#   define PARAMS(Args) ()
+#  endif
+# endif
+
 int
   group_member PARAMS ((gid_t));
 
