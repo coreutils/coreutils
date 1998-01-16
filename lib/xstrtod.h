53cfcbd19c5e08093e74b4fc53b5dae369c98fd5
Jim Meyering <jim@meyering.net>
1998-01-16 08:51:39 +0000

    Make PARAMS-defining conditionals consistent.

--- lib/xstrtod.h
+++ lib/xstrtod.h
@@ -1,12 +1,13 @@
 #ifndef XSTRTOD_H
 # define XSTRTOD_H 1
 
-#undef PARAMS
-#if defined (__STDC__) && __STDC__
-# define PARAMS(Args) Args
-#else
-# define PARAMS(Args) ()
-#endif
+# ifndef PARAMS
+#  if defined PROTOTYPES || (defined __STDC__ && __STDC__)
+#   define PARAMS(Args) Args
+#  else
+#   define PARAMS(Args) ()
+#  endif
+# endif
 
 int
   xstrtod PARAMS ((const char *str, const char **ptr, double *result));
