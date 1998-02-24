938e7fe7ef2d5f8b437abac42fb548965301db18
Jim Meyering <jim@meyering.net>
1998-02-24 22:58:26 +0000

    add copyright

--- lib/basename.c
+++ lib/basename.c
@@ -1,4 +1,19 @@
-/* basename.c -- return the last element in a path */
+/* basename.c -- return the last element in a path
+   Copyright (C) 1990, 1998 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #if HAVE_CONFIG_H
 # include <config.h>
