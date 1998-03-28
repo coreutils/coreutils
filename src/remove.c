ca6cf35a5e97cd75eb8f0257b7ab7f1d85a97289
Jim Meyering <jim@meyering.net>
1998-03-28 14:56:11 +0000

    Add copyright. [_AIX]: Add #pragma alloca.

--- src/remove.c
+++ src/remove.c
@@ -1,3 +1,26 @@
+/* remove.c -- core functions for removing files and directories
+   Copyright (C) 88, 90, 91, 94, 95, 96, 97, 1998 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
+/* Extracted from rm.c and librarified by Jim Meyering.  */
+
+#ifdef _AIX
+ #pragma alloca
+#endif
+
 #include <config.h>
 #include <stdio.h>
 #include <sys/types.h>
