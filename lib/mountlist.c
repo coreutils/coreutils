aa1c5467ec3a43e2bad348c241222aa7d3fee085
Jim Meyering <jim@meyering.net>
1998-06-29 17:01:48 +0000

    (fstype_to_string): Guard with #if ! HAVE_F_FSTYPENAME_IN_STATFS.

--- lib/mountlist.c
+++ lib/mountlist.c
@@ -142,99 +142,101 @@ xatoi (cp)
 
 #if MOUNTED_GETMNTINFO
 
+# if ! HAVE_F_FSTYPENAME_IN_STATFS
 static char *
 fstype_to_string (short t)
 {
   switch (t)
     {
-# ifdef MOUNT_PC
+#  ifdef MOUNT_PC
     case MOUNT_PC:
       return "pc";
-# endif
-# ifdef MOUNT_MFS
+#  endif
+#  ifdef MOUNT_MFS
     case MOUNT_MFS:
       return "mfs";
-# endif
-# ifdef MOUNT_LO
+#  endif
+#  ifdef MOUNT_LO
     case MOUNT_LO:
       return "lo";
-# endif
-# ifdef MOUNT_TFS
+#  endif
+#  ifdef MOUNT_TFS
     case MOUNT_TFS:
       return "tfs";
-# endif
-# ifdef MOUNT_TMP
+#  endif
+#  ifdef MOUNT_TMP
     case MOUNT_TMP:
       return "tmp";
-# endif
-# ifdef MOUNT_UFS
+#  endif
+#  ifdef MOUNT_UFS
    case MOUNT_UFS:
      return "ufs" ;
-# endif
-# ifdef MOUNT_NFS
+#  endif
+#  ifdef MOUNT_NFS
    case MOUNT_NFS:
      return "nfs" ;
-# endif
-# ifdef MOUNT_MSDOS
+#  endif
+#  ifdef MOUNT_MSDOS
    case MOUNT_MSDOS:
      return "msdos" ;
-# endif
-# ifdef MOUNT_LFS
+#  endif
+#  ifdef MOUNT_LFS
    case MOUNT_LFS:
      return "lfs" ;
-# endif
-# ifdef MOUNT_LOFS
+#  endif
+#  ifdef MOUNT_LOFS
    case MOUNT_LOFS:
      return "lofs" ;
-# endif
-# ifdef MOUNT_FDESC
+#  endif
+#  ifdef MOUNT_FDESC
    case MOUNT_FDESC:
      return "fdesc" ;
-# endif
-# ifdef MOUNT_PORTAL
+#  endif
+#  ifdef MOUNT_PORTAL
    case MOUNT_PORTAL:
      return "portal" ;
-# endif
-# ifdef MOUNT_NULL
+#  endif
+#  ifdef MOUNT_NULL
    case MOUNT_NULL:
      return "null" ;
-# endif
-# ifdef MOUNT_UMAP
+#  endif
+#  ifdef MOUNT_UMAP
    case MOUNT_UMAP:
      return "umap" ;
-# endif
-# ifdef MOUNT_KERNFS
+#  endif
+#  ifdef MOUNT_KERNFS
    case MOUNT_KERNFS:
      return "kernfs" ;
-# endif
-# ifdef MOUNT_PROCFS
+#  endif
+#  ifdef MOUNT_PROCFS
    case MOUNT_PROCFS:
      return "procfs" ;
-# endif
-# ifdef MOUNT_AFS
+#  endif
+#  ifdef MOUNT_AFS
    case MOUNT_AFS:
      return "afs" ;
-# endif
-# ifdef MOUNT_CD9660
+#  endif
+#  ifdef MOUNT_CD9660
    case MOUNT_CD9660:
      return "cd9660" ;
-# endif
-# ifdef MOUNT_UNION
+#  endif
+#  ifdef MOUNT_UNION
    case MOUNT_UNION:
      return "union" ;
-# endif
-# ifdef MOUNT_DEVFS
+#  endif
+#  ifdef MOUNT_DEVFS
    case MOUNT_DEVFS:
      return "devfs" ;
-# endif
-# ifdef MOUNT_EXT2FS
+#  endif
+#  ifdef MOUNT_EXT2FS
    case MOUNT_EXT2FS:
      return "ext2fs" ;
-# endif
+#  endif
     default:
       return "?";
     }
 }
+# endif /* ! HAVE_F_FSTYPENAME_IN_STATFS */
 
 /* __NetBSD__ || BSD_NET2 || __OpenBSD__ */
 static char *
