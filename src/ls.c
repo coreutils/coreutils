79d411ceba0e71d6c049f5fe44142247b752adee
Jim Meyering <jim@meyering.net>
1998-08-15 16:43:13 +0000

    (qmark_funny_chars): Add comment from Paul eggert.

--- src/ls.c
+++ src/ls.c
@@ -449,8 +449,15 @@ struct ignore_pattern
 
 static struct ignore_pattern *ignore_patterns;
 
-/* Nonzero means output nongraphic chars in file names as `?'.  -q  */
-
+/* Nonzero means output nongraphic chars in file names as `?'.
+   (-q, --hide-control-chars)
+   qmark_funny_chars and the quoting style (-Q, --quoting-style=WORD) are
+   independent.  The algorithm is: first, obey the quoting style to get a
+   string representing the file name;  then, if qmark_funny_chars is set,
+   replace all nonprintable chars in that string with `?'.  It's necessary
+   to replace nonprintable chars even in quoted strings, because we don't
+   want to mess up the terminal if control chars get sent to it, and some
+   quoting methods pass through control chars as-is.  */
 static int qmark_funny_chars;
 
 /* Quoting options for file and dir name output.  */
@@ -820,6 +827,7 @@ decode_switches (int argc, char **argv)
       if (isatty (1))
 	{
 	  format = many_per_line;
+	  /* See description of qmark_funny_chars, above.  */
 	  qmark_funny_chars = 1;
 	}
       else
