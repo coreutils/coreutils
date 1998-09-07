164bff8dc027a0d91690dce814bbe52e1a1a1892
Jim Meyering <jim@meyering.net>
1998-09-07 02:18:43 +0000

    *** empty log message ***

--- lib/posixtm.h
+++ lib/posixtm.h
@@ -1,11 +1,16 @@
+#ifndef POSIXTM_H_
+# define POSIXTM_H_
+
 /* POSIX Date Syntax flags.  */
-#define PDS_LEADING_YEAR 1
-#define PDS_TRAILING_YEAR 2
-#define PDS_CENTURY 4
-#define PDS_SECONDS 8
+# define PDS_LEADING_YEAR 1
+# define PDS_TRAILING_YEAR 2
+# define PDS_CENTURY 4
+# define PDS_SECONDS 8
 
 time_t
 posixtime (const char *s, unsigned int syntax_bits);
 
 struct tm *
 posixtm (const char *s, unsigned int syntax_bits);
+
+#endif
