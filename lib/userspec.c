9497429d9b153fe90efb1856af0c09e31279c560
Jim Meyering <jim@meyering.net>
1998-12-19 17:20:03 +0000

    break long line

--- lib/userspec.c
+++ lib/userspec.c
@@ -1,5 +1,5 @@
 /* userspec.c -- Parse a user and group string.
-   Copyright (C) 1989, 1990, 1991, 1992, 1997, 1998 Free Software Foundation, Inc.
+   Copyright (C) 1989-1992, 1997, 1998 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
