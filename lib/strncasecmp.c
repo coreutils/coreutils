5c6d6248e9104638dbd54db848d27415b89f8854
Jim Meyering <jim@meyering.net>
1999-01-01 07:08:54 +0000

    .

--- lib/strncasecmp.c
+++ lib/strncasecmp.c
@@ -1,3 +1,19 @@
+/* Copyright (C) 1998 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
 #if HAVE_CONFIG_H
 # include <config.h>
 #endif
@@ -7,7 +23,7 @@
 
 #define TOLOWER(Ch) (isupper (Ch) ? tolower (Ch) : (Ch))
 
-/* Compare no more than N characters of S1 and S2,
+/* Compare no more than N characters of strings S1 and S2,
    ignoring case, returning less than, equal to or
    greater than zero if S1 is lexicographically less
    than, equal to or greater than S2.  */
@@ -26,9 +42,11 @@ strncasecmp (const char *s1, const char *s2, size_t n)
     {
       c1 = TOLOWER (*p1++);
       c2 = TOLOWER (*p2++);
-      if (c1 == '\0' || c1 != c2)
-        return c1 - c2;
-    } while (--n > 0);
+
+      if (--n == 0 || c1 == '\0')
+	break;
+    }
+  while (c1 == c2);
 
   return c1 - c2;
 }
