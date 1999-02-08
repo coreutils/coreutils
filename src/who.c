e9144d8d30b1f8fd4c544cc73ff0720015dfd5cd
Jim Meyering <jim@meyering.net>
1999-02-08 03:30:52 +0000

    copyright date

--- src/who.c
+++ src/who.c
@@ -1,5 +1,5 @@
 /* GNU's who.
-   Copyright (C) 92, 93, 94, 95, 96, 1997, 1998 Free Software Foundation, Inc.
+   Copyright (C) 1992-1999 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
