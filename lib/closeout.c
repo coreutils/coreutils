42edc330edf5617544515398ebef84916b6f044f
Jim Meyering <jim@meyering.net>
1999-04-04 14:30:30 +0000

    fix comments add FIXME

--- lib/closeout.c
+++ lib/closeout.c
@@ -43,14 +43,19 @@ extern int errno;
 #include "error.h"
 
 /* Close standard output, exiting with status STATUS on failure.
-   If a program writes *anything* to stdout, that program should close
-   stdout and make sure that the close succeeds.  Otherwise, suppose that
-   you go to the extreme of checking the return status of every function
-   that does an explicit write to stdout.  The last printf can succeed in
-   writing to the internal stream buffer, and yet the fclose(stdout) could
-   still fail (due e.g., to a disk full error) when it tries to write
-   out that buffered data.  Thus, you would be left with an incomplete
-   output file and the offending program would exit successfully.
+   If a program writes *anything* to stdout, that program should `fflush'
+   stdout and make sure that it succeeds before exiting.  Otherwise,
+   suppose that you go to the extreme of checking the return status
+   of every function that does an explicit write to stdout.  The last
+   printf can succeed in writing to the internal stream buffer, and yet
+   the fclose(stdout) could still fail (due e.g., to a disk full error)
+   when it tries to write out that buffered data.  Thus, you would be
+   left with an incomplete output file and the offending program would
+   exit successfully.
+
+   FIXME: note the fflush suggested above is implicit in the fclose
+   we actually do below.  Consider doing only the fflush and/or using
+   setvbuf to inhibit buffering.
 
    Besides, it's wasteful to check the return value from every call
    that writes to stdout -- just let the internal stream state record
