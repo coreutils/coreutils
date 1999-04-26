d1e59e17e75f23e3e5ee2bb4fa7623bf4b6bca7f
Jim Meyering <jim@meyering.net>
1999-04-26 13:13:36 +0000

    <config.h>, <sys/types.h>: Include for mode_t. (mode_string): Now takes mode_t.

--- lib/filemode.h
+++ lib/filemode.h
@@ -1,9 +1,19 @@
-#ifndef PARAMS
-# if defined PROTOTYPES || (defined __STDC__ && __STDC__)
-#  define PARAMS(Args) Args
-# else
-#  define PARAMS(Args) ()
-# endif
-#endif
+#ifndef FILEMODE_H_
 
-void mode_string PARAMS ((short unsigned int mode, char *str));
+# if HAVE_CONFIG_H
+#  include <config.h>
+# endif
+
+# include <sys/types.h>
+
+# ifndef PARAMS
+#  if defined PROTOTYPES || (defined __STDC__ && __STDC__)
+#   define PARAMS(Args) Args
+#  else
+#   define PARAMS(Args) ()
+#  endif
+# endif
+
+void mode_string PARAMS ((mode_t mode, char *str));
+
+#endif
