99571c629e1c4b58bdb5071c54c897d3d239e685
Jim Meyering <jim@meyering.net>
1999-05-03 02:42:49 +0000

    (main): Manually handle `--', since we no longer call getopt.

--- src/factor.c
+++ src/factor.c
@@ -183,6 +183,13 @@ main (int argc, char **argv)
 
   parse_long_options (argc, argv, PROGRAM_NAME, GNU_PACKAGE, VERSION,
 		      AUTHORS, usage);
+  /* The above handles --help and --version.
+     Since there is no other invocation of getopt, handle `--' here.  */
+  if (argc > 1 && STREQ (argv[1], "--"))
+    {
+      --argc;
+      ++argv;
+    }
 
   fail = 0;
   if (argc == 1)
