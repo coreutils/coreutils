4c9f46a4a1f84420b405833d5f94d672c7343e9f
Jim Meyering <jim@meyering.net>
1999-05-13 20:44:45 +0000

    [__GLIBC__ >= 2]: #if-out prototypes.

--- lib/getline.h
+++ lib/getline.h
@@ -1,4 +1,4 @@
-/*  Copyright (C) 1995, 1997 Free Software Foundation, Inc.
+/*  Copyright (C) 1995, 1997, 1999 Free Software Foundation, Inc.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -27,10 +27,12 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 #  endif
 # endif
 
+# if __GLIBC__ < 2
 int
 getline PARAMS ((char **_lineptr, size_t *_n, FILE *_stream));
 
 int
 getdelim PARAMS ((char **_lineptr, size_t *_n, int _delimiter, FILE *_stream));
+# endif
 
 #endif /* not GETLINE_H_ */
