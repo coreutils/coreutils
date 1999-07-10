c44e7a8352db0c045f72d683e63dc2144a31840b
Jim Meyering <jim@meyering.net>
1999-07-10 16:36:56 +0000

    use a space, not TAB after #define

--- lib/strtoul.c
+++ lib/strtoul.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1999 Free Software Foundation, Inc.
 
 NOTE: The canonical source of this file is maintained with the GNU C Library.
 Bugs can be reported to bug-glibc@prep.ai.mit.edu.
@@ -17,6 +17,6 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software Foundation,
 Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
-#define	UNSIGNED	1
+#define UNSIGNED	1
 
 #include <strtol.c>
