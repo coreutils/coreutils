bee39b77d9722e8c705c4feb30d44cbf5ecbc3bb
Jim Meyering <jim@meyering.net>
1999-08-13 07:57:15 +0000

    .

---
+++ tests/tsort/Test.pm
@@ -0,0 +1,18 @@
+# -*-perl-*-
+package Test;
+require 5.002;
+use strict;
+
+my @tv = (
+#test   options   input   expected-output   expected-return-code
+#
+["cycle-1", '', "t x\nt x\ns t\n", "FIXME", 0],
+
+);
+
+sub test_vector
+{
+  return @tv;
+}
+
+1;
