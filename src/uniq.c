7a7ef5ff5befba0d3822b3ab301736fcfc2d0a68
Jim Meyering <jim@meyering.net>
1999-08-22 11:31:08 +0000

    (check_file): Explain why we have separate `if' and `else' blocks when the code in the else block could handle both cases.

--- src/uniq.c
+++ src/uniq.c
@@ -238,6 +238,11 @@ check_file (const char *infile, const char *outfile)
   initbuffer (thisline);
   initbuffer (prevline);
 
+  /* This duplication is to distinguish the common case (in which none of the
+     following options has been specified: --count, -repeated, --all-repeated,
+     --unique) from the others.  In the common case, we can output each new
+     line right away, without waiting to see if the next one is different).  */
+
   if (mode == output_all && countmode == count_none)
     {
       char *prevfield IF_LINT (= NULL);
