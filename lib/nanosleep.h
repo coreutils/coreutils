1885aeaaa41210070c20148ad6ceb725018c470d
Jim Meyering <jim@meyering.net>
2000-01-05 12:03:06 +0000

    *** empty log message ***

---
+++ lib/nanosleep.h
@@ -0,0 +1,57 @@
+/*  Copyright (C) 2000 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
+#if ! defined NANOSLEEP_H
+# define NANOSLEEP_H
+
+# if HAVE_CONFIG_H
+#  include <config.h>
+# endif
+
+# ifndef PARAMS
+#  if defined PROTOTYPES || (defined __STDC__ && __STDC__)
+#   define PARAMS(Args) Args
+#  else
+#   define PARAMS(Args) ()
+#  endif
+# endif
+
+# include <sys/types.h>
+# if TIME_WITH_SYS_TIME
+#  include <sys/time.h>
+#  include <time.h>
+# else
+#  if HAVE_SYS_TIME_H
+#   include <sys/time.h>
+#  else
+#   include <time.h>
+#  endif
+# endif
+
+# if ! HAVE_STRUCT_TIMESPEC
+/* Some systems don't define this struct.  */
+struct timespec
+{
+  time_t tv_sec;
+  long tv_nsec;
+};
+# endif
+
+int
+nanosleep PARAMS ((const struct timespec *requested_delay,
+		   struct timespec *remaining_delay)
+
+#endif
