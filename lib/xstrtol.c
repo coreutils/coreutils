4e4c0e24d594e3c971b9116619c82053f2b1f6a2
Jim Meyering <jim@meyering.net>
2000-01-07 07:03:01 +0000

    tweak copyright line

--- lib/xstrtol.c
+++ lib/xstrtol.c
@@ -1,5 +1,5 @@
 /* A more useful interface to strtol.
-   Copyright 1995, 1996, 1998, 1999 Free Software Foundation, Inc.
+   Copyright (C) 1995, 1996, 1998-2000 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
