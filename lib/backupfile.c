7a80c0cacc1d49d96c5f0eaf5de8c0b261c06339
Jim Meyering <jim@meyering.net>
2000-02-27 17:39:52 +0000

    Use `#if !HAVE_DECL...' instead of `#ifndef HAVE_DECL..' now that autoconf always defines the HAVE_DECL_ symbols.
    Arrange for cpp to fail if the configure-time
    declaration check was not run.

--- lib/backupfile.c
+++ lib/backupfile.c
@@ -1,5 +1,5 @@
 /* backupfile.c -- make Emacs style backup file names
-   Copyright (C) 1990-1997, 1998, 1999 Free Software Foundation, Inc.
+   Copyright (C) 1990-1997, 1998, 1999, 2000 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -65,6 +65,9 @@ char *malloc ();
 #endif
 
 #ifndef HAVE_DECL_GETENV
+'this configure-time declaration test was not run'
+#endif
+#if !HAVE_DECL_GETENV
 char *getenv ();
 #endif
 
