77ec117299ea3817acd0c1a439b1f28b735f86bd
Jim Meyering <jim@meyering.net>
2000-03-01 13:22:57 +0000

    New file from Bruno. filter through cppi

---
+++ lib/unicodeio.h
@@ -0,0 +1,9 @@
+#ifndef UNICODEIO_H
+# define UNICODEIO_H
+
+# include <stdio.h>
+
+/* Outputs the Unicode character CODE to the output stream STREAM.  */
+extern void print_unicode_char (FILE *stream, unsigned int code);
+
+#endif
