aca3853e18394ac12f46d57348961e40cfe43a1c
Jim Meyering <jim@meyering.net>
2000-03-05 10:21:58 +0000

    (PARAMS): Define. Use it to guard prototype.

--- lib/unicodeio.h
+++ lib/unicodeio.h
@@ -3,7 +3,15 @@
 
 # include <stdio.h>
 
+# ifndef PARAMS
+#  if defined PROTOTYPES || (defined __STDC__ && __STDC__)
+#   define PARAMS(Args) Args
+#  else
+#   define PARAMS(Args) ()
+#  endif
+# endif
+
 /* Outputs the Unicode character CODE to the output stream STREAM.  */
-extern void print_unicode_char (FILE *stream, unsigned int code);
+extern void print_unicode_char PARAMS((FILE *stream, unsigned int code));
 
 #endif
