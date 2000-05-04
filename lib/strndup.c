c8fa96131e6895aa4b8d299b4ef4804703cbc73f
Jim Meyering <jim@meyering.net>
2000-05-04 06:35:49 +0000

    update from glibc

--- lib/strndup.c
+++ lib/strndup.c
@@ -1,21 +1,21 @@
-/* Copyright (C) 1996, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1996, 1997, 1998, 2000 Free Software Foundation, Inc.
 
-NOTE: The canonical source of this file is maintained with the GNU C Library.
-Bugs can be reported to bug-glibc@prep.ai.mit.edu.
+   NOTE: The canonical source of this file is maintained with the GNU C Library.
+   Bugs can be reported to bug-glibc@prep.ai.mit.edu.
 
-This program is free software; you can redistribute it and/or modify it
-under the terms of the GNU General Public License as published by the
-Free Software Foundation; either version 2, or (at your option) any
-later version.
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2, or (at your option) any
+   later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software Foundation,
-Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #ifdef HAVE_CONFIG_H
 # include "config.h"
@@ -24,22 +24,32 @@ Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 #include <stdio.h>
 #include <sys/types.h>
 
-#ifdef STDC_HEADERS
-# include <string.h>
+#if defined _LIBC || defined STDC_HEADERS
 # include <stdlib.h>
+# include <string.h>
 #else
 char *malloc ();
 #endif
 
-/* Duplicate S, returning an identical malloc'd string.  */
+#undef __strndup
+#undef strndup
+
+#ifndef weak_alias
+# define __strndup strndup
+#endif
+
 char *
-strndup (const char *s, size_t n)
+__strndup (const char *s, size_t n)
 {
-  char *new = malloc (n + 1);
+  size_t len = strnlen (s, n);
+  char *new = malloc (len + 1);
 
   if (new == NULL)
     return NULL;
 
-  new[n] = '\0';
-  return (char *) memcpy (new, s, n);
+  new[len] = '\0';
+  return (char *) memcpy (new, s, len);
 }
+#ifdef weak_alias
+weak_alias (__strndup, strndup)
+#endif
