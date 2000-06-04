1f07d34ed234af32ed55692c6659b8805b91a8aa
Jim Meyering <jim@meyering.net>
2000-06-04 13:20:20 +0000

    Include <config.h> if HAVE_CONFIG_H.

--- lib/strnlen.c
+++ lib/strnlen.c
@@ -17,6 +17,10 @@
    write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.  */
 
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
 #include <string.h>
 
 #ifndef HAVE_DECL_MEMCHR
