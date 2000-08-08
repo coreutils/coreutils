13587cf0ec42e3fcf9ec0600705cc41dadd6a847
Jim Meyering <jim@meyering.net>
2000-08-08 07:11:12 +0000

    (xstrndup): Invoke xalloc_die instead of printing our own message.

--- src/dircolors.c
+++ src/dircolors.c
@@ -126,7 +126,7 @@ xstrndup (const char *s, size_t n)
 {
   char *new = strndup (s, n);
   if (new == NULL)
-    error (EXIT_FAILURE, 0, _("virtual memory exhausted"));
+    xalloc_die ();
   return new;
 }
 
