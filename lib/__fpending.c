5cd332146d5c34ed0f8b5ec52a5c954ff7496be9
Jim Meyering <jim@meyering.net>
2000-08-25 07:43:45 +0000

    *** empty log message ***

--- lib/__fpending.c
+++ lib/__fpending.c
@@ -1,41 +1,41 @@
 size_t
 __fpending (FILE *fp)
 {
-#if FPENDING_GLIBC2
+#if STREAM_FPENDING_GLIBC2
   return fp->_IO_write_ptr - fp->_IO_write_base;
 #endif
 
-#if FPENDING__PTR
+#if STREAM_FPENDING__PTR
   /* Traditional Unix */
   return fp->_ptr - fp->_base;
 #endif
 
-#if FPENDING__P
+#if STREAM_FPENDING__P
   /* BSD */
   return fp->_p - fp->_bf._base;
 #endif
 
-#if FPENDING__P
+#if STREAM_FPENDING__P
   /* SCO, Unixware */
   return fp->__ptr - fp->__base;
 #endif
 
-#if FPENDING__BUFP
+#if STREAM_FPENDING__BUFP
   /* old glibc? */
   return fp->__bufp - fp->__buffer;
 #endif
 
-#if FPENDING__PPTR
+#if STREAM_FPENDING__PPTR
   /* old glibc iostream? */
   return fp->_pptr - fp->_pbase;
 #endif
 
-#if FPENDING__PTR_DEREF
+#if STREAM_FPENDING__PTR_DEREF
   /* VMS */
   return (*fp)->_ptr - (*fp)->_base;
 #endif
 
-#if FPENDING_NOT_AVAILABLE
+#if STREAM_FPENDING_NOT_AVAILABLE
   /* e.g., DGUX R4.11 */
   return 1; /* i.e. the info is not available */
 #endif
