93fe4a3b72e22a610f27927010da65c1f454ac43
Jim Meyering <jim@meyering.net>
2000-09-08 09:01:32 +0000

    (S_IRWXUGO): Define here, instead.

--- src/sys2.h
+++ src/sys2.h
@@ -118,6 +118,10 @@
 # define S_IXUGO (S_IXUSR | S_IXGRP | S_IXOTH)
 #endif
 
+#ifndef S_IRWXUGO
+# define S_IRWXUGO (S_IRWXU | S_IRWXG | S_IRWXO)
+#endif
+
 /* All the mode bits that can be affected by chmod.  */
 #define CHMOD_MODE_BITS \
   (S_ISUID | S_ISGID | S_ISVTX | S_IRWXU | S_IRWXG | S_IRWXO)
