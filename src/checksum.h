cf806f5d2daf229f790d40c264ffb736914b0ef7
Jim Meyering <jim@meyering.net>
2000-10-22 11:51:50 +0000

    .

---
+++ src/checksum.h
@@ -0,0 +1,15 @@
+#include <config.h>
+
+#include <sys/types.h>
+#include "system.h"
+
+/* For long options that have no equivalent short option, use a
+   non-character as a pseudo short option, starting with CHAR_MAX + 1.  */
+enum
+{
+  ALG_UNSPECIFIED = 0,
+  ALG_MD5 = CHAR_MAX + 1,
+  ALG_SHA1
+};
+
+extern int algorithm;
