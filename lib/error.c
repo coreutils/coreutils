b3f23e14d2cd9e950265d1ecc44a84623d9dc9e6
Jim Meyering <jim@meyering.net>
2000-11-11 14:32:00 +0000

    Add a couple #includes, merging from GNU libc version.

--- lib/error.c
+++ lib/error.c
@@ -1,5 +1,5 @@
 /* Error handler for noninteractive utilities
-   Copyright (C) 1990-2000 Free Software Foundation, Inc.
+   Copyright (C) 1990-1998, 2000 Free Software Foundation, Inc.
 
    This file is part of the GNU C Library.  Its master source is NOT part of
    the C library, however.  The master source lives in /gd/gnu/lib.
@@ -26,6 +26,9 @@
 #endif
 
 #include <stdio.h>
+#if HAVE_LIBINTL_H
+# include <libintl.h>
+#endif
 
 #if HAVE_VPRINTF || HAVE_DOPRNT || _LIBC
 # if __STDC__
@@ -82,6 +85,11 @@ unsigned int error_message_count;
    Instead make it a weak alias.  */
 # define error __error
 # define error_at_line __error_at_line
+
+# ifdef USE_IN_LIBIO
+#  include <libio/iolibio.h>
+#  define fflush(s) _IO_fflush (s)
+# endif
 
 #else /* not _LIBC */
 
