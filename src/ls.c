5285933ed8920cee9340e0ce38e039ef8decb282
Jim Meyering <jim@meyering.net>
2001-01-02 07:19:59 +0000

    copyright

--- src/ls.c
+++ src/ls.c
@@ -1,5 +1,5 @@
 /* `dir', `vdir' and `ls' directory listing programs for GNU.
-   Copyright (C) 85, 88, 90, 91, 1995-2000 Free Software Foundation, Inc.
+   Copyright (C) 85, 88, 90, 91, 1995-2001 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
