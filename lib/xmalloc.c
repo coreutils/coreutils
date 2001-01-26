dcfda5180125c72e734db4b3a57013c2ee953b0d
Jim Meyering <jim@meyering.net>
2001-01-26 11:13:28 +0000

    [HAVE_DONE_WORKING_MALLOC_CHECK]: Enclose error-evoking line in double quotes so that it evokes a better diagnostic. [HAVE_DONE_WORKING_REALLOC_CHECK]: Likewise.

--- lib/xmalloc.c
+++ lib/xmalloc.c
@@ -47,11 +47,11 @@ void free ();
 #endif
 
 #ifndef HAVE_DONE_WORKING_MALLOC_CHECK
-you must run the autoconf test for a properly working malloc -- see malloc.m4
+"you must run the autoconf test for a properly working malloc -- see malloc.m4"
 #endif
 
 #ifndef HAVE_DONE_WORKING_REALLOC_CHECK
-you must run the autoconf test for a properly working realloc -- see realloc.m4
+"you must run the autoconf test for a properly working realloc --see realloc.m4"
 #endif
 
 /* Exit value when the requested amount of memory is not available.
