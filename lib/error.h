0e6ddcffef72387d4aeed4cb108c34837975c273
Jim Meyering <jim@meyering.net>
2001-08-04 17:52:38 +0000

    (__attribute__): Remove `|| __STRICT_ANSI__' from #if stmt, to get in sync with glibc.

--- lib/error.h
+++ lib/error.h
@@ -25,7 +25,7 @@
 
 #ifndef __attribute__
 /* This feature is available in gcc versions 2.5 and later.  */
-# if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 5) || __STRICT_ANSI__
+# if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 5)
 #  define __attribute__(Spec) /* empty */
 # endif
 /* The __-protected variants of `format' and `printf' attributes
