465a0e3b5ed69466f11764ed85ab0fe331238064
Jim Meyering <jim@meyering.net>
2001-08-04 17:06:05 +0000

    (MBSW_REJECT_UNPRINTABLE, MBSW_REJECT_INVALID): Renamed from MBSW_ACCEPT_UNPRINTABLE and MBSW_ACCEPT_INVALID with inverted sense.  All uses changed.

--- lib/mbswidth.h
+++ lib/mbswidth.h
@@ -16,7 +16,7 @@
    Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #ifndef PARAMS
-# if defined PROTOTYPES || defined __STDC__
+# if defined (__GNUC__) || __STDC__
 #  define PARAMS(Args) Args
 # else
 #  define PARAMS(Args) ()
@@ -25,13 +25,14 @@
 
 /* Optional flags to influence mbswidth/mbsnwidth behavior.  */
 
-/* If this bit is set, assume invalid characters have width 0.
-   Otherwise, return -1 upon finding an invalid or incomplete character.  */
-#define MBSW_ACCEPT_INVALID 1
+/* If this bit is set, return -1 upon finding an invalid or incomplete
+   character.  Otherwise, assume invalid characters have width 1.  */
+#define MBSW_REJECT_INVALID 1
 
-/* If this bit is set, assume unprintable characters have width 1.
-   Otherwise, return -1 upon finding a non-printable character.  */
-#define MBSW_ACCEPT_UNPRINTABLE	2
+/* If this bit is set, return -1 upon finding a non-printable character.
+   Otherwise, assume unprintable characters have width 0 if they are
+   control characters and 1 otherwise.  */
+#define MBSW_REJECT_UNPRINTABLE	2
 
 /* Returns the number of screen columns needed for STRING.  */
 #define mbswidth gnu_mbswidth  /* avoid clash with UnixWare 7.1.1 function */
