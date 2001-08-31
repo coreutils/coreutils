8a7b2f0191e5fe406c325eeffa2872e357866c2e
Jim Meyering <jim@meyering.net>
2001-08-31 07:49:39 +0000

    Remove '2001' from copyright notice.

--- lib/hash.c
+++ lib/hash.c
@@ -1,5 +1,5 @@
 /* hash - hashing table processing.
-   Copyright (C) 1998, 1999, 2000, 2001 Free Software Foundation, Inc.
+   Copyright (C) 1998, 1999, 2000 Free Software Foundation, Inc.
    Written by Jim Meyering, 1992.
 
    This program is free software; you can redistribute it and/or modify
