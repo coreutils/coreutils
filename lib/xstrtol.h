2adb484d904c34c91af29a4cbe7ccf17f53d6f56
Jim Meyering <jim@meyering.net>
2001-08-31 08:15:10 +0000

    * lib/xstrtol.h: Add copyright notice. (_DECLARE_XSTRTOL): Improve quality of diagnostic for LONGINT_INVALID_SUFFIX_CHAR.

--- lib/xstrtol.h
+++ lib/xstrtol.h
@@ -1,3 +1,20 @@
+/* A more useful interface to strtol.
+   Copyright 1995, 1996, 1998, 1999, 2001 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
 #ifndef XSTRTOL_H_
 # define XSTRTOL_H_ 1
 
@@ -44,7 +61,7 @@ _DECLARE_XSTRTOL (xstrtoumax, uintmax_t)
 	  break;							\
 									\
 	case LONGINT_INVALID_SUFFIX_CHAR:				\
-	  error ((Exit_code), 0, "invalid character following %s `%s'",	\
+	  error ((Exit_code), 0, "invalid character following %s in `%s'", \
 		 (Argument_type_string), (Str));			\
 	  break;							\
 									\
