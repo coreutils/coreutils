edbe4553a820a4fdf6bfdb83e1560df7d6d7a778
Jim Meyering <jim@meyering.net>
2001-08-31 06:46:52 +0000

    copyright

--- src/du.c
+++ src/du.c
@@ -1,5 +1,5 @@
 /* du -- summarize disk usage
-   Copyright (C) 88, 89, 90, 91, 1995-2000 Free Software Foundation, Inc.
+   Copyright (C) 88, 89, 90, 91, 1995-2001 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
