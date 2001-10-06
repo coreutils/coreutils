de879011f3d17911a31472852f9baf56b627c84f
Jim Meyering <jim@meyering.net>
2001-10-06 17:24:58 +0000

    Update prototype for hash_init.

--- src/cp-hash.h
+++ src/cp-hash.h
@@ -1,8 +1,4 @@
-/* For created inodes, a pointer in the search structure to this
-   character identifies the inode as being new.  */
-extern char new_file;
-
-void hash_init PARAMS ((unsigned int modulus, unsigned int entry_tab_size));
+void hash_init PARAMS ((void));
 void forget_all PARAMS ((void));
 char *remember_copied PARAMS ((const char *node, ino_t ino, dev_t dev));
 int remember_created PARAMS ((const char *path));
