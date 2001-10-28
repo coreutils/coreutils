09edea73816c6233cc63e39d71d010943ad26373
Jim Meyering <jim@meyering.net>
2001-10-28 10:01:07 +0000

    update copyright year

--- src/sum.c
+++ src/sum.c
@@ -1,5 +1,5 @@
 /* sum -- checksum and count the blocks in a file
-   Copyright (C) 86, 89, 91, 1995-2000 Free Software Foundation, Inc.
+   Copyright (C) 86, 89, 91, 1995-2001 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
