f7eb4c8913cd6466edd7040013819e0afbc519bf
Jim Meyering <jim@meyering.net>
2001-11-11 15:08:02 +0000

    (usage): Split --help output into smaller pieces. Use fputs, not printf.

--- src/tail.c
+++ src/tail.c
@@ -246,49 +246,61 @@ Mandatory arguments to long options are mandatory for short options too.\n\
       --retry              keep trying to open a file even if it is\n\
                            inaccessible when tail starts or if it becomes\n\
                            inaccessible later -- useful only with -f\n\
+"), DEFAULT_N_LINES);
+     fputs (_("\
   -c, --bytes=N            output the last N bytes\n\
   -f, --follow[={name|descriptor}]\n\
                            output appended data as the file grows;\n\
                            -f, --follow, and --follow=descriptor are\n\
                            equivalent\n\
   -F                       same as --follow=name --retry\n\
+"), stdout);
+     printf (_("\
   -n, --lines=N            output the last N lines, instead of the last %d\n\
       --max-unchanged-stats=N\n\
                            with --follow=name, reopen a FILE which has not\n\
                            changed size after N (default %d) iterations\n\
                            to see if it has been unlinked or renamed\n\
                            (this is the usual case of rotated log files)\n\
+"),
+	     DEFAULT_N_LINES,
+	     DEFAULT_MAX_N_UNCHANGED_STATS_BETWEEN_OPENS
+	     );
+     fputs (_("\
       --pid=PID            with -f, terminate after process ID, PID dies\n\
   -q, --quiet, --silent    never output headers giving file names\n\
   -s, --sleep-interval=S   with -f, each iteration lasts approximately S\n\
                            (default 1) seconds\n\
+"), stdout);
+     fputs (_("\
   -v, --verbose            always output headers giving file names\n\
       --help               display this help and exit\n\
       --version            output version information and exit\n\
 \n\
-"),
-	      DEFAULT_N_LINES, DEFAULT_N_LINES,
-	      DEFAULT_MAX_N_UNCHANGED_STATS_BETWEEN_OPENS
-	      );
-      printf (_("\
+"), stdout);
+     fputs (_("\
 If the first character of N (the number of bytes or lines) is a `+',\n\
 print beginning with the Nth item from the start of each file, otherwise,\n\
 print the last N items in the file.  N may have a multiplier suffix:\n\
 b for 512, k for 1024, m for 1048576 (1 Meg).  A first OPTION of -VALUE\n\
 or +VALUE is treated like -n VALUE or -n +VALUE unless VALUE has one of\n\
 the [bkm] suffix multipliers, in which case it is treated like -c VALUE\n\
+"), stdout);
+     fputs (_("\
 or -c +VALUE.  Warning: a first option of +VALUE is obsolescent, and support\n\
 for it will be withdrawn.\n\
 \n\
 With --follow (-f), tail defaults to following the file descriptor, which\n\
 means that even if a tail'ed file is renamed, tail will continue to track\n\
 its end.  This default behavior is not desirable when you really want to\n\
+"), stdout);
+     fputs (_("\
 track the actual name of the file, not the file descriptor (e.g., log\n\
 rotation).  Use --follow=name in that case.  That causes tail to track the\n\
 named file by reopening it periodically to see if it has been removed and\n\
 recreated by some other program.\n\
 \n\
-"));
+"), stdout);
       puts (_("\nReport bugs to <bug-textutils@gnu.org>."));
     }
   exit (status == 0 ? EXIT_SUCCESS : EXIT_FAILURE);
