6f468fedb43dff9e6966ec73d3bb61a28d227484
Jim Meyering <jim@meyering.net>
2001-11-23 18:45:32 +0000

    (main): Don't split string in the middle of sentence.

--- src/ptx.c
+++ src/ptx.c
@@ -1975,13 +1975,15 @@ it under the terms of the GNU General Public License as published by\n\
 the Free Software Foundation; either version 2, or (at your option)\n\
 any later version.\n\
 \n\
-This program is distributed in the hope that it will be useful,\n\
 "), stdout);
 	  fputs (_("\
+This program is distributed in the hope that it will be useful,\n\
 but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n\
 GNU General Public License for more details.\n\
 \n\
+"), stdout);
+	  fputs (_("\
 You should have received a copy of the GNU General Public License\n\
 along with this program; if not, write to the Free Software Foundation,\n\
 Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.\n"),
