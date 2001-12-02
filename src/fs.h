3e947ade15b48fe7f7f3229548f6c726135b0c83
Jim Meyering <jim@meyering.net>
2001-12-02 20:08:14 +0000

    *** empty log message ***

---
+++ src/fs.h
@@ -0,0 +1,27 @@
+/* define the magic numbers as given by statfs(2) */
+/* please send additions to meskes@debian.org     */
+
+#define AFFS_SUPER_MAGIC      0xADFF
+#define EXT_SUPER_MAGIC       0x137D
+#define EXT2_OLD_SUPER_MAGIC  0xEF51
+#define EXT2_SUPER_MAGIC      0xEF53
+#define HPFS_SUPER_MAGIC      0xF995E849
+#define ISOFS_SUPER_MAGIC     0x9660
+#define MINIX_SUPER_MAGIC     0x137F /* orig. minix */
+#define MINIX_SUPER_MAGIC2    0x138F /* 30 char minix */
+#define MINIX2_SUPER_MAGIC    0x2468 /* minix V2 */
+#define MINIX2_SUPER_MAGIC2   0x2478 /* minix V2, 30 char names */
+#define MSDOS_SUPER_MAGIC     0x4d44
+#define NCP_SUPER_MAGIC       0x564c
+#define NFS_SUPER_MAGIC       0x6969
+#define PROC_SUPER_MAGIC      0x9fa0
+#define SMB_SUPER_MAGIC       0x517B
+#define XENIX_SUPER_MAGIC     0x012FF7B4
+#define SYSV4_SUPER_MAGIC     0x012FF7B5
+#define SYSV2_SUPER_MAGIC     0x012FF7B6
+#define COH_SUPER_MAGIC       0x012FF7B7
+#define UFS_MAGIC             0x00011954
+#define _XIAFS_SUPER_MAGIC    0x012FD16D
+#define NTFS_SUPER_MAGIC      0x5346544e
+#define TMPFS_MAGIC	      0x1021994
+#define REISERFS_MAGIC        0x52654973
