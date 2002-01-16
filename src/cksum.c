f29617e3fe93740a859a79d6304d8b457c8d9d47
Jim Meyering <jim@meyering.net>
2002-01-16 23:01:04 +0000

    add 2002 to Copyright line

--- src/cksum.c
+++ src/cksum.c
@@ -1,5 +1,5 @@
 /* cksum -- calculate and print POSIX checksums and sizes of files
-   Copyright (C) 92, 1995-2001 Free Software Foundation, Inc.
+   Copyright (C) 92, 1995-2002 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
