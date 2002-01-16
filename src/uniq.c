a5bda3260c1977f85774e1aa69b5e0f79cfa788d
Jim Meyering <jim@meyering.net>
2002-01-16 22:59:27 +0000

    add 2002 to Copyright line

--- src/uniq.c
+++ src/uniq.c
@@ -1,5 +1,5 @@
 /* uniq -- remove duplicate lines from a sorted file
-   Copyright (C) 86, 91, 1995-2001, Free Software Foundation, Inc.
+   Copyright (C) 86, 91, 1995-2002, Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
