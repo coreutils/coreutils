b4012f613a88c938e0d4c3006ce5accae0c92e28
Jim Meyering <jim@meyering.net>
2002-01-16 22:38:26 +0000

    add 2002 to Copyright line

--- lib/strverscmp.c
+++ lib/strverscmp.c
@@ -1,5 +1,5 @@
 /* Compare strings while treating digits characters numerically.
-   Copyright (C) 1997, 2000 Free Software Foundation, Inc.
+   Copyright (C) 1997, 2000, 2002 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Jean-François Bignolles <bignolle@ecoledoc.ibp.fr>, 1997.
 
