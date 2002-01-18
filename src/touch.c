1f2f9be7f828c75e70a91f04fbe5103409703e34
Jim Meyering <jim@meyering.net>
2002-01-18 15:36:23 +0000

    (main): Issue a warning for obsolete usage, unless POSIXLY_CORRECT.

--- src/touch.c
+++ src/touch.c
@@ -364,6 +364,16 @@ main (int argc, char **argv)
       newtime = posixtime (argv[optind], PDS_TRAILING_YEAR);
       if (newtime != (time_t) -1)
 	{
+	  if (! getenv ("POSIXLY_CORRECT"))
+	    {
+	      struct tm const *tm = posixtm (argv[optind], PDS_TRAILING_YEAR);
+	      error (0, 0,
+		     _("warning: `touch %s' is obsolete; use `touch -t %04d%02d%02d%02d%02d.%02d'"),
+		     argv[optind],
+		     tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
+		     tm->tm_hour, tm->tm_min, tm->tm_sec);
+	    }
+
 	  optind++;
 	  date_set++;
 	}
