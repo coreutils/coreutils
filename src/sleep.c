a25f0cdb7e09a945cbf2ae49b5308545fda7d39c
Jim Meyering <jim@meyering.net>
2002-03-01 23:34:23 +0000

    reflect renaming: nanosleep.h -> timespec.h

--- src/sleep.c
+++ src/sleep.c
@@ -1,5 +1,5 @@
 /* sleep - delay for a specified amount of time.
-   Copyright (C) 84, 1991-1997, 1999-2001 Free Software Foundation, Inc.
+   Copyright (C) 84, 1991-1997, 1999-2002 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -34,7 +34,7 @@
 #include "system.h"
 #include "error.h"
 #include "long-options.h"
-#include "nanosleep.h"
+#include "timespec.h"
 #include "xstrtod.h"
 #include "closeout.h"
 
