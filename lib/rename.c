db8d2fe339324ce223b2e3a24d1fdd82a1960e37
Jim Meyering <jim@meyering.net>
2002-03-08 08:23:47 +0000

    Mention that this wrapper is needed also on mips-dec-ultrix4.4 systems.

--- lib/rename.c
+++ lib/rename.c
@@ -1,6 +1,7 @@
 /* Work around the bug in some systems whereby rename fails when the source
-   path has a trailing slash.  The rename from SunOS 4.1.1_U1 has this bug.
-   Copyright (C) 2001 Free Software Foundation, Inc.
+   path has a trailing slash.  The rename functions of SunOS 4.1.1_U1 and
+   mips-dec-ultrix4.4 have this bug.
+   Copyright (C) 2001, 2002 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
