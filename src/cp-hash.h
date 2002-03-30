baa4aff63326a7f297828996aeaeb8152f159929
Jim Meyering <jim@meyering.net>
2002-03-30 07:11:24 +0000

    (forget_created): Prototype.

--- src/cp-hash.h
+++ src/cp-hash.h
@@ -1,4 +1,5 @@
 void hash_init PARAMS ((void));
 void forget_all PARAMS ((void));
+void forget_created PARAMS ((ino_t ino, dev_t dev));
 char *remember_copied PARAMS ((const char *node, ino_t ino, dev_t dev));
 int remember_created PARAMS ((const char *path));
