c2e4117240f098c12767feab3fae708aebbd96ec
Jim Meyering <jim@meyering.net>
2002-05-22 08:39:30 +0000

    Include "file-type.h", etc.

--- lib/file-type.c
+++ lib/file-type.c
@@ -1,3 +1,31 @@
+/* return a string describing the type of a file
+   Copyright (C) 2002 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
+/* Extracted from diffutils/src/diff.c.  */
+
+#include "file-type.h"
+
+#if ENABLE_NLS
+# include <libintl.h>
+# define _(text) gettext (text)
+#else
+# define _(text) text
+#endif
+
 char const *
 file_type (struct stat const *st)
 {
