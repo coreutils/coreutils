cb8155615ad8a58758ca7a086ac269eebb9e87a0
Jim Meyering <jim@meyering.net>
2002-06-15 07:36:57 +0000

    (S_ISLNK): Define to 0 if neither S_ISLNK nor S_IFLNK is defined.

--- lib/lchown.c
+++ lib/lchown.c
@@ -1,5 +1,5 @@
 /* Provide a stub lchown function for systems that lack it.
-   Copyright (C) 1998, 1999 Free Software Foundation, Inc.
+   Copyright (C) 1998, 1999, 2002 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -30,8 +30,13 @@ extern int errno;
 #ifdef STAT_MACROS_BROKEN
 # undef S_ISLNK
 #endif
-#if !defined(S_ISLNK) && defined(S_IFLNK)
-# define S_ISLNK(m) (((m) & S_IFMT) == S_IFLNK)
+
+#ifndef S_ISLNK
+# ifdef S_IFLNK
+#  define S_ISLNK(m) (((m) & S_IFMT) == S_IFLNK)
+# else
+#  define S_ISLNK(m) 0
+# endif
 #endif
 
 /* Declare chown to avoid a warning.  Don't include unistd.h,
