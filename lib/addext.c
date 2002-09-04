de684695820a2c43fc6f1e5f2f3aea37c49a9eec
Jim Meyering <jim@meyering.net>
2002-09-04 07:53:42 +0000

    (addext): Add a cast to avoid gcc's warning about `comparison between signed and unsigned'.

--- lib/addext.c
+++ lib/addext.c
@@ -1,5 +1,5 @@
 /* addext.c -- add an extension to a file name
-   Copyright 1990, 1997, 1998, 1999, 2001 Free Software Foundation, Inc.
+   Copyright 1990, 1997, 1998, 1999, 2001, 2002 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -83,7 +83,7 @@ addext (char *filename, char const *ext, int e)
 	  *s = c;
 	}
       if (0 <= name_max || errno == 0)
-	slen_max = name_max == (size_t) name_max ? name_max : -1;
+	slen_max = name_max == (long) (size_t) name_max ? name_max : -1;
     }
 #endif
 
