17bbb9bcb17642ba35fcdec3e01c5e5b37e1f55b
Jim Meyering <jim@meyering.net>
2002-11-22 13:23:52 +0000

    From gnulib: Don't include <sys/types.h>.  It's unnecessary.

--- lib/strstr.c
+++ lib/strstr.c
@@ -31,7 +31,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 #if defined _LIBC || defined HAVE_STRING_H
 # include <string.h>
 #endif
-#include <sys/types.h>
 
 typedef unsigned chartype;
 
