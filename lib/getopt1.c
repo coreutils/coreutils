4d314289c3454813475bb033426834f9d875e55e
Jim Meyering <jim@meyering.net>
2002-11-24 10:01:15 +0000

    Remove 2002 from copyright line, to sync with gnulib.

--- lib/getopt1.c
+++ lib/getopt1.c
@@ -1,5 +1,5 @@
 /* getopt_long and getopt_long_only entry points for GNU getopt.
-   Copyright (C) 1987,88,89,90,91,92,93,94,96,97,98, 02
+   Copyright (C) 1987,88,89,90,91,92,93,94,96,97,98
      Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
