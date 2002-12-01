0937f50e9e193bba8e981d48741a9ce0a05f51f4
Jim Meyering <jim@meyering.net>
2002-12-01 12:10:41 +0000

    comment tweak

--- lib/long-options.c
+++ lib/long-options.c
@@ -1,5 +1,5 @@
 /* Utility to accept --help and --version options as unobtrusively as possible.
-   Copyright (C) 1993, 1994, 1998-2000, 2002 Free Software Foundation, Inc.
+   Copyright (C) 1993-1994, 1998-1999, 2000, 2002 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
