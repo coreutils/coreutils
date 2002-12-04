16c87869c68673af9ed8afc1046103778b2356f6
Jim Meyering <jim@meyering.net>
2002-12-04 08:55:54 +0000

    Simply #define LSTAT and include stat.c.

---
+++ lib/lstat.c
@@ -0,0 +1,23 @@
+/* Work around the bug in some systems whereby lstat succeeds when
+   given the zero-length file name argument.  The lstat from SunOS4.1.4
+   has this bug.
+   Copyright (C) 1997-2002 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
+/* written by Jim Meyering */
+
+#define LSTAT
+#include "stat.c"
