f2cb00a54a67e2e95fbeed4091204386797a899d
Jim Meyering <jim@meyering.net>
2002-12-04 08:56:58 +0000

    author hardly matters here :-)

--- lib/lstat.c
+++ lib/lstat.c
@@ -17,7 +17,5 @@
    along with this program; if not, write to the Free Software Foundation,
    Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
-/* written by Jim Meyering */
-
 #define LSTAT
 #include "stat.c"
