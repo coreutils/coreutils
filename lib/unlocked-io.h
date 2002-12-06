76fd0dbc453f4d23dc09ee383077cb5b58d9d81a
Jim Meyering <jim@meyering.net>
2002-12-06 13:37:11 +0000

    New file, but with proper copyright notice and attribution.  Note: this is *not* the same as the file in gnulib. This uses #if HAVE_FUNC_UNLOCKED for each `FUNC', rather than HAVE_DECL_FUNC_UNLOCKED.  This usage is consistent with the autoconf macro in ../m4/jm-glibc-io.m4.  Modulo comments, this file still contains exactly what was generated by gen-uio.

--- lib/unlocked-io.h
+++ lib/unlocked-io.h
@@ -1,3 +1,24 @@
+/* Prefer faster, non-thread-safe stdio functions if available.
+
+   Copyright (C) 2001, 2002 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+   USA.  */
+
+/* Written by Jim Meyering.  */
+
 #ifndef UNLOCKED_IO_H
 # define UNLOCKED_IO_H 1
 
