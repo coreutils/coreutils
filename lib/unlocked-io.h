839df841d347680bf0d79cda20f78e57b3471ddf
Jim Meyering <jim@meyering.net>
2002-12-11 07:21:42 +0000

    Sync from gnulib.

--- lib/unlocked-io.h
+++ lib/unlocked-io.h
@@ -33,55 +33,55 @@
    more efficient but not thread-safe.  That they're not thread-safe is
    fine since all of the applications in this package are single threaded.  */
 
-#  if HAVE_CLEARERR_UNLOCKED
+#  if HAVE_DECL_CLEARERR_UNLOCKED
 #   undef clearerr
 #   define clearerr(x) clearerr_unlocked (x)
 #  endif
-#  if HAVE_FEOF_UNLOCKED
+#  if HAVE_DECL_FEOF_UNLOCKED
 #   undef feof
 #   define feof(x) feof_unlocked (x)
 #  endif
-#  if HAVE_FERROR_UNLOCKED
+#  if HAVE_DECL_FERROR_UNLOCKED
 #   undef ferror
 #   define ferror(x) ferror_unlocked (x)
 #  endif
-#  if HAVE_FFLUSH_UNLOCKED
+#  if HAVE_DECL_FFLUSH_UNLOCKED
 #   undef fflush
 #   define fflush(x) fflush_unlocked (x)
 #  endif
-#  if HAVE_FGETS_UNLOCKED
+#  if HAVE_DECL_FGETS_UNLOCKED
 #   undef fgets
 #   define fgets(x,y,z) fgets_unlocked (x,y,z)
 #  endif
-#  if HAVE_FPUTC_UNLOCKED
+#  if HAVE_DECL_FPUTC_UNLOCKED
 #   undef fputc
 #   define fputc(x,y) fputc_unlocked (x,y)
 #  endif
-#  if HAVE_FPUTS_UNLOCKED
+#  if HAVE_DECL_FPUTS_UNLOCKED
 #   undef fputs
 #   define fputs(x,y) fputs_unlocked (x,y)
 #  endif
-#  if HAVE_FREAD_UNLOCKED
+#  if HAVE_DECL_FREAD_UNLOCKED
 #   undef fread
 #   define fread(w,x,y,z) fread_unlocked (w,x,y,z)
 #  endif
-#  if HAVE_FWRITE_UNLOCKED
+#  if HAVE_DECL_FWRITE_UNLOCKED
 #   undef fwrite
 #   define fwrite(w,x,y,z) fwrite_unlocked (w,x,y,z)
 #  endif
-#  if HAVE_GETC_UNLOCKED
+#  if HAVE_DECL_GETC_UNLOCKED
 #   undef getc
 #   define getc(x) getc_unlocked (x)
 #  endif
-#  if HAVE_GETCHAR_UNLOCKED
+#  if HAVE_DECL_GETCHAR_UNLOCKED
 #   undef getchar
 #   define getchar() getchar_unlocked ()
 #  endif
-#  if HAVE_PUTC_UNLOCKED
+#  if HAVE_DECL_PUTC_UNLOCKED
 #   undef putc
 #   define putc(x,y) putc_unlocked (x,y)
 #  endif
-#  if HAVE_PUTCHAR_UNLOCKED
+#  if HAVE_DECL_PUTCHAR_UNLOCKED
 #   undef putchar
 #   define putchar(x) putchar_unlocked (x)
 #  endif
