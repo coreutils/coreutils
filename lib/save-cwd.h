9805d6c5cca7df0397c8545cc00064010f91488d
Jim Meyering <jim@meyering.net>
2003-01-10 22:46:11 +0000

    Remove uses of PARAMS.

--- lib/save-cwd.h
+++ lib/save-cwd.h
@@ -7,17 +7,9 @@ struct saved_cwd
     char *name;
   };
 
-# ifndef PARAMS
-#  if defined PROTOTYPES || (defined __STDC__ && __STDC__)
-#   define PARAMS(Args) Args
-#  else
-#   define PARAMS(Args) ()
-#  endif
-# endif
-
-int save_cwd PARAMS ((struct saved_cwd *cwd));
-int restore_cwd PARAMS ((const struct saved_cwd *cwd, const char *dest,
-			 const char *from));
-void free_cwd PARAMS ((struct saved_cwd *cwd));
+int save_cwd (struct saved_cwd *cwd);
+int restore_cwd (const struct saved_cwd *cwd, const char *dest,
+		 const char *from);
+void free_cwd (struct saved_cwd *cwd);
 
 #endif /* SAVE_CWD_H */
