e58f9df1b3d7da93ad1721246bc24c3fad43f0a6
Jim Meyering <jim@meyering.net>
2003-02-15 22:38:32 +0000

    add an assertion

--- lib/mmap-stack.c
+++ lib/mmap-stack.c
@@ -1,8 +1,29 @@
+/* Run with a larger (mmap'd) stack.
+
+   Copyright (C) 2003 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
+/* Written by Jim Meyering.  */
+
 #if HAVE_CONFIG_H
 # include <config.h>
 #endif
 
 #include <stdio.h>
+#include <assert.h>
 #include <unistd.h>
 #include <stdarg.h>
 #if HAVE_SYS_MMAN_H
@@ -68,6 +89,7 @@ run_on_mmaped_stack (void (*func) (void), size_t argc, ...)
     unsigned i;
     va_list ap;
 
+    assert (argc <= ARGC_MAX);
     va_start (ap, argc);
     for (i = 0; i < argc; i++)
       bs_argv[i] = va_arg (ap, long);
