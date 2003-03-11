c7cb614727bf97017bf34af03bdeee5e70b486e7
Jim Meyering <jim@meyering.net>
2003-03-11 10:07:17 +0000

    (EXIT_FAILURE): Define if the system doesn't. Reported by Bruce Becker; see: http://mail.gnu.org/archive/html/bug-bison/2003-03/msg00017.html

--- lib/argmatch.c
+++ lib/argmatch.c
@@ -1,5 +1,7 @@
 /* argmatch.c -- find a match for a string in an array
-   Copyright (C) 1990, 1998, 1999, 2001, 2002 Free Software Foundation, Inc.
+
+   Copyright (C) 1990, 1998, 1999, 2001, 2002, 2003 Free Software
+   Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -42,6 +44,10 @@
    literal_quoting_style.  */
 #ifndef ARGMATCH_QUOTING_STYLE
 # define ARGMATCH_QUOTING_STYLE locale_quoting_style
+#endif
+
+#ifndef EXIT_FAILURE
+# define EXIT_FAILURE 1
 #endif
 
 /* Non failing version of argmatch call this function after failing. */
