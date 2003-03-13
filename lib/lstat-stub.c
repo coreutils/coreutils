30593686a860c3021d64521927e6bbea7c3b2a05
Jim Meyering <jim@meyering.net>
2003-03-13 13:07:11 +0000

    .

---
+++ lib/lstat-stub.c
@@ -0,0 +1,12 @@
+#include <config.h>
+#include <sys/stat.h>
+
+/* A trivial substitute for `lstat'.
+
+   DJGPP 2.03 and earlier don't have `lstat' and don't support
+   symlinks. */
+
+int lstat (const char *fname, struct stat *st_buf)
+{
+  return stat (fname, st_buf);
+}
---
+++ lib/readlink-stub.c
@@ -0,0 +1,14 @@
+#include <config.h>
+#include <stddef.h>
+#include <errno.h>
+
+/* A trivial substitute for `readlink'.
+
+   DJGPP 2.03 and earlier don't have `readlink' and don't support
+   symlinks. */
+
+int readlink (const char *filename, char *buffer, size_t size)
+{
+  errno = EINVAL;
+  return -1;
+}
