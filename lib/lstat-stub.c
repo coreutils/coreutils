f3ad658b9883e69c6b4e4d7de12b2fd618ef664a
Jim Meyering <jim@meyering.net>
2003-03-17 19:21:13 +0000

    (lstat): Put function type on its own line.

--- lib/lstat-stub.c
+++ lib/lstat-stub.c
@@ -6,7 +6,8 @@
    DJGPP 2.03 and earlier don't have `lstat' and don't support
    symlinks. */
 
-int lstat (const char *fname, struct stat *st_buf)
+int
+lstat (const char *fname, struct stat *st_buf)
 {
   return stat (fname, st_buf);
 }
