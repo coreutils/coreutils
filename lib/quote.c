837fc9bfa58d3af4d8d58c133e8d9611d0bfdea6
Jim Meyering <jim@meyering.net>
2003-03-18 08:49:12 +0000

    (quote_n): Fix typo in comment.

--- lib/quote.c
+++ lib/quote.c
@@ -1,5 +1,5 @@
 /* quote.c - quote arguments for output
-   Copyright (C) 1998, 1999, 2000, 2001 Free Software Foundation, Inc.
+   Copyright (C) 1998, 1999, 2000, 2001, 2003 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -28,16 +28,16 @@
 #include "quotearg.h"
 #include "quote.h"
 
-/* Return an unambiguous printable representated, allocated in slot N,
-   for NAME, suitable for diagnostics.  */
+/* Return an unambiguous printable representation of NAME,
+   allocated in slot N, suitable for diagnostics.  */
 char const *
 quote_n (int n, char const *name)
 {
   return quotearg_n_style (n, locale_quoting_style, name);
 }
 
-/* Return an unambiguous printable representation of NAME, suitable
-   for diagnostics.  */
+/* Return an unambiguous printable representation of NAME,
+   suitable for diagnostics.  */
 char const *
 quote (char const *name)
 {
