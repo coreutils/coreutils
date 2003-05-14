dc7b620a314756c1cfa1e9152e528cda63e586b9
Jim Meyering <jim@meyering.net>
2003-05-14 09:13:26 +0000

    (usage): Don't use `,' as the thousands separator in e.g. 1,000,000 and 1,048,576.  Instead, do this: `SIZE may be ..., MB 1000*1000, M 1024*1024 and so on...'

--- src/du.c
+++ src/du.c
@@ -214,7 +214,7 @@ Mandatory arguments to long options are mandatory for short options too.\n\
       fputs (VERSION_OPTION_DESCRIPTION, stdout);
       fputs (_("\n\
 SIZE may be (or may be an integer optionally followed by) one of following:\n\
-kB 1000, K 1024, MB 1,000,000, M 1,048,576, and so on for G, T, P, E, Z, Y.\n\
+kB 1000, K 1024, MB 1000*1000, M 1024*1024, and so on for G, T, P, E, Z, Y.\n\
 "), stdout);
       printf (_("\nReport bugs to <%s>.\n"), PACKAGE_BUGREPORT);
     }
