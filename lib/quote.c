361f358a64b9bca638d77bb1b44a0cfaa43ae182
Jim Meyering <jim@meyering.net>
2003-06-06 20:45:46 +0000

    Dont include <stddef.h> or <sys/types.h>; not needed.

--- lib/quote.c
+++ lib/quote.c
@@ -21,10 +21,6 @@
 # include <config.h>
 #endif
 
-#if HAVE_STDDEF_H
-# include <stddef.h>  /* For the definition of size_t on windows w/MSVC.  */
-#endif
-#include <sys/types.h>
 #include "quotearg.h"
 #include "quote.h"
 
