199e0abbe0270d9f1adda0f64940d81acc8219c9
Jim Meyering <jim@meyering.net>
2003-06-06 20:08:58 +0000

    Don't include <sys/types.h>.  From gnulib.

--- lib/memcasecmp.c
+++ lib/memcasecmp.c
@@ -1,5 +1,5 @@
 /* Case-insensitive buffer comparator.
-   Copyright (C) 1996, 1997, 2000 Free Software Foundation, Inc.
+   Copyright (C) 1996, 1997, 2000, 2003 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -21,7 +21,6 @@
 # include <config.h>
 #endif
 
-#include <sys/types.h>
 #include <ctype.h>
 
 #if defined (STDC_HEADERS) || (!defined (isascii) && !defined (HAVE_ISASCII))
