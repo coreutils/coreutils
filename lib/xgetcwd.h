2c8b39745bb06c678c1d488bb2157e0f5c1921a0
Jim Meyering <jim@meyering.net>
2003-06-07 10:09:18 +0000

    add copyright years from gnulib

--- lib/xgetcwd.h
+++ lib/xgetcwd.h
@@ -1,5 +1,5 @@
 /* prototype for xgetcwd
-   Copyright (C) 2003 Free Software Foundation, Inc.
+   Copyright (C) 1995, 2001, 2003 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
