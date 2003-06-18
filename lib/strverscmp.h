b289f42fdf31012933729c0372403e78f3276bfc
Jim Meyering <jim@meyering.net>
2003-06-18 07:53:12 +0000

    (PARAMS): Remove.  All uses removed. Do not include config.h; no longer needed. Anyway, config.h should always be included before any other file.

--- lib/strverscmp.h
+++ lib/strverscmp.h
@@ -3,18 +3,6 @@
 #ifndef STRVERSCMP_H_
 # define STRVERSCMP_H_
 
-# if HAVE_CONFIG_H
-#  include <config.h>
-# endif
-
-# ifndef PARAMS
-#  if defined PROTOTYPES || (defined __STDC__ && __STDC__)
-#   define PARAMS(Args) Args
-#  else
-#   define PARAMS(Args) ()
-#  endif
-# endif
-
-int strverscmp PARAMS ((const char*, const char*));
+int strverscmp (const char *, const char *);
 
 #endif /* not STRVERSCMP_H_ */
