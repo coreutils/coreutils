62b9c742e911c9da01fe289f0452dc67d68d69dc
Jim Meyering <jim@meyering.net>
2003-06-18 06:11:45 +0000

    Update from gnulib.

--- lib/xstrtol.h
+++ lib/xstrtol.h
@@ -1,5 +1,7 @@
 /* A more useful interface to strtol.
-   Copyright 1995, 1996, 1998, 1999, 2001 Free Software Foundation, Inc.
+
+   Copyright (C) 1995, 1996, 1998, 1999, 2001, 2002, 2003 Free
+   Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -18,17 +20,12 @@
 #ifndef XSTRTOL_H_
 # define XSTRTOL_H_ 1
 
+/* Get uintmax_t.  */
 # if HAVE_INTTYPES_H
-#  include <inttypes.h> /* for uintmax_t */
-# elif HAVE_STDINT_H
-#  include <stdint.h>
-# endif
-
-# ifndef PARAMS
-#  if defined PROTOTYPES || (defined __STDC__ && __STDC__)
-#   define PARAMS(Args) Args
-#  else
-#   define PARAMS(Args) ()
+#  include <inttypes.h>
+# else
+#  if HAVE_STDINT_H
+#   include <stdint.h>
 #  endif
 # endif
 
@@ -41,9 +38,7 @@ typedef enum strtol_error strtol_error;
 # endif
 
 # define _DECLARE_XSTRTOL(name, type) \
-  strtol_error \
-    name PARAMS ((const char *s, char **ptr, int base, \
-		  type *val, const char *valid_suffixes));
+  strtol_error name (const char *, char **, int, type *, const char *);
 _DECLARE_XSTRTOL (xstrtol, long int)
 _DECLARE_XSTRTOL (xstrtoul, unsigned long int)
 _DECLARE_XSTRTOL (xstrtoimax, intmax_t)
