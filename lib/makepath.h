33a46de4adc4988b90a43d657d037120a6953c32
Jim Meyering <jim@meyering.net>
2003-06-18 07:51:52 +0000

    (PARAMS): Remove.  All uses removed.

--- lib/makepath.h
+++ lib/makepath.h
@@ -1,22 +1,12 @@
-#ifndef PARAMS
-# if defined PROTOTYPES || (defined __STDC__ && __STDC__)
-#  define PARAMS(Args) Args
-# else
-#  define PARAMS(Args) ()
-# endif
-#endif
+int make_path (const char *_argpath,
+	       int _mode,
+	       int _parent_mode,
+	       uid_t _owner,
+	       gid_t _group,
+	       int _preserve_existing,
+	       const char *_verbose_fmt_string);
 
-int
-make_path PARAMS ((const char *_argpath,
-		   int _mode,
-		   int _parent_mode,
-		   uid_t _owner,
-		   gid_t _group,
-		   int _preserve_existing,
-		   const char *_verbose_fmt_string));
-
-int
-make_dir PARAMS ((const char *dir,
-		  const char *dirpath,
-		  mode_t mode,
-		  int *created_dir_p));
+int make_dir (const char *dir,
+	      const char *dirpath,
+	      mode_t mode,
+	      int *created_dir_p);
