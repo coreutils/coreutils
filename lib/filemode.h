3ed2170b6430b2a421dd6411504ae983a4fac65a
Jim Meyering <jim@meyering.net>
2003-06-18 07:48:30 +0000

    (PARAMS): Remove.  All uses removed. Do not include config.h; no longer needed. Anyway, config.h should always be included before any other file.

--- lib/filemode.h
+++ lib/filemode.h
@@ -1,19 +1,7 @@
 #ifndef FILEMODE_H_
 
-# if HAVE_CONFIG_H
-#  include <config.h>
-# endif
-
 # include <sys/types.h>
 
-# ifndef PARAMS
-#  if defined PROTOTYPES || (defined __STDC__ && __STDC__)
-#   define PARAMS(Args) Args
-#  else
-#   define PARAMS(Args) ()
-#  endif
-# endif
-
-void mode_string PARAMS ((mode_t mode, char *str));
+void mode_string (mode_t mode, char *str);
 
 #endif
