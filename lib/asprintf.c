b269489ba15f4921e22895df571763d13e529fa2
Jim Meyering <jim@meyering.net>
2003-07-10 11:15:20 +0000

    Now that a program (`who') uses asprintf, we need all of these: * asnprintf.c, asprintf.c, printf-args.c, printf-args.h, printf-parse.c: * printf-parse.h, vasnprintf.c, vasnprintf.h, vasprintf.c, vasprintf.h: New files, from gnulib.

---
+++ lib/asprintf.c
@@ -0,0 +1,38 @@
+/* Formatted output to strings.
+   Copyright (C) 1999, 2002 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+   USA.  */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+/* Specification.  */
+#include "vasprintf.h"
+
+#include <stdarg.h>
+
+int
+asprintf (char **resultp, const char *format, ...)
+{
+  va_list args;
+  int result;
+
+  va_start (args, format);
+  result = vasprintf (resultp, format, args);
+  va_end (args);
+  return result;
+}
--- lib/vasprintf.h
+++ lib/vasprintf.h
@@ -17,34 +17,34 @@
    USA.  */
 
 #ifndef _VASPRINTF_H
-# define _VASPRINTF_H
+#define _VASPRINTF_H
 
-# if HAVE_VASPRINTF
+#if HAVE_VASPRINTF
 
 /* Get asprintf(), vasprintf() declarations.  */
-#  include <stdio.h>
+#include <stdio.h>
 
-# else
+#else
 
 /* Get va_list.  */
-#  include <stdarg.h>
+#include <stdarg.h>
 
-#  ifndef __attribute__
+#ifndef __attribute__
 /* This feature is available in gcc versions 2.5 and later.  */
-#   if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 5) || __STRICT_ANSI__
-#    define __attribute__(Spec) /* empty */
-#   endif
+# if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 5) || __STRICT_ANSI__
+#  define __attribute__(Spec) /* empty */
+# endif
 /* The __-protected variants of `format' and `printf' attributes
    are accepted by gcc versions 2.6.4 (effectively 2.7) and later.  */
-#   if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 7)
-#    define __format__ format
-#    define __printf__ printf
-#   endif
-#  endif
+# if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 7)
+#  define __format__ format
+#  define __printf__ printf
+# endif
+#endif
 
-#  ifdef	__cplusplus
+#ifdef	__cplusplus
 extern "C" {
-#  endif
+#endif
 
 /* Write formatted output to a string dynamically allocated with malloc().
    If the memory allocation succeeds, store the address of the string in
@@ -55,10 +55,10 @@ extern int asprintf (char **result, const char *format, ...)
 extern int vasprintf (char **result, const char *format, va_list args)
        __attribute__ ((__format__ (__printf__, 2, 0)));
 
-#  ifdef	__cplusplus
+#ifdef	__cplusplus
 }
-#  endif
+#endif
 
-# endif
+#endif
 
 #endif /* _VASPRINTF_H */
