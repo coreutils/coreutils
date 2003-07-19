2925e558cb9acb957a7f3695cc8bf4ebab98dcb2
Jim Meyering <jim@meyering.net>
2003-07-19 08:05:44 +0000

    (close_stdout_set_status, close_stdout_status): Remove.

--- lib/closeout.h
+++ lib/closeout.h
@@ -19,9 +19,7 @@
 #ifndef CLOSEOUT_H
 # define CLOSEOUT_H 1
 
-void close_stdout_set_status (int status);
 void close_stdout_set_file_name (const char *file);
 void close_stdout (void);
-void close_stdout_status (int status);
 
 #endif
