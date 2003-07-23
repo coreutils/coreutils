ee6abf82dddfecf6fb4ebe3a9e5650441de0c101
Jim Meyering <jim@meyering.net>
2003-07-23 06:14:20 +0000

    Factored out of dircolors.c from coreutils.

---
+++ lib/xstrndup.c
@@ -0,0 +1,38 @@
+/* A wrapper around xstrndup.
+
+   Copyright (C) 2003 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
+/* Written by Jim Meyering. */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <stddef.h>
+
+#include "xalloc.h"
+
+char *strndup (char const *, size_t);
+
+char *
+xstrndup (char const *s, size_t n)
+{
+  char *new = strndup (s, n);
+  if (new == NULL)
+    xalloc_die ();
+  return (char *) new;
+}
