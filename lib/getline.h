08fd4795f3e0c84bf5fc3164818720d8597aa622
Jim Meyering <jim@meyering.net>
2003-08-07 09:25:26 +0000

    Merge from gnulib.

--- lib/getline.h
+++ lib/getline.h
@@ -1,4 +1,7 @@
-/*  Copyright (C) 1995, 1997, 1999, 2000-2002 Free Software Foundation, Inc.
+/* Replacement for GNU C library function getline
+
+   Copyright (C) 1995, 1997, 1999, 2000, 2001, 2002, 2003 Free
+   Software Foundation, Inc.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -20,12 +23,16 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 # include <stddef.h>
 # include <stdio.h>
 
+/* Get ssize_t.  */
+# include <sys/types.h>
+
 /* glibc2 has these functions declared in <stdio.h>.  Avoid redeclarations.  */
 # if __GLIBC__ < 2
 
-int getline (char **_lineptr, size_t *_n, FILE *_stream);
+extern ssize_t getline (char **_lineptr, size_t *_linesize, FILE *_stream);
 
-int getdelim (char **_lineptr, size_t *_n, int _delimiter, FILE *_stream);
+extern ssize_t getdelim (char **_lineptr, size_t *_linesize, int _delimiter,
+			  FILE *_stream);
 
 # endif
 
