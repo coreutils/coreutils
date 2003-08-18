6e0aaab30ecae8de26e19520919072f435cba754
Jim Meyering <jim@meyering.net>
2003-08-18 08:58:27 +0000

    Sync with gnulib.

--- lib/setenv.h
+++ lib/setenv.h
@@ -1,17 +1,46 @@
-#ifndef SETENV_H
-# define SETENV_H 1
+/* Setting environment variables.
+   Copyright (C) 2001-2003 Free Software Foundation, Inc.
 
-# undef __P
-# if defined (__STDC__) && __STDC__
-#  define	__P(x) x
-# else
-#  define	__P(x) ()
-# endif
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
 
-int
-  setenv __P ((const char *name, const char *value, int replace));
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-void
-  unsetenv __P ((const char *name));
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
-#endif /* SETENV_H */
+#if HAVE_SETENV || HAVE_UNSETENV
+
+/* Get setenv(), unsetenv() declarations.  */
+# include <stdlib.h>
+
+#endif
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#if !HAVE_SETENV
+
+/* Set NAME to VALUE in the environment.
+   If REPLACE is nonzero, overwrite an existing value.  */
+extern int setenv (const char *name, const char *value, int replace);
+
+#endif
+
+#if !HAVE_UNSETENV
+
+/* Remove the variable NAME from the environment.  */
+extern int unsetenv (const char *name);
+
+#endif
+
+#ifdef	__cplusplus
+}
+#endif
