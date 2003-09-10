02889673a1e6864e27558954f32d8759a860b43c
Jim Meyering <jim@meyering.net>
2003-09-10 08:27:55 +0000

    (atexit): Define using a prototype.

--- lib/atexit.c
+++ lib/atexit.c
@@ -4,8 +4,7 @@
 #include "config.h"
 
 int
-atexit(f)
-     void (*f)();
+atexit (void (*f) (void))
 {
   /* If the system doesn't provide a definition for atexit, use on_exit
      if the system provides that.  */
