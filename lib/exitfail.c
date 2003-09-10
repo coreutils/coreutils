74916e862824f1fb31b4ff521036f51e2bdb3c91
Jim Meyering <jim@meyering.net>
2003-09-10 08:31:57 +0000

    Include <stdlib.h> unconditionally.

--- lib/exitfail.c
+++ lib/exitfail.c
@@ -1,6 +1,6 @@
 /* Failure exit status
 
-   Copyright (C) 2002 Free Software Foundation, Inc.
+   Copyright (C) 2002, 2003 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -21,9 +21,7 @@
 # include <config.h>
 #endif
 
-#if HAVE_STDLIB_H
-# include <stdlib.h>
-#endif
+#include <stdlib.h>
 #ifndef EXIT_FAILURE
 # define EXIT_FAILURE 1
 #endif
