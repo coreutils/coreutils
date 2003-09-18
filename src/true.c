2ca6722ca467acf9a46f84eccfc4c2f458e9dfaa
Jim Meyering <jim@meyering.net>
2003-09-18 17:38:47 +0000

    (main): Append NULL to version_etc argument list.

--- src/true.c
+++ src/true.c
@@ -63,7 +63,7 @@ main (int argc, char **argv)
 	usage (EXIT_SUCCESS);
 
       if (STREQ (argv[1], "--version"))
-	version_etc (stdout, PROGRAM_NAME, GNU_PACKAGE, VERSION, AUTHORS);
+	version_etc (stdout, PROGRAM_NAME, GNU_PACKAGE, VERSION, AUTHORS, NULL);
     }
 
   exit (EXIT_SUCCESS);
