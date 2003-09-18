0ed744a035771785651d7ce08880ea8ed54860d3
Jim Meyering <jim@meyering.net>
2003-09-18 18:10:22 +0000

    (version_etc_va): Declare it.

--- lib/version-etc.h
+++ lib/version-etc.h
@@ -24,8 +24,12 @@
 
 extern char *version_etc_copyright;
 
+void version_etc_va (FILE *stream,
+		     char const *command_name, char const *package,
+		     char const *version, va_list authors);
+
 void version_etc (FILE *stream,
-		  const char *command_name, const char *package,
-		  const char *version, const char *authors);
+		  char const *command_name, char const *package,
+		  char const *version, ...);
 
 #endif /* VERSION_ETC_H */
