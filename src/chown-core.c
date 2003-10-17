1bac4198f278c21ebcd13fc6e9a30ffff5b9e760
Jim Meyering <jim@meyering.net>
2003-10-17 10:49:38 +0000

    tweak comment

--- src/chown-core.c
+++ src/chown-core.c
@@ -289,10 +289,10 @@ chown_files (char **files, int bit_flags,
   if (fts == NULL)
     {
       /* This can fail in three ways: out of memory, invalid bit_flags,
-	 and one of the FILES is an empty string.
-	 We could try to decipher that errno==EINVAL means invalid
-	 bit_flags and errno==ENOENT, but that seems wrong.  Ideally
-	 fts_open would return a proper error indicator.  For now,
+	 and one or more of the FILES is an empty string.  We could try
+	 to decipher that errno==EINVAL means invalid bit_flags and
+	 errno==ENOENT means there's an empty string, but that seems wrong.
+	 Ideally, fts_open would return a proper error indicator.  For now,
 	 we'll presume that the bit_flags are valid and just check for
 	 empty strings.  */
       bool invalid_arg = false;
