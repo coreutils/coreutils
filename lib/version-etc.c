79b9e74be5546fdefc840b388796111693b6d585
Jim Meyering <jim@meyering.net>
2003-10-18 08:10:55 +0000

    Include stdarg.h, stdlib.h. (version_etc_copyright): Declare as readonly. (version_etc_va): New function. Provide a different translatable string for each possible number of authors < 10. Abbreviate when there are 10 authors or more. (version_etc): Make this function variadic. Call version_etc_va.

--- lib/version-etc.c
+++ lib/version-etc.c
@@ -21,18 +21,134 @@
 # include <config.h>
 #endif
 
-#include <stdio.h>
-#include "unlocked-io.h"
+/* Specification.  */
 #include "version-etc.h"
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "unlocked-io.h"
 
 #include "gettext.h"
 #define _(msgid) gettext (msgid)
 
 /* Default copyright goes to the FSF. */
 
-char* version_etc_copyright =
+const char* version_etc_copyright =
   /* Do *not* mark this string for translation.  */
   "Copyright (C) 2003 Free Software Foundation, Inc.";
+
+
+/* Like version_etc, below, but with the NULL-terminated author list
+   provided via a variable of type va_list.  */
+void
+version_etc_va (FILE *stream,
+		const char *command_name, const char *package,
+		const char *version, va_list authors)
+{
+  unsigned int n_authors;
+
+  /* Count the number of authors.  */
+  {
+    va_list tmp_authors;
+
+#ifdef __va_copy
+    __va_copy (tmp_authors, authors);
+#else
+    tmp_authors = authors;
+#endif
+
+    n_authors = 0;
+    while (va_arg (tmp_authors, const char *) != NULL)
+      ++n_authors;
+  }
+
+  if (command_name)
+    fprintf (stream, "%s (%s) %s\n", command_name, package, version);
+  else
+    fprintf (stream, "%s %s\n", package, version);
+
+  switch (n_authors)
+    {
+    case 0:
+      /* The caller must provide at least one author name.  */
+      abort ();
+    case 1:
+      /* TRANSLATORS: %s denotes an author name.  */
+      vfprintf (stream, _("Written by %s.\n"), authors);
+      break;
+    case 2:
+      /* TRANSLATORS: Each %s denotes an author name.  */
+      vfprintf (stream, _("Written by %s and %s.\n"), authors);
+      break;
+    case 3:
+      /* TRANSLATORS: Each %s denotes an author name.  */
+      vfprintf (stream, _("Written by %s, %s, and %s.\n"), authors);
+      break;
+    case 4:
+      /* TRANSLATORS: Each %s denotes an author name.
+	 You can use line breaks, estimating that each author name occupies
+	 ca. 16 screen columns and that a screen line has ca. 80 columns.  */
+      vfprintf (stream, _("Written by %s, %s, %s,\nand %s.\n"), authors);
+      break;
+    case 5:
+      /* TRANSLATORS: Each %s denotes an author name.
+	 You can use line breaks, estimating that each author name occupies
+	 ca. 16 screen columns and that a screen line has ca. 80 columns.  */
+      vfprintf (stream, _("Written by %s, %s, %s,\n%s, and %s.\n"), authors);
+      break;
+    case 6:
+      /* TRANSLATORS: Each %s denotes an author name.
+	 You can use line breaks, estimating that each author name occupies
+	 ca. 16 screen columns and that a screen line has ca. 80 columns.  */
+      vfprintf (stream, _("Written by %s, %s, %s,\n%s, %s, and %s.\n"),
+		authors);
+      break;
+    case 7:
+      /* TRANSLATORS: Each %s denotes an author name.
+	 You can use line breaks, estimating that each author name occupies
+	 ca. 16 screen columns and that a screen line has ca. 80 columns.  */
+      vfprintf (stream, _("Written by %s, %s, %s,\n%s, %s, %s, and %s.\n"),
+		authors);
+      break;
+    case 8:
+      /* TRANSLATORS: Each %s denotes an author name.
+	 You can use line breaks, estimating that each author name occupies
+	 ca. 16 screen columns and that a screen line has ca. 80 columns.  */
+      vfprintf (stream, _("\
+Written by %s, %s, %s,\n%s, %s, %s, %s,\nand %s.\n"),
+		authors);
+      break;
+    case 9:
+      /* TRANSLATORS: Each %s denotes an author name.
+	 You can use line breaks, estimating that each author name occupies
+	 ca. 16 screen columns and that a screen line has ca. 80 columns.  */
+      vfprintf (stream, _("\
+Written by %s, %s, %s,\n%s, %s, %s, %s,\n%s, and %s.\n"),
+		authors);
+      break;
+    default:
+      /* 10 or more authors.  Use an abbreviation, since the human reader
+	 will probably not want to read the entire list anyway.  */
+      /* TRANSLATORS: Each %s denotes an author name.
+	 You can use line breaks, estimating that each author name occupies
+	 ca. 16 screen columns and that a screen line has ca. 80 columns.  */
+      vfprintf (stream, _("\
+Written by %s, %s, %s,\n%s, %s, %s, %s,\n%s, %s, and others.\n"),
+		authors);
+      break;
+    }
+  va_end (authors);
+  putc ('\n', stream);
+
+  fputs (version_etc_copyright, stream);
+  putc ('\n', stream);
+
+  fputs (_("\
+This is free software; see the source for copying conditions.  There is NO\n\
+warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n"),
+	 stream);
+}
 
 
 /* Display the --version information the standard way.
@@ -44,23 +160,17 @@ char* version_etc_copyright =
 
    or
 
-   COMMAND_NAME (PACKAGE) VERSION.  */
+   COMMAND_NAME (PACKAGE) VERSION.
+
+   The author names are passed as separate arguments, with an additional
+   NULL argument at the end.  */
 void
 version_etc (FILE *stream,
 	     const char *command_name, const char *package,
-	     const char *version, const char *written_by)
+	     const char *version, /* const char *author1, ...*/ ...)
 {
-  if (command_name)
-    fprintf (stream, "%s (%s) %s\n", command_name, package, version);
-  else
-    fprintf (stream, "%s %s\n", package, version);
-  fprintf (stream, "%s\n\n", written_by);
+  va_list authors;
 
-  fputs (version_etc_copyright, stream);
-  putc ('\n', stream);
-
-  fputs (_("\
-This is free software; see the source for copying conditions.  There is NO\n\
-warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n"),
-	 stream);
+  va_start (authors, version);
+  version_etc_va (stream, command_name, package, version, authors);
 }
