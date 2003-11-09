3bd82a1260fb8ad310eccd98abc5b8ca788edacf
Jim Meyering <jim@meyering.net>
2003-11-09 21:16:27 +0000

    *** empty log message ***

---
+++ lib/root-dev-ino.c
@@ -0,0 +1,39 @@
+/* root-dev-ino.c -- get the device and inode numbers for `/'.
+   Copyright (C) 2003 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
+/* Written by Jim Meyering. */
+
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <stdlib.h>
+#include "root-dev-ino.h"
+
+/* Call lstat to get the device and inode numbers for `/'.
+   Upon failure, return NULL.  Otherwise, set the members of
+   *ROOT_D_I accordingly and return ROOT_D_I.  */
+struct dev_ino *
+get_root_dev_ino (struct dev_ino *root_d_i)
+{
+  struct stat statbuf;
+  if (lstat ("/", &statbuf))
+    return NULL;
+  root_d_i->st_ino = statbuf.st_ino;
+  root_d_i->st_dev = statbuf.st_dev;
+  return root_d_i;
+}
