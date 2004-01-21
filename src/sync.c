4b5c8fe38786a34d0bb537b935f3a23bd5dead14
Jim Meyering <jim@meyering.net>
2004-01-21 23:45:53 +0000

    (usage): Use EXIT_SUCCESS, not 0, for clarity.

--- src/sync.c
+++ src/sync.c
@@ -1,5 +1,5 @@
 /* sync - update the super block
-   Copyright (C) 1994-2003 Free Software Foundation, Inc.
+   Copyright (C) 1994-2004 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -36,7 +36,7 @@ char *program_name;
 void
 usage (int status)
 {
-  if (status != 0)
+  if (status != EXIT_SUCCESS)
     fprintf (stderr, _("Try `%s --help' for more information.\n"),
 	     program_name);
   else
