ec65003050283f2d2498aa55aecda02657557bd2
Jim Meyering <jim@meyering.net>
2004-03-12 11:53:46 +0000

    (src_to_dest_lookup): Add prototype.

--- src/cp-hash.h
+++ src/cp-hash.h
@@ -3,3 +3,4 @@ void forget_all (void);
 void forget_created (ino_t ino, dev_t dev);
 char *remember_copied (const char *node, ino_t ino, dev_t dev);
 int remember_created (const char *path);
+char *src_to_dest_lookup (ino_t ino, dev_t dev);
