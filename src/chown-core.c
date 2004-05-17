c2740635cdd2318a22e82d5e3189d5f55eaafd29
Jim Meyering <jim@meyering.net>
2004-05-17 12:07:48 +0000

    (chopt_init): Affect each symlink referent by default.

--- src/chown-core.c
+++ src/chown-core.c
@@ -45,7 +45,7 @@ chopt_init (struct Chown_option *chopt)
 {
   chopt->verbosity = V_off;
   chopt->root_dev_ino = NULL;
-  chopt->affect_symlink_referent = false;
+  chopt->affect_symlink_referent = true;
   chopt->recurse = false;
   chopt->force_silent = false;
   chopt->user_name = 0;
