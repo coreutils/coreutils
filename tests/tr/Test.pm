27d6dec23d7a7b9f856ed7ac995e9483be5ea9d7
Jim Meyering <jim@meyering.net>
2004-06-02 08:50:42 +0000

    New tests bs-055, bs-at-end, repeat-Compl. Fix comment for range-a-a.

--- tests/tr/Test.pm
+++ tests/tr/Test.pm
@@ -68,7 +68,7 @@ my @tv = (
 ['y', '-d ' . q|'a-z'|, 'abc $code', ' $', 0],
 ['z', '-ds ' . q|'a-z' '$.'|, 'a.b.c $$$$code\\', '. $\\', 0],
 
-# Make sure that a-a is accepted, even though POSIX 1001.2 says it is illegal.
+# Make sure that a-a is accepted.
 ['range-a-a', q|'a-a' 'z'|,         'abc',    'zbc',               0],
 #
 ['null', q|'a' ''''|,          '',       '',                  1],
@@ -84,6 +84,8 @@ my @tv = (
 ['o-rep-2',   q|'[b*010]cd' '[a*7]BC[x*]'|, 'bcd', 'BCx', 0],
 
 ['esc',     q|'a\-z' 'A-Z'|,		'abc-z', 'AbcBC', 0],
+['bs-055', q|'a\055b' def|,		"a\055b", 'def', 0],
+['bs-at-end', q|'\' x|,		        "\\", 'x', 0],
 
 #
 # From Ross
@@ -108,6 +110,7 @@ my @tv = (
 ['repeat-0',             q|abc '[b*0]'|, 'abcd', 'bbbd', 0],
 ['repeat-000',           q|abc '[b*00000000000000000000]'|, 'abcd', 'bbbd', 0],
 ['repeat-compl', '-c ' . q|'[a*65536]\n' '[b*]'|, 'abcd', 'abbb', 0],
+['repeat-Compl', '-C ' . q|'[a*65536]\n' '[b*]'|, 'abcd', 'abbb', 0],
 
 );
 
