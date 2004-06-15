ba01709da8c69d8b8f83aef5d9df05b25c92e0aa
Jim Meyering <jim@meyering.net>
2004-06-15 18:04:13 +0000

    (main): Recognize --help, --version even if POSIXLY_CORRECT.  POSIX does not specify any options, but it does not prohibit options either, so "unlink" is like "expr" here.

--- src/unlink.c
+++ src/unlink.c
@@ -71,10 +71,8 @@ main (int argc, char **argv)
 
   atexit (close_stdout);
 
-  /* Don't recognize --help or --version if POSIXLY_CORRECT is set.  */
-  if (getenv ("POSIXLY_CORRECT") == NULL)
-    parse_long_options (argc, argv, PROGRAM_NAME, GNU_PACKAGE, VERSION,
-			usage, AUTHORS, (char const *) NULL);
+  parse_long_options (argc, argv, PROGRAM_NAME, GNU_PACKAGE, VERSION,
+		      usage, AUTHORS, (char const *) NULL);
 
   /* The above handles --help and --version.
      Since there is no other invocation of getopt, handle `--' here.  */
