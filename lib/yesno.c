5fd9b42ed30e2d05ca94c9ae335bed3b225f503b
Jim Meyering <jim@meyering.net>
2004-06-20 13:42:11 +0000

    Include "yesno.h".

--- lib/yesno.c
+++ lib/yesno.c
@@ -1,5 +1,5 @@
 /* yesno.c -- read a yes/no response from stdin
-   Copyright (C) 1990, 1998, 2001, 2003 Free Software Foundation, Inc.
+   Copyright (C) 1990, 1998, 2001, 2003, 2004 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -23,6 +23,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "unlocked-io.h"
+#include "yesno.h"
 
 /* Read one line from standard input
    and return nonzero if that line begins with y or Y,
