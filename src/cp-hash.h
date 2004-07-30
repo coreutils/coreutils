1c9f7316d24551598f1b9c4f0a5bda3f3ecfcd86
Paul Eggert <eggert@cs.ucla.edu>
2004-07-30 07:55:33 +0000

    (remember_created): Use bool when appropriate.

--- src/cp-hash.h
+++ src/cp-hash.h
@@ -2,5 +2,5 @@ void hash_init (void);
 void forget_all (void);
 void forget_created (ino_t ino, dev_t dev);
 char *remember_copied (const char *node, ino_t ino, dev_t dev);
-int remember_created (const char *path);
+bool remember_created (const char *path);
 char *src_to_dest_lookup (ino_t ino, dev_t dev);
