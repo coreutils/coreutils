f4afb6798d80629d1cc794df918c9f886deb07cc
Paul Eggert <eggert@cs.ucla.edu>
2004-08-02 22:42:37 +0000

    (enum canonicalize_mode_t): Reformat comments to fit in 80 columns.

--- lib/canonicalize.h
+++ lib/canonicalize.h
@@ -3,16 +3,21 @@
 
 enum canonicalize_mode_t
   {
-    CAN_EXISTING = 0,  /* All path components must exist. */
-    CAN_ALL_BUT_LAST = 1,  /* All path components exluding last one must exist. */
-    CAN_MISSING = 2,  /* No requirements on components existence. */
+    /* All path components must exist.  */
+    CAN_EXISTING = 0,
+
+    /* All path components excluding last one must exist.  */
+    CAN_ALL_BUT_LAST = 1,
+
+    /* No requirements on components existence.  */
+    CAN_MISSING = 2
   };
 typedef enum canonicalize_mode_t canonicalize_mode_t;
 
 char *canonicalize_filename_mode (const char *, canonicalize_mode_t);
 
-#if !HAVE_CANONICALIZE_FILE_NAME
+# if !HAVE_CANONICALIZE_FILE_NAME
 char *canonicalize_file_name (const char *);
-#endif
+# endif
 
 #endif /* !CANONICALIZE_H_ */
