b603dc305017a8da9c47599e2417d29c39bf787e
Jim Meyering <jim@meyering.net>
2005-01-19 10:22:02 +0000

    (chdir_long): Update prototype.

--- lib/chdir-long.h
+++ lib/chdir-long.h
@@ -1,5 +1,5 @@
 /* provide a chdir function that tries not to fail due to ENAMETOOLONG
-   Copyright (C) 2004 Free Software Foundation, Inc.
+   Copyright (C) 2004, 2005 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -31,5 +31,5 @@
 #ifndef PATH_MAX
 # define chdir_long(Dir) chdir (Dir)
 #else
-int chdir_long (char const *dir);
+int chdir_long (char *dir);
 #endif
