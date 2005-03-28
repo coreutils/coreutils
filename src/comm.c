87c01f7dcb4e945f42b072396c1403ff3bb37a6a
Jim Meyering <jim@meyering.net>
2005-03-28 17:54:13 +0000

    (long_options): Use NULL, not `0'.

--- src/comm.c
+++ src/comm.c
@@ -1,5 +1,5 @@
 /* comm -- compare two sorted files line by line.
-   Copyright (C) 86, 90, 91, 1995-2004 Free Software Foundation, Inc.
+   Copyright (C) 86, 90, 91, 1995-2005 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -57,7 +57,7 @@ static struct option const long_options[] =
 {
   {GETOPT_HELP_OPTION_DECL},
   {GETOPT_VERSION_OPTION_DECL},
-  {0, 0, 0, 0}
+  {NULL, 0, NULL, 0}
 };
 
 
