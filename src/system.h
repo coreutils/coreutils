24e7f6c2482be6ee3d46efbc995a2e17819566e1
Jim Meyering <jim@meyering.net>
2005-03-29 14:09:35 +0000

    Remove #if-0'd-out code.

--- src/system.h
+++ src/system.h
@@ -839,20 +839,3 @@ ptr_align (void *ptr, size_t alignment)
    ((Type_max) / 10 < Accum || Accum * 10 + (Digit_val) < Accum		\
 	       ? 0 : ((Accum = Accum * 10 + (Digit_val)), 1))		\
   )
-
-#if 0
-#define ASSERT_ABORT(Expr) do { if (!(Expr)) abort (); } while (0)
-#define DECIMAL_DIGIT_ACCUMULATE(Accum, Digit_val, Type_max, Overflow)	\
-  do									\
-    {									\
-      int _digit_val = Digit_val;					\
-      ASSERT_ABORT (0 <= _digit_val && _digit_val < 10			\
-		    && Accum <= Type_max);				\
-      /* Ensure that Type_max is the maximum value of Accum.  */	\
-      VERIFY_W_TYPEOF (TYPE_MAXIMUM (__typeof__ (Accum)) == (Type_max)); \
-      if (!(Overflow = ((Type_max) / 10 < Accum				\
-			|| Accum * 10 + _digit_val < Accum)))		\
-		   Accum = Accum * 10 + _digit_val;			\
-    }									\
-  while (0)
-#endif
