05613505e521a669d8bff1d701a29857112c6322
Jim Meyering <jim@meyering.net>
2005-04-01 22:29:15 +0000

    Correct indentation of the two just-split lines.

--- src/pr.c
+++ src/pr.c
@@ -1059,7 +1059,7 @@ main (int argc, char **argv)
 	    if (xstrtol (optarg, NULL, 10, &tmp_long, "") != LONGINT_OK
 	        || tmp_long <= 0 || tmp_long > INT_MAX)
 	      error (EXIT_FAILURE, 0,
-	         _("`-w PAGE_WIDTH' invalid number of characters: `%s'"),
+		     _("`-w PAGE_WIDTH' invalid number of characters: `%s'"),
 		     optarg);
 	    if (!truncate_lines)
 	      chars_per_line = tmp_long;
@@ -1073,7 +1073,7 @@ main (int argc, char **argv)
 	    if (xstrtol (optarg, NULL, 10, &tmp_long, "") != LONGINT_OK
 		|| tmp_long <= 0 || tmp_long > INT_MAX)
 	      error (EXIT_FAILURE, 0,
-		 _("`-W PAGE_WIDTH' invalid number of characters: `%s'"),
+		     _("`-W PAGE_WIDTH' invalid number of characters: `%s'"),
 		     optarg);
 	    chars_per_line = tmp_long;
 	    break;
