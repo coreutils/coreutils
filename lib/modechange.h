5580780a3713868a758cfb82c999bc57b1bdd56f
Paul Eggert <eggert@cs.ucla.edu>
2005-04-28 16:29:22 +0000

    (MODE_X_IF_ANY_X, MODE_COPY_EXISTING): (struct mode_change): Move to modechange.c; callers don't need to see this stuff. (MODE_MASK_EQUALS, MODE_MASK_PLUS, MODE_MASK_MINUS, MODE_MASK_ALL): (MODE_INVALID, MODE_MEMORY_EXHAUSTED, MODE_BAD_REFERENCE): Remove. (mode_change, mode_adjust): Reflect the new signatures noted above.

--- lib/modechange.h
+++ lib/modechange.h
@@ -1,5 +1,7 @@
 /* modechange.h -- definitions for file mode manipulation
-   Copyright (C) 1989, 1990, 1997, 2003, 2004 Free Software Foundation, Inc.
+
+   Copyright (C) 1989, 1990, 1997, 2003, 2004, 2005 Free Software
+   Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -22,38 +24,9 @@
 
 # include <sys/types.h>
 
-/* Affect the execute bits only if at least one execute bit is set already,
-   or if the file is a directory. */
-# define MODE_X_IF_ANY_X 01
-
-/* If set, copy some existing permissions for u, g, or o onto the other two.
-   Which of u, g, or o is copied is determined by which bits are set in the
-   `value' field. */
-# define MODE_COPY_EXISTING 02
-
-struct mode_change
-{
-  char op;			/* One of "=+-". */
-  char flags;			/* Special operations. */
-  mode_t affected;		/* Set for u/g/o/s/s/t, if to be affected. */
-  mode_t value;			/* Bits to add/remove. */
-  struct mode_change *next;	/* Link to next change in list. */
-};
-
-/* Masks for mode_compile argument. */
-# define MODE_MASK_EQUALS 1
-# define MODE_MASK_PLUS 2
-# define MODE_MASK_MINUS 4
-# define MODE_MASK_ALL (MODE_MASK_EQUALS | MODE_MASK_PLUS | MODE_MASK_MINUS)
-
-/* Error return values for mode_compile. */
-# define MODE_INVALID (struct mode_change *) 0
-# define MODE_MEMORY_EXHAUSTED (struct mode_change *) 1
-# define MODE_BAD_REFERENCE (struct mode_change *) 2
-
-struct mode_change *mode_compile (const char *, unsigned int);
+struct mode_change *mode_compile (const char *);
 struct mode_change *mode_create_from_ref (const char *);
-mode_t mode_adjust (mode_t, const struct mode_change *);
+mode_t mode_adjust (mode_t, struct mode_change const *, mode_t);
 void mode_free (struct mode_change *);
 
 #endif
