37b4097c76b749f90393f8322e4af4c9f231c26a
Paul Eggert <eggert@cs.ucla.edu>
2005-04-29 23:51:11 +0000

    (tv): Rename tests from obs to obs-plus if they use file names starting with +. (test_vector): Don't set _POSIX2_VERSION if obs but not obs-plus.

--- tests/tail/Test.pm
+++ tests/tail/Test.pm
@@ -5,8 +5,8 @@ use strict;
 my @tv = (
 # test name, options, input, expected output, expected return code
 #
-['obs-c1', '+2c', 'abcd', 'bcd', 0],
-['obs-c2', '+8c', 'abcd', '', 0],
+['obs-plus-c1', '+2c', 'abcd', 'bcd', 0],
+['obs-plus-c2', '+8c', 'abcd', '', 0],
 ['obs-c3', '-1c', 'abcd', 'd', 0],
 ['obs-c4', '-9c', 'abcd', 'abcd', 0],
 ['obs-c5', '-12c', 'x' . ('y' x 12) . 'z', ('y' x 11) . 'z', 0],
@@ -15,20 +15,20 @@ my @tv = (
 ['obs-l1', '-1l', 'x', 'x', 0],
 ['obs-l2', '-1l', "x\ny\n", "y\n", 0],
 ['obs-l3', '-1l', "x\ny", "y", 0],
-['obs-l4', '+1l', "x\ny\n", "x\ny\n", 0],
-['obs-l5', '+2l', "x\ny\n", "y\n", 0],
+['obs-plus-l4', '+1l', "x\ny\n", "x\ny\n", 0],
+['obs-plus-l5', '+2l', "x\ny\n", "y\n", 0],
 
 # Same as -l tests, but without the `l'.
 ['obs-1', '-1', 'x', 'x', 0],
 ['obs-2', '-1', "x\ny\n", "y\n", 0],
 ['obs-3', '-1', "x\ny", "y", 0],
-['obs-4', '+1', "x\ny\n", "x\ny\n", 0],
-['obs-5', '+2', "x\ny\n", "y\n", 0],
+['obs-plus-4', '+1', "x\ny\n", "x\ny\n", 0],
+['obs-plus-5', '+2', "x\ny\n", "y\n", 0],
 
 # This is equivalent to +10c
-['obsx-1', '+c', 'x' . ('y' x 10) . 'z', 'yyz', 0],
+['obs-plus-x1', '+c', 'x' . ('y' x 10) . 'z', 'yyz', 0],
 # This is equivalent to +10l
-['obsx-2', '+l', "x\n" . ("y\n" x 10) . 'z', "y\ny\nz", 0],
+['obs-plus-x2', '+l', "x\n" . ("y\n" x 10) . 'z', "y\ny\nz", 0],
 # With no number, this is like -10l
 ['obs-l', '-l', "x\n" . ("y\n" x 10) . 'z', ("y\n" x 9) . 'z', 0],
 
@@ -87,7 +87,7 @@ sub test_vector
     {
       my ($test_name, $flags, $in, $exp, $ret) = @$t;
 
-      $test_name =~ /^(obs|minus-)/
+      $test_name =~ /^(obs-plus-|minus-)/
 	and $Test::env{$test_name} = ['_POSIX2_VERSION=199209'];
 
       # If you run the minus* tests with a FILE arg they'd hang.
