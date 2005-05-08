963e363388569485a681110708e78f0ffb04f93f
Paul Eggert <eggert@cs.ucla.edu>
2005-05-08 16:50:57 +0000

    * yesno.c: Include getline.h, not ctype.h. (yesno): Don't remove leading white space; POSIX doesn't allow it. Use getline to remove arbitrary restriction on response length.

--- lib/yesno.c
+++ lib/yesno.c
@@ -1,5 +1,7 @@
 /* yesno.c -- read a yes/no response from stdin
-   Copyright (C) 1990, 1998, 2001, 2003, 2004 Free Software Foundation, Inc.
+
+   Copyright (C) 1990, 1998, 2001, 2003, 2004, 2005 Free Software
+   Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -21,36 +23,31 @@
 
 #include "yesno.h"
 
-#include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-#if USE_UNLOCKED_IO
-# include "unlocked-io.h"
-#endif
+#include "getline.h"
 
-/* Read one line from standard input
-   and return nonzero if that line begins with y or Y,
-   otherwise return 0. */
+/* Return true if we read an affirmative line from standard input.  */
 
 extern int rpmatch (char const *response);
 
 bool
 yesno (void)
 {
-  /* We make some assumptions here:
-     a) leading white space in the response are not vital
-     b) the first 128 characters of the answer are enough (the rest can
-	be ignored)
-     I cannot think for a situation where this is not ok.  --drepper@gnu  */
-  char buf[128];
-  int len = 0;
-  int c;
+  char *response = NULL;
+  size_t response_size = 0;
+  ssize_t response_len = getline (&response, &response_size, stdin);
+  bool yes;
 
-  while ((c = getchar ()) != EOF && c != '\n')
-    if ((len > 0 && len < 127) || (len == 0 && !isspace (c)))
-      buf[len++] = c;
-  buf[len] = '\0';
+  if (response_len <= 0)
+    yes = false;
+  else
+    {
+      response[response_len - 1] = '\0';
+      yes = (0 < rpmatch (response));
+    }
 
-  return rpmatch (buf) == 1;
+  free (response);
+  return yes;
 }
