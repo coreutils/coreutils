c0c815af8db441526ef112700b9185df6006c771
Jim Meyering <jim@meyering.net>
2005-05-14 06:57:06 +0000

    Update FSF postal mail address.

--- src/factor.c
+++ src/factor.c
@@ -13,7 +13,7 @@
 
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software Foundation,
-   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1307, USA.  */
 
 /* Written by Paul Rubin <phr@ocf.berkeley.edu>.
    Adapted for GNU, fixed to factor UINT_MAX by Jim Meyering.  */
--- src/ptx.c
+++ src/ptx.c
@@ -14,7 +14,7 @@
 
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software Foundation,
-   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1307, USA.
 
    François Pinard <pinard@iro.umontreal.ca> */
 
@@ -2003,7 +2003,7 @@ GNU General Public License for more details.\n\
 	  fputs (_("\
 You should have received a copy of the GNU General Public License\n\
 along with this program; if not, write to the Free Software Foundation,\n\
-Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.\n"),
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1307, USA.\n"),
 		 stdout);
 
 	  exit (EXIT_SUCCESS);
