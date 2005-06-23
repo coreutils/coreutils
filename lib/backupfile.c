5fd26561cbc65bdbb89455e5420492131c796b52
Jim Meyering <jim@meyering.net>
2005-06-23 13:26:15 +0000

    (backup_args): Change a `0' to NULL.

--- lib/backupfile.c
+++ lib/backupfile.c
@@ -1,7 +1,7 @@
 /* backupfile.c -- make Emacs style backup file names
 
    Copyright (C) 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
-   1999, 2000, 2001, 2002, 2003, 2004 Free Software Foundation, Inc.
+   1999, 2000, 2001, 2002, 2003, 2004, 2005 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -340,7 +340,7 @@ static char const * const backup_args[] =
   "simple", "never",
   "existing", "nil",
   "numbered", "t",
-  0
+  NULL
 };
 
 static const enum backup_type backup_types[] =
