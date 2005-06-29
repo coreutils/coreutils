fd98517183ca2c17b87cc881db5d143f7498d70d
Jim Meyering <jim@meyering.net>
2005-06-29 16:26:01 +0000

    (show_date): Use X2REALLOC rather than x2realloc.

--- src/du.c
+++ src/du.c
@@ -430,7 +430,7 @@ show_date (const char *format, time_t when, int nsec)
 
   do
     {
-      out = x2realloc (out, &out_length);
+      out = X2REALLOC (out, &out_length);
 
       /* Mark the first byte of the buffer so we can detect the case
          of nstrftime producing an empty string.  Otherwise, this loop
