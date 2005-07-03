8b0a2afe9f57016e0d740aab36ff1a283832c703
Paul Eggert <eggert@cs.ucla.edu>
2005-07-03 06:43:07 +0000

    Add support for fcntl--.h, stdio--.h, stdlib--.h, unistd--.h, etc.

---
+++ lib/fd-reopen.c
@@ -0,0 +1,46 @@
+/* Invoke open, but return either a desired file descriptor or -1.
+
+   Copyright (C) 2005 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
+/* Written by Paul Eggert.  */
+
+#include "fd-reopen.h"
+
+#include <errno.h>
+#include <fcntl.h>
+
+/* Open a file to a particular file descriptor.  This is like standard
+   `open', except it always returns DESIRED_FD if successful.  */
+
+int
+fd_reopen (int desired_fd, char const *file, int flags, mode_t mode)
+{
+  int fd;
+
+  close (desired_fd);
+  fd = open (file, flags, mode);
+  if (fd == desired_fd || fd < 0)
+    return fd;
+  else
+    {
+      int fd2 = fcntl (fd, F_DUPFD, desired_fd);
+      int saved_errno = errno;
+      close (fd);
+      errno = saved_errno;
+      return fd2;
+    }
+}
---
+++ m4/fcntl-safer.m4
@@ -0,0 +1,11 @@
+# fcntl-safer.m4 serial 1
+dnl Copyright (C) 2005 Free Software Foundation, Inc.
+dnl This file is free software; the Free Software Foundation
+dnl gives unlimited permission to copy and/or distribute it,
+dnl with or without modifications, as long as this notice is preserved.
+
+AC_DEFUN([gl_FCNTL_SAFER],
+[
+  AC_LIBSOURCES([open-safer.c, fcntl-safer.h])
+  AC_LIBOBJ([open-safer])
+])
