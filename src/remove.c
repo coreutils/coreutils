32034e05b7fec52e0019e374b9cd1c463f94d812
Jim Meyering <jim@meyering.net>
2005-11-22 21:12:28 +0000

    whoops.  Add back decl/definition of n_lengths.

--- src/remove.c
+++ src/remove.c
@@ -574,7 +574,11 @@ AD_push (int fd_cwd, Dirstack_state *ds, char const *dir,
 
   /* Extend the stack.  */
   obstack_blank (&ds->Active_dir, sizeof (struct AD_ent));
-  assert (AD_stack_height (ds) == n_lengths + 1);
+
+  {
+    size_t n_lengths = obstack_object_size (&ds->len_stack) / sizeof (size_t);
+    assert (AD_stack_height (ds) == n_lengths + 1);
+  }
 
   /* Fill in the new values.  */
   top = AD_stack_top (ds);
