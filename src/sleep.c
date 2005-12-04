f390052b80112f49a5f2e235f0cac126a006e01f
Jim Meyering <jim@meyering.net>
2005-12-04 09:58:34 +0000

    (usage): Say what happens with two or more arguments.

--- src/sleep.c
+++ src/sleep.c
@@ -51,7 +51,8 @@ Usage: %s NUMBER[SUFFIX]...\n\
 Pause for NUMBER seconds.  SUFFIX may be `s' for seconds (the default),\n\
 `m' for minutes, `h' for hours or `d' for days.  Unlike most implementations\n\
 that require NUMBER be an integer, here NUMBER may be an arbitrary floating\n\
-point number.\n\
+point number.  Given two or more arguments, pause for the amount of time\n\
+specified by the sum of their values.\n\
 \n\
 "),
 	      program_name, program_name);
