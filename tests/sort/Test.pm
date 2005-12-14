ce1be021a75b31e21e554e3dca0c492e909852c3
Paul Eggert <eggert@cs.ucla.edu>
2005-12-14 22:45:29 +0000

    (incompat1, incompat2, incompat3): New tests.

--- tests/sort/Test.pm
+++ tests/sort/Test.pm
@@ -247,6 +247,11 @@ my @tv = (
 # Specifying two -o options should evoke a failure
 ["o2", '-o x -o y', '', '', 2],
 
+# Specifying incompatible options should evoke a failure.
+["incompat1", '-in', '', '', 2],
+["incompat2", '-fR', '', '', 2],
+["incompat3", '-dfgiMnR', '', '', 2],
+
 # -t '\0' is accepted, as of coreutils-5.0.91
 ['nul-tab', "-k2,2 -t '\\0'", "a\0z\01\nb\0y\02\n", "b\0y\02\na\0z\01\n", 0],
 );
