7c364b54fc9822a82f305846d349048293ac4783
Paul Eggert <eggert@cs.ucla.edu>
2006-01-22 08:46:54 +0000

    (quotearg_buffer_restyled): Add "default: break;" to pacify gcc -Wswitch-default.

--- lib/quotearg.c
+++ lib/quotearg.c
@@ -1,7 +1,7 @@
 /* quotearg.c - quote arguments for output
 
-   Copyright (C) 1998, 1999, 2000, 2001, 2002, 2004, 2005 Free Software
-   Foundation, Inc.
+   Copyright (C) 1998, 1999, 2000, 2001, 2002, 2004, 2005, 2006 Free
+   Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -307,6 +307,9 @@ quotearg_buffer_restyled (char *buffer, size_t buffersize,
 		    STORE ('\\');
 		    STORE ('?');
 		    break;
+
+		  default:
+		    break;
 		  }
 	      break;
 
@@ -454,6 +457,9 @@ quotearg_buffer_restyled (char *buffer, size_t buffersize,
 				case '[': case '\\': case '^':
 				case '`': case '|':
 				  goto use_shell_always_quoting_style;
+
+				default:
+				  break;
 				}
 			  }
 
