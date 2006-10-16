7a0f8b04873b427720cd2a88ed1adff8c0e1f445
Paul Eggert <eggert@cs.ucla.edu>
2006-10-16 16:53:22 +0000

    * src/groups.sh (version): Reword message to match the other programs. Problem reported by Eric Blake.

--- src/groups.sh
+++ src/groups.sh
@@ -32,11 +32,12 @@ Same as id -Gn.  If no USERNAME, use current process.
 Report bugs to <@PACKAGE_BUGREPORT@>."
 
 version='groups (@GNU_PACKAGE@) @VERSION@
-Written by David MacKenzie.
-
 Copyright (C) 2006 Free Software Foundation, Inc.
-This is free software; see the source for copying conditions.  There is NO
-warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.'
+This is free software.  You may redistribute copies of it under the terms of
+the GNU General Public License <http://www.gnu.org/licenses/gpl.html>.
+There is NO WARRANTY, to the extent permitted by law.
+
+Written by David MacKenzie.'
 
 
 for arg
