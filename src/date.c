54bdcc09ae15fd063002fff33e9ea920b5187379
Ed Santiago <ed@edsantiago.com>
2007-02-23 16:11:01 +0100

    * src/date.c (usage): Mention that --rfc-3339 uses space, not 'T', for a separator.  Include sample RFC 2822 and 3339 output.

--- src/date.c
+++ src/date.c
@@ -142,10 +142,13 @@ Display the current time in the given FORMAT, or set the system date.\n\
 "), stdout);
       fputs (_("\
   -r, --reference=FILE      display the last modification time of FILE\n\
-  -R, --rfc-2822            output date and time in RFC 2822 format\n\
+  -R, --rfc-2822            output date and time in RFC 2822 format.\n\
+                            Example: Mon, 07 Aug 2006 12:34:56 -0600\n\
       --rfc-3339=TIMESPEC   output date and time in RFC 3339 format.\n\
                             TIMESPEC=`date', `seconds', or `ns' for\n\
                             date and time to the indicated precision.\n\
+                            Date and time components are separated by\n\
+                            a single space: 2006-08-07 12:34:56-06:00\n\
   -s, --set=STRING          set time described by STRING\n\
   -u, --utc, --universal    print or set Coordinated Universal Time\n\
 "), stdout);
