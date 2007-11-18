98ed526f44839dec30d0f7dc352d919ee58f47ad
Jim Meyering <meyering@redhat.com>
2007-11-18 21:19:33 +0100

    Remove unused parameter in remove.c.
    * src/remove.c (fd_to_subdirp): Remove unused parameter.
    (remove_cwd_entries, remove_dir): Update callers.

--- src/remove.c
+++ src/remove.c
@@ -1197,7 +1197,7 @@ remove_entry (int fd_cwd, Dirstack_state const *ds, char const *filename,
    isn't restored.  */
 static DIR *
 fd_to_subdirp (int fd_cwd, char const *f,
-	       struct rm_options const *x, int prev_errno,
+	       int prev_errno,
 	       struct stat *subdir_sb,
 	       int *cwd_errno ATTRIBUTE_UNUSED)
 {
@@ -1309,7 +1309,7 @@ remove_cwd_entries (DIR **dirp,
 	case RM_NONEMPTY_DIR:
 	  {
 	    DIR *subdir_dirp = fd_to_subdirp (dirfd (*dirp), f,
-					      x, errno, subdir_sb, NULL);
+					      errno, subdir_sb, NULL);
 	    if (subdir_dirp == NULL)
 	      {
 		status = RM_ERROR;
@@ -1397,7 +1397,7 @@ remove_dir (int fd_cwd, Dirstack_state *ds, char const *dir,
      fd_to_subdirp's fstat, along with the `fstat' and the dev/ino
      comparison in AD_push ensure that we detect it and fail.  */
 
-  DIR *dirp = fd_to_subdirp (fd_cwd, dir, x, 0, dir_st, cwd_errno);
+  DIR *dirp = fd_to_subdirp (fd_cwd, dir, 0, dir_st, cwd_errno);
 
   if (dirp == NULL)
     {
