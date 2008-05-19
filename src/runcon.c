26552f27565a518b8536bd1e158f8c28ef98cb94
Jim Meyering <meyering@redhat.com>
2008-05-19 17:02:12 +0200

    * src/runcon.c: Add copyright comment.

--- src/runcon.c
+++ src/runcon.c
@@ -1,3 +1,19 @@
+/* runcon -- run command with specified security context
+   Copyright (C) 2005-2008 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
 /*
  * runcon [ context |
  *         ( [ -c ] [ -r role ] [-t type] [ -u user ] [ -l levelrange ] )
