2e6fa14d16ffda65ffb45fad94140fc340b84f26
Ondřej Vašík <ovasik@redhat.com>
2008-06-23 14:00:26 +0200

    echo: use AUTHORS from bash's built-in echo
    * AUTHORS,src/echo.c: Use bash builtin echo authors instead of FIXME unknown

--- src/echo.c
+++ src/echo.c
@@ -24,7 +24,9 @@
 /* The official name of this program (e.g., no `g' prefix).  */
 #define PROGRAM_NAME "echo"
 
-#define AUTHORS proper_name ("FIXME unknown")
+#define AUTHORS \
+  proper_name ("Brian Fox"), \
+  proper_name ("Chet Ramey")
 
 /* echo [-neE] [arg ...]
 Output the ARGs.  If -n is specified, the trailing newline is
