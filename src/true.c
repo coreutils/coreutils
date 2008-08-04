6b9f1bdd66a2e0b0f494d6f86e4cda43bf2215d2
Jim Meyering <meyering@redhat.com>
2008-08-04 10:12:47 +0200

    false: mark "false" description for translation
    * src/true.c (usage): Mark both strings with N_, so that
    the one for "false" is also extracted for translation.
    For consistency, mark both, although only the latter one needed it.
    Inspired by a patch from Benno Schulenberg.

--- src/true.c
+++ src/true.c
@@ -42,8 +42,8 @@ Usage: %s [ignored command line arguments]\n\
 	  program_name, program_name);
   printf ("%s\n\n",
 	  _(EXIT_STATUS == EXIT_SUCCESS
-	    ? "Exit with a status code indicating success."
-	    : "Exit with a status code indicating failure."));
+	    ? N_("Exit with a status code indicating success.")
+	    : N_("Exit with a status code indicating failure.")));
   fputs (HELP_OPTION_DESCRIPTION, stdout);
   fputs (VERSION_OPTION_DESCRIPTION, stdout);
   printf (USAGE_BUILTIN_WARNING, PROGRAM_NAME);
