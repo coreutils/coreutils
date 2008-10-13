59964c164b33f01b1eb3e26ce5c65a78e741148a
Jim Meyering <meyering@redhat.com>
2008-10-13 21:03:38 +0200

    doc: adjust spacing in ls --help output, for better man formatting
    * src/ls.c (usage): Adjust spacing so the "Exit status:" list
    renders well in the help2man-generated man page.

--- src/ls.c
+++ src/ls.c
@@ -4531,11 +4531,11 @@ colors, and can be set easily by the dircolors command.\n\
       fputs (_("\
 \n\
 Exit status:\n\
-0 if OK,\n\
-1 if minor problems (e.g., failure to access a file or directory not\n\
-  specified as a command line argument\n\
-2 if serious trouble (e.g., memory exhausted, invalid option or failure\n\
-  to access a file or directory specified as a command line argument).\n\
+ 0  if OK,\n\
+ 1  if minor problems (e.g., failure to access a file or directory not\n\
+    specified as a command line argument,\n\
+ 2  if serious trouble (e.g., memory exhausted, invalid option or failure\n\
+    to access a file or directory specified as a command line argument).\n\
 "), stdout);
       emit_bug_reporting_address ();
     }
