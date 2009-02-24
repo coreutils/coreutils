17b18bfa6ccd2638d8241504745fa50378c196d9
Jim Meyering <meyering@redhat.com>
2009-02-24 21:44:51 +0100

    maint: remove an unnecessary inclusion of <signal.h>
    * src/operand2sig.c: Don't include <signal.h>.

--- src/operand2sig.c
+++ src/operand2sig.c
@@ -1,5 +1,5 @@
 /* operand2sig.c -- common function for parsing signal specifications
-   Copyright (C) 2008 Free Software Foundation, Inc.
+   Copyright (C) 2008, 2009 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -24,7 +24,6 @@
 #include <config.h>
 #include <stdio.h>
 #include <sys/types.h>
-#include <signal.h>
 
 #if HAVE_SYS_WAIT_H
 # include <sys/wait.h>
