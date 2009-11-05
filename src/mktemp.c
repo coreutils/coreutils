25d2460ae3dcbfb557016bbac6e8568e258caed3
Jim Meyering <meyering@redhat.com>
2009-11-05 16:23:59 +0100

    mktemp: don't use suff_len in place of #-of-`X's variable
    * src/mktemp.c (mkstemp_len, mkdtemp_len): Pass x_len as final
    argument, and not suff_len.

--- src/mktemp.c
+++ src/mktemp.c
@@ -118,14 +118,14 @@ static int
 mkstemp_len (char *tmpl, size_t suff_len, size_t x_len, bool dry_run)
 {
   return gen_tempname_len (tmpl, suff_len, 0, dry_run ? GT_NOCREATE : GT_FILE,
-                           suff_len);
+                           x_len);
 }
 
 static int
 mkdtemp_len (char *tmpl, size_t suff_len, size_t x_len, bool dry_run)
 {
   return gen_tempname_len (tmpl, suff_len, 0, dry_run ? GT_NOCREATE : GT_DIR,
-                           suff_len);
+                           x_len);
 }
 
 int
