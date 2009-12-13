c149538652964625541f45dc4ed0be9da1dcb30f
Jim Meyering <meyering@redhat.com>
2009-12-13 12:23:25 +0100

    nohup: if fd_repoen fails (redirecting stdin), report it
    * src/nohup.c (main): Don't ignore fd_reopen failure.

--- src/nohup.c
+++ src/nohup.c
@@ -123,7 +123,11 @@ main (int argc, char **argv)
      to ensure any read evokes an error.  */
   if (ignoring_input)
     {
-      fd_reopen (STDIN_FILENO, "/dev/null", O_WRONLY, 0);
+      if (fd_reopen (STDIN_FILENO, "/dev/null", O_WRONLY, 0) < 0)
+        {
+          error (0, errno, _("failed to render standard input unusable"));
+          exit (exit_internal_failure);
+        }
       if (!redirecting_stdout && !redirecting_stderr)
         error (0, 0, _("ignoring input"));
     }
