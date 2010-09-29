bfda815cf4c889ac8165004fee5678fee67fbaf1
jeff.liu <jeff.liu@oracle.com>
2010-09-29 16:11:41 +0800

    bug#6131: [PATCH]: fiemap support for efficient sparse file copy
    Jim Meyering wrote:
    > jeff.liu wrote:
    >> Sorry for the delay.
    >>
    >> This is the new patch to isolate the stuff regarding to extents reading to a new module. and teach
    >> cp(1) to make use of it.
    >
    > Jeff,
    >
    > I applied your patch to my rebased fiemap-copy branch.
    > My first step was to run the usual
    >
    >   ./bootstrap && ./configure && make && make check
    >
    > "make check" failed on due to a double free in your new code:
    > (x86_64, Fedora 13, ext4 working directory)
    >
    > To get details, I made this temporary modification:
    Hi Jim,
    
    I am sorry for the fault, it fixed at the patch below.
    Would you please revie at your convenience?
    
    Changes:
    ========
    1. fix write_zeros() as Jim's comments, thanks for pointing this out.
    2. remove char const *fname from struct extent_scan.
    3. change the signature of open_extent_scan() from "void open_extent_scan(struct extent_scan
    **scan)" to "void open_extent_scan(struct extent_scan *scan)"; the reason is I'd like to reduce once
    memory allocation for the extent_scan variable, instead, using stack to save it.
    4. remove close_extent_scan() from a function defined at extent-scan.c to extent-scan.h as a Macro
    definination, but it does nothing for now, since initial extent scan defined at stack.
    5. add a macro "free_extents_info()" defined at extent-scan.h to release the memory allocated to
    extent info which should be called combine with get_extents_info(), it just one line, so IMHO,
    define it as macro should be ok.
    
    I have done the memory check via `valgrind`, no issue found.
    make test against cp/sparse-fiemap failed at the extent compare stage, but the file content is
    identical to each other by comparing those two files "j1/j2" manually.
    Is it make sense if we verify them through diff(1) since the testing file is in small size?
    or we have to merge the contig extents from the output of `filefrag', I admit I have not dig into
    the filefrag-extent-compare at the moment, I need to recall the perl language syntax. :-P.
    
    >From 50a3338db06442fa2d789fd65175172d140cc96e Mon Sep 17 00:00:00 2001
    From: Jie Liu <jeff.liu@oracle.com>
    Date: Wed, 29 Sep 2010 15:35:43 +0800
    Subject: [PATCH 1/1] cp: add a new module for scanning extents
    
    * src/extent-scan.c: Source code for scanning extents.
      Call open_extent_scan() to initialize extent scan.
      Call get_extents_info() to get a number of extents for each iteration.
    * src/extent-scan.h: Header file of extent-scan.c.
      Wrap free_extent_info() as macro define to release the space allocated extent_info per extent scan.
      Wrap close_extent_scan() as macro define but do nothing at the moment.
    * src/Makefile.am: Reference it and link it to copy_source.
    * src/copy.c: Make use of the new module, replace fiemap_copy() with extent_copy().
    
    Signed-off-by: Jie Liu <jeff.liu@oracle.com>

---
+++ src/extent-scan.c
@@ -0,0 +1,113 @@
+/* extent-scan.c -- core functions for scanning extents
+   Copyright (C) 2010 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   Written by Jie Liu (jeff.liu@oracle.com).  */
+
+#include <config.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <assert.h>
+
+#include "system.h"
+#include "extent-scan.h"
+#include "error.h"
+#include "quote.h"
+
+#ifndef HAVE_FIEMAP
+# include "fiemap.h"
+#endif
+
+/* Allocate space for struct extent_scan, initialize the entries if
+   necessary and return it as the input argument of get_extents_info().  */
+extern void
+open_extent_scan (int src_fd, struct extent_scan *scan)
+{
+  scan->fd = src_fd;
+  scan->ei_count = 0;
+  scan->scan_start = 0;
+  scan->initial_scan_failed = false;
+  scan->hit_last_extent = false;
+}
+
+#ifdef __linux__
+# ifndef FS_IOC_FIEMAP
+#  define FS_IOC_FIEMAP _IOWR ('f', 11, struct fiemap)
+# endif
+/* Call ioctl(2) with FS_IOC_FIEMAP (available in linux 2.6.27) to
+   obtain a map of file extents excluding holes.  */
+extern bool
+get_extents_info (struct extent_scan *scan)
+{
+  union { struct fiemap f; char c[4096]; } fiemap_buf;
+  struct fiemap *fiemap = &fiemap_buf.f;
+  struct fiemap_extent *fm_extents = &fiemap->fm_extents[0];
+  enum { count = (sizeof fiemap_buf - sizeof *fiemap) / sizeof *fm_extents };
+  verify (count != 0);
+  unsigned int i;
+
+  /* This is required at least to initialize fiemap->fm_start,
+     but also serves (in mid 2010) to appease valgrind, which
+     appears not to know the semantics of the FIEMAP ioctl. */
+  memset (&fiemap_buf, 0, sizeof fiemap_buf);
+
+  fiemap->fm_start = scan->scan_start;
+  fiemap->fm_flags = FIEMAP_FLAG_SYNC;
+  fiemap->fm_extent_count = count;
+  fiemap->fm_length = FIEMAP_MAX_OFFSET - scan->scan_start;
+
+  /* Fall back to the standard copy if call ioctl(2) failed for the
+     the first time.  */
+  if (ioctl (scan->fd, FS_IOC_FIEMAP, fiemap) < 0)
+    {
+      if (scan->scan_start == 0)
+        scan->initial_scan_failed = true;
+      return false;
+    }
+
+  /* If 0 extents are returned, then more get_extent_table() are not needed.  */
+  if (fiemap->fm_mapped_extents == 0)
+    {
+      scan->hit_last_extent = true;
+      return false;
+    }
+
+  scan->ei_count = fiemap->fm_mapped_extents;
+  scan->ext_info = xnmalloc (scan->ei_count, sizeof (struct extent_info));
+
+  for (i = 0; i < scan->ei_count; i++)
+    {
+      assert (fm_extents[i].fe_logical <= OFF_T_MAX);
+
+      scan->ext_info[i].ext_logical = fm_extents[i].fe_logical;
+      scan->ext_info[i].ext_length = fm_extents[i].fe_length;
+      scan->ext_info[i].ext_flags = fm_extents[i].fe_flags;
+    }
+
+  i--;
+  if (scan->ext_info[i].ext_flags & FIEMAP_EXTENT_LAST)
+    {
+      scan->hit_last_extent = true;
+      return true;
+    }
+
+  scan->scan_start = fm_extents[i].fe_logical + fm_extents[i].fe_length;
+
+  return true;
+}
+#else
+extern bool get_extents_info (ignored) { errno = ENOTSUP; return false; }
+#endif
