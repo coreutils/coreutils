coreutils/coreutils

commit 59e2e55d0f154a388adc9bac37d2b45f2ba971f8

sort: fix issues with month sorting in some locales
* src/sort.c (char fold_toupper[]): Change to unsigned
so as the correct comparisons are made in getmonth().
This fixes unibyte locales where abbreviated months
have characters that are > 0x7F, but it also works for
multibyte locales with the caveat that multibyte characters
are matched case sensitively.
With this change, the following example sorts correctly:
  $ echo -e "1 márta\n2 Feabhra" | LANG=ga_IE.utf8 sort -k2,2M
  2 Feabhra
  1 márta
* src/sort.c (inittables): Since we ignore blanks around months
in the input, don't include them when they're present in the locale.
With this change, the following example sorts correctly:
  $ echo -e "1 2月\n2 1月" | LANG=ja_JP.utf8 sort -k2,2M
  2 1月
  1 2月
* tests/misc/sort-month: A new test to exercise the above cases.
* tests/Makefile.am: Reference the new test.
* NEWS: Mention the fix.

src/sort.c
@@ -209,7 +209,7 @@ static bool nonprinting[UCHAR_LIM];
 static bool nondictionary[UCHAR_LIM];
 
 /* Translation table folding lower case to upper.  */
-static char fold_toupper[UCHAR_LIM];
+static unsigned char fold_toupper[UCHAR_LIM];
 
 #define MONTHS_PER_YEAR 12
 
@@ -1129,7 +1129,7 @@ inittables (void)
         {
           char const *s;
           size_t s_len;
-          size_t j;
+          size_t j, k;
           char *name;
 
           s = (char *) nl_langinfo (ABMON_1 + i);
@@ -1137,9 +1137,10 @@ inittables (void)
           monthtab[i].name = name = xmalloc (s_len + 1);
           monthtab[i].val = i + 1;
 
-          for (j = 0; j < s_len; j++)
-            name[j] = fold_toupper[to_uchar (s[j])];
-          name[j] = '\0';
+          for (j = k = 0; j < s_len; j++)
+            if (! isblank (to_uchar (s[j])))
+              name[k++] = fold_toupper[to_uchar (s[j])];
+          name[k] = '\0';
         }
       qsort ((void *) monthtab, MONTHS_PER_YEAR,
              sizeof *monthtab, struct_month_cmp);

tests/Makefile.am
@@ -225,6 +225,7 @@ TESTS =						\
   misc/sort-files0-from				\
   misc/sort-merge				\
   misc/sort-merge-fdlimit			\
+  misc/sort-month				\
   misc/sort-rand				\
   misc/sort-version				\
   misc/split-a					\

tests/misc/sort-month
@@ -1,0 +1,38 @@
+#!/bin/sh
+# Test sorting of abbreviated months from the locale
+
+# Copyright (C) 2010 Free Software Foundation, Inc.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+if test "$VERBOSE" = yes; then
+  set -x
+  sort --version
+fi
+
+. $srcdir/test-lib.sh
+
+locale --version >/dev/null 2>&1 ||
+  skip_test_ 'The locale utility is not present'
+
+# C will be used if the locale is not present
+for LOC in "$LOCALE_FR" "$LOCALE_FR_UTF8" "ja_JP.utf8"; do
+  mon="$(LC_ALL="$LOC" locale abmon 2>/dev/null);"
+  smon=$(LC_ALL="$LOC" locale abmon 2>/dev/null |
+          tr ';' '\n' | shuf | nl | LC_ALL="$LOC" sort -b -k2,2M |
+          cut -f2 | tr '\n' ';')
+  test "$mon" = "$smon" || { fail=1; break; }
+done
+
+Exit $fail

commit 34ef0a01f0e1d620dac17d35832449b6880641b0

sort: fix unportable cast of unsigned char * -> char *
* src/sort.c (fold_toupper): Change this back from char to
unsigned char, fixing a portability issue introduced in commit
59e2e55d0f154a388adc9bac37d2b45f2ba971f8 dated February 26, as the
C Standard doesn't let you convert from unsigned char * to char *
without a cast, and the (in theory more portable) style here is to
convert char values, not pointer values.
(getmonth): Convert char to unsigned char when needed for
comparison.

src/sort.c
@@ -268,7 +268,7 @@ static bool nonprinting[UCHAR_LIM];
 static bool nondictionary[UCHAR_LIM];
 
 /* Translation table folding lower case to upper.  */
-static unsigned char fold_toupper[UCHAR_LIM];
+static char fold_toupper[UCHAR_LIM];
 
 #define MONTHS_PER_YEAR 12
 
@@ -1952,12 +1952,12 @@ getmonth (char const *month, char **ea)
                 *ea = (char *) m;
               return monthtab[ix].val;
             }
-          if (fold_toupper[to_uchar (*m)] < to_uchar (*n))
+          if (to_uchar (fold_toupper[to_uchar (*m)]) < to_uchar (*n))
             {
               hi = ix;
               break;
             }
-          else if (fold_toupper[to_uchar (*m)] > to_uchar (*n))
+          else if (to_uchar (fold_toupper[to_uchar (*m)]) > to_uchar (*n))
             {
               lo = ix + 1;
               break;