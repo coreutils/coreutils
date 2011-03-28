17a7e4592727b44d0a5550d1340e354786109af7
Mathieu Bridon <bochecha@fedoraproject.org>
2011-03-28 09:39:53 +0200

    tests: avoid unwarranted failure in mock-simulated non-SELinux env.
    * tests/init.cfg (require_selinux_): Skip the test also when
    /proc/filesystems does not list selinuxfs.
    Add comments.
    * cfg.mk (exclude_file_name_regexp--sc_file_system): Exempt
    tests/init.cfg, with its use of /proc/filesystems.
    Based on the patch by Mathieu Bridon in http://debbugs.gnu.org/8359.
    More discussion in http://bugzilla.redhat.com/573111

--- cfg.mk
+++ cfg.mk
@@ -345,7 +345,8 @@ exclude_file_name_regexp--sc_po_check = ^gl/
 exclude_file_name_regexp--sc_prohibit_always-defined_macros = ^src/seq\.c$$
 exclude_file_name_regexp--sc_prohibit_empty_lines_at_EOF = ^tests/pr/
 exclude_file_name_regexp--sc_program_name = ^(gl/.*|lib/euidaccess-stat)\.c$$
-exclude_file_name_regexp--sc_file_system = NEWS|^(src/df\.c|tests/misc/df-P)$$
+exclude_file_name_regexp--sc_file_system = \
+  NEWS|^(tests/init\.cfg|src/df\.c|tests/misc/df-P)$$
 exclude_file_name_regexp--sc_prohibit_always_true_header_tests = \
   ^m4/stat-prog\.m4$$
 exclude_file_name_regexp--sc_prohibit_fail_0 = \
