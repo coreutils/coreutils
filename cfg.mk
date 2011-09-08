1888629627078debffdb7520fb7feffed5f2343d
Jim Meyering <meyering@redhat.com>
2011-09-08 17:23:21 +0200

    maint: post-release administrivia
    * NEWS: Add header line for next release.
    * .prev-version: Record previous version.
    * cfg.mk (old_NEWS_hash): Auto-update.

--- cfg.mk
+++ cfg.mk
@@ -27,7 +27,7 @@ bootstrap-tools = autoconf,automake,gnulib,bison
 # Now that we have better tests, make this the default.
 export VERBOSE = yes
 
-old_NEWS_hash = d491296a7e0e2269b2b96dc4bd5f77a8
+old_NEWS_hash = 0869235d8e62fd7ed8d8dfacfad2e4d9
 
 # Add an exemption for sc_makefile_at_at_check.
 _makefile_at_at_check_exceptions = ' && !/^cu_install_program =/'
