8ac05f82a263364310dc9d8bfd734c478c03fbdd
Jim Meyering <meyering@redhat.com>
2012-01-07 17:18:14 +0100

    maint: update quoting of "Try `prog --help'" to "Try 'prog --help'"
    Automatically adjust both the source (now in only one place)
    and all tests that expect the resulting output via this:
    
    git grep -l 'Try.*--help' src/system.h tests \
      | xargs perl -pi -e 's/Try \\?`(\S+ --help)/Try '\''$1/'

--- src/system.h
+++ src/system.h
@@ -559,7 +559,7 @@ emit_ancillary_info (void)
 static inline void
 emit_try_help (void)
 {
-  fprintf (stderr, _("Try `%s --help' for more information.\n"), program_name);
+  fprintf (stderr, _("Try '%s --help' for more information.\n"), program_name);
 }
 
 #include "inttostr.h"
