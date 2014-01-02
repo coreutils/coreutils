299924fc224b4419350e3b7749650178a509206a
Bernhard Voelker <mail@bernhard-voelker.de>
2014-01-02 19:20:50 +0100

    maint: fix copyright holder and year in selinux sources
    * src/selinux.c: As the copyright is assigned to the FSF for all
    of coreutils, replace the copyright holder from "Red Hat, Inc."
    to "Free Software Foundation, Inc.".  Prompted by a warning of
    'make update-copyright'.  Also update the copyright year.

--- src/selinux.h
+++ src/selinux.h
@@ -1,5 +1,5 @@
 /* selinux - core functions for maintaining SELinux labeling
-   Copyright (C) 2012-2013 Red Hat, Inc.
+   Copyright (C) 2012-2014 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
