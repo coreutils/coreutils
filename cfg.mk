7ebc4d84c27de8bf2a1d76b6fd1b014af97af132
Pádraig Brady <P@draigBrady.com>
2016-01-13 11:28:45 +0000

    maint: fix old spelling mistakes in NEWS
    * NEWS: s/descendent/descendant/; s/indiscriminite/indiscriminate/.
    * cfg.mk (old_NEWS_hash): Update via `make update-NEWS-hash`.

--- cfg.mk
+++ cfg.mk
@@ -45,7 +45,7 @@ export VERBOSE = yes
 # 4914152 9e
 export XZ_OPT = -8e
 
-old_NEWS_hash = 5c64c69e6303deaceecacc4b498a61fd
+old_NEWS_hash = f17fb1ab51acb854afd2f45b8940a029
 
 # Add an exemption for sc_makefile_at_at_check.
 _makefile_at_at_check_exceptions = ' && !/^cu_install_prog/ && !/dynamic-dep/'
