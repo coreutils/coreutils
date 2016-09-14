373ba16f332d0632f2ce987893ed67bb4334c5d2
Jim Meyering <meyering@fb.com>
2016-09-14 14:16:40 -0700

    maint: avoid "make distcheck" failure due to excess .deps directories
    * Makefile.am (my-distcheck): Remove all .deps directories before the
    recursive diff that searches for left-behind files.  Otherwise, with
    automake master (some time after v1.15), "make distcheck" would fail
    due to those directories being left behind after "make distclean".

--- dist-check.mk
+++ dist-check.mk
@@ -120,6 +120,10 @@ my-distcheck: $(DIST_ARCHIVES) $(local-check)
 	)
 	(cd $(t) && mv $(distdir) $(distdir).old	\
 	  && $(amtar_extract_) - ) < $(preferred_tarball_)
+# With post-v1.15 automake, "distclean" would fail to remove .deps
+# directories, leading to a spurious failure of the following recursive diff.
+# FIXME: remove this, once automake is fixed.
+	find $(t)/$(distdir).old $(t)/$(distdir) -name .deps | xargs rmdir
 	diff -ur $(t)/$(distdir).old $(t)/$(distdir)
 	-rm -rf $(t)
 	rmdir $(tmpdir)/$(PACKAGE) $(tmpdir)
