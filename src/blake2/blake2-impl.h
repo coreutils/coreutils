8b1c6eebaccb1105adc29ec4ae76691df9e691fa
Pádraig Brady <P@draigBrady.com>
2017-07-23 01:50:37 -0700

    maint: resync with blake2 upstream
    * src/blake2/blake2-impl.h: Don't use the equivalent explicit_bzero().

--- src/blake2/blake2-impl.h
+++ src/blake2/blake2-impl.h
@@ -153,7 +153,8 @@ static BLAKE2_INLINE uint64_t rotr64( const uint64_t w, const unsigned c )
 /* prevents compiler optimizing out memset() */
 static BLAKE2_INLINE void secure_zero_memory(void *v, size_t n)
 {
-  explicit_bzero (v, n);
+  static void *(*const volatile memset_v)(void *, int, size_t) = &memset;
+  memset_v(v, 0, n);
 }
 
 #endif
