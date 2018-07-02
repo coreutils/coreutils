83322567de97855a5b72772ce1ad06856dab11d2
Pádraig Brady <P@draigBrady.com>
2018-07-01 18:28:37 -0700

    maint: post-release administrivia
    * NEWS: Add header line for next release.
    * .prev-version: Record previous version.
    * cfg.mk (old_NEWS_hash): Auto-update.

--- cfg.mk
+++ cfg.mk
@@ -48,7 +48,7 @@ export VERBOSE = yes
 # 4914152 9e
 export XZ_OPT = -8e
 
-old_NEWS_hash = 48668bce5e01bf434b1d1ff10d141884
+old_NEWS_hash = 79133b52351baf64693804eed58a0cbc
 
 # Add an exemption for sc_makefile_at_at_check.
 _makefile_at_at_check_exceptions = ' && !/^cu_install_prog/ && !/dynamic-dep/'
