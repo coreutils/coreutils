643a7325ec95d9e81b15656b62f2be800d00fe00
Paul Eggert <eggert@cs.ucla.edu>
2019-01-25 11:15:31 -0800

    sleep: improve doc
    Problem reported by Robert Elz.
    * doc/coreutils.texi (sleep invocation):
    Say that arguments must be non-negative, which means they cannot
    be arbitrary floating-point numbers.  Mention POSIX, not
    “historical implementations” that are no longer of practical
    interest.  List the extensions to POSIX.
    * src/sleep.c (usage): Omit needless words, removing dubious
    commentary about “most implementations” and incorrect commentary
    about “arbitrary”.  Details about exactly which numbers are
    allowed can be found in the documentation.

--- src/sleep.c
+++ src/sleep.c
@@ -51,9 +51,8 @@ usage (int status)
 Usage: %s NUMBER[SUFFIX]...\n\
   or:  %s OPTION\n\
 Pause for NUMBER seconds.  SUFFIX may be 's' for seconds (the default),\n\
-'m' for minutes, 'h' for hours or 'd' for days.  Unlike most implementations\n\
-that require NUMBER be an integer, here NUMBER may be an arbitrary floating\n\
-point number.  Given two or more arguments, pause for the amount of time\n\
+'m' for minutes, 'h' for hours or 'd' for days.  NUMBER need not be an\n\
+integer.  Given two or more arguments, pause for the amount of time\n\
 specified by the sum of their values.\n\
 \n\
 "),
