aefd434e56c6e8042d98b1fee20f3e8f90a127ef
Bernhard Voelker <mail@bernhard-voelker.de>
2020-05-25 16:31:08 +0200

    maint: copy FDL from gnulib instead of using it as module
    Since the previous gnulib update, bootstrap outputs this warning:
    
      Notice from module fdl:
        Don't use this module! Instead, copy the referenced license file \
        into your version control repository.
    
    See gnulib commit:
      https://git.sv.gnu.org/cgit/gnulib.git/commit/?id=88fc5afbccc9
    
    * bootstrap.conf (gnulib_modules): Remove 'fdl'.
    * doc/fdl.texi: Add file as a copy of 'gnulib/doc/fdl.texi'.
    * doc/.gitignore (/fdl.texi): Remove entry.
    * cfg.mk (FILTER_LONG_LINES): Add pattern for the 'fdl.texi' file.

--- cfg.mk
+++ cfg.mk
@@ -327,11 +327,13 @@ sc_prohibit-gl-attributes:
 
 # Look for lines longer than 80 characters, except omit:
 # - urls
+# - the fdl.texi file copied from gnulib,
 # - the help2man script copied from upstream,
 # - tests involving long checksum lines, and
 # - the 'pr' test cases.
 FILTER_LONG_LINES =						\
   \|^[^:]*NEWS:.*https\{,1\}://| d;					\
+  \|^[^:]*doc/fdl.texi:| d;					\
   \|^[^:]*man/help2man:| d;					\
   \|^[^:]*tests/misc/sha[0-9]*sum.*\.pl[-:]| d;			\
   \|^[^:]*tests/pr/|{ \|^[^:]*tests/pr/pr-tests:| !d; };
