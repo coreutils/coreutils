4d665a1e949d504a464cef5e45897f8cdd94d9ba
Cael Rasmussen <53895123+caelwithcats@users.noreply.github.com>
2020-10-07 10:23:14 +1100

    false: fix copy & paste

--- src/false.c
+++ src/false.c
@@ -1,5 +1,4 @@
-/* basename -- strip directory and suffix from file names
-   Copyright (C) 1990-2020 Free Software Foundation, Inc.
+/* Copyright (C) 1990-2020 Free Software Foundation, Inc.
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
