1b988ffa957d20661d9b811c1cfe91791743dd93
Paul Eggert <eggert@cs.ucla.edu>
2021-11-15 21:30:15 -0800

    maint: fix nonnull decl
    * gl/lib/randread.h (randread_new): Do not mark with
    _GL_ATTRIBUTE_RETURNS_NONNULL, since it can return NULL.

--- gl/lib/randread.h
+++ gl/lib/randread.h
@@ -26,8 +26,7 @@ struct randread_source;
 
 int randread_free (struct randread_source *) _GL_ATTRIBUTE_NONNULL ();
 struct randread_source *randread_new (char const *, size_t)
-  _GL_ATTRIBUTE_MALLOC _GL_ATTRIBUTE_DEALLOC (randread_free, 1)
-  _GL_ATTRIBUTE_RETURNS_NONNULL;
+  _GL_ATTRIBUTE_MALLOC _GL_ATTRIBUTE_DEALLOC (randread_free, 1);
 void randread (struct randread_source *, void *, size_t)
   _GL_ATTRIBUTE_NONNULL ();
 void randread_set_handler (struct randread_source *, void (*) (void const *))
