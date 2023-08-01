coreutils/coreutils

commit e0a4a60af52e40e5ee5c7d5f1764150301d3dea9

cut: promptly diagnose write errors
* src/cut.c (cut_bytes): Diagnose errors from fwrite() and putchar().
(cut_fields): Likewise.
* tests/misc/write-errors.sh: Enable the test for cut,
and augment to cover both cut_bytes() and cut_fields().
* NEWS: Mention the improvement.

src/cut.c
@@ -232,7 +232,8 @@ cut_bytes (FILE *stream)
 
       if (c == line_delim)
         {
-          putchar (c);
+          if (putchar (c) < 0)
+            write_error ();
           byte_idx = 0;
           print_delimiter = false;
           current_rp = frp;
@@ -252,13 +253,16 @@ cut_bytes (FILE *stream)
                 {
                   if (print_delimiter && is_range_start_index (byte_idx))
                     {
-                      fwrite (output_delimiter_string, sizeof (char),
-                              output_delimiter_length, stdout);
+                      if (fwrite (output_delimiter_string, sizeof (char),
+                                  output_delimiter_length, stdout)
+                          != output_delimiter_length)
+                        write_error ();
                     }
                   print_delimiter = true;
                 }
 
-              putchar (c);
+              if (putchar (c) < 0)
+                write_error ();
             }
         }
     }
@@ -325,7 +329,9 @@ cut_fields (FILE *stream)
                 }
               else
                 {
-                  fwrite (field_1_buffer, sizeof (char), n_bytes, stdout);
+                  if (fwrite (field_1_buffer, sizeof (char), n_bytes, stdout)
+                      != n_bytes)
+                    write_error ();
                   /* Make sure the output line is newline terminated.  */
                   if (field_1_buffer[n_bytes - 1] != line_delim)
                     putchar (line_delim);
@@ -336,7 +342,9 @@ cut_fields (FILE *stream)
           if (print_kth (1))
             {
               /* Print the field, but not the trailing delimiter.  */
-              fwrite (field_1_buffer, sizeof (char), n_bytes - 1, stdout);
+              if (fwrite (field_1_buffer, sizeof (char), n_bytes - 1, stdout)
+                  != n_bytes - 1)
+                write_error ();
 
               /* With -d$'\n' don't treat the last '\n' as a delimiter.  */
               if (delim == line_delim)
@@ -360,14 +368,17 @@ cut_fields (FILE *stream)
         {
           if (found_any_selected_field)
             {
-              fwrite (output_delimiter_string, sizeof (char),
-                      output_delimiter_length, stdout);
+              if (fwrite (output_delimiter_string, sizeof (char),
+                          output_delimiter_length, stdout)
+                  != output_delimiter_length)
+                write_error ();
             }
           found_any_selected_field = true;
 
           while ((c = getc (stream)) != delim && c != line_delim && c != EOF)
             {
-              putchar (c);
+              if (putchar (c) < 0)
+                write_error ();
               prev_c = c;
             }
         }
@@ -398,7 +409,8 @@ cut_fields (FILE *stream)
             {
               if (c == line_delim || prev_c != line_delim
                   || delim == line_delim)
-                putchar (line_delim);
+                if (putchar (line_delim) < 0)
+                  write_error ();
             }
           if (c == EOF)
             break;

tests/misc/write-errors.sh
@@ -28,7 +28,8 @@ fi
 echo "\
 cat /dev/zero
 # TODO: comm -z /dev/zero /dev/zero
-# TODO: cut -z -c1- /dev/zero
+cut -z -c1- /dev/zero
+cut -z -f1- /dev/zero
 dd if=/dev/zero
 expand /dev/zero
 factor --version; yes 1 | factor

commit 7783df863a9cb9c4946b4f219992edaef2252ffe

cut: promptly diagnose write errors, continued
* src/cut.c: Complete the error-handling improvements started in
commit e0a4a60af5, by adding a couple of remaining checks for putchar().
While there, sprinkle a few rather useful comments, and perform a few
small code cleanups, to make the code and the comments more uniform
and more conformant to the official coding style.  Also make the help
message slightly more uniform.

src/cut.c
@@ -70,7 +70,7 @@ static char *field_1_buffer;
 /* The number of bytes allocated for FIELD_1_BUFFER.  */
 static size_t field_1_bufsize;
 
-/* If true do not output lines containing no delimiter characters.
+/* If true, do not output lines containing no delimiter characters.
    Otherwise, all such lines are printed.  This option is valid only
    with field mode.  */
 static bool suppress_non_delimited;
@@ -160,7 +160,7 @@ Print selected parts of lines from each FILE to standard output.\n\
                             the default is to use the input delimiter\n\
 "), stdout);
       fputs (_("\
-  -z, --zero-terminated    line delimiter is NUL, not newline\n\
+  -z, --zero-terminated   line delimiter is NUL, not newline\n\
 "), stdout);
       fputs (HELP_OPTION_DESCRIPTION, stdout);
       fputs (VERSION_OPTION_DESCRIPTION, stdout);
@@ -241,7 +241,10 @@ cut_bytes (FILE *stream)
       else if (c == EOF)
         {
           if (byte_idx > 0)
-            putchar (line_delim);
+          {
+            if (putchar (line_delim) < 0)
+              write_error ();
+          }
           break;
         }
       else
@@ -273,7 +276,7 @@ cut_bytes (FILE *stream)
 static void
 cut_fields (FILE *stream)
 {
-  int c;
+  int c;	/* Each character from the file.  */
   uintmax_t field_idx = 1;
   bool found_any_selected_field = false;
   bool buffer_first_field;
@@ -334,11 +337,15 @@ cut_fields (FILE *stream)
                     write_error ();
                   /* Make sure the output line is newline terminated.  */
                   if (field_1_buffer[n_bytes - 1] != line_delim)
-                    putchar (line_delim);
+                    {
+                      if (putchar (line_delim) < 0)
+                        write_error ();
+                    }
                   c = line_delim;
                 }
               continue;
             }
+
           if (print_kth (1))
             {
               /* Print the field, but not the trailing delimiter.  */
@@ -357,7 +364,9 @@ cut_fields (FILE *stream)
                     }
                 }
               else
-                found_any_selected_field = true;
+                {
+                  found_any_selected_field = true;
+                }
             }
           next_item (&field_idx);
         }
@@ -385,9 +394,7 @@ cut_fields (FILE *stream)
       else
         {
           while ((c = getc (stream)) != delim && c != line_delim && c != EOF)
-            {
-              prev_c = c;
-            }
+            prev_c = c;
         }
 
       /* With -d$'\n' don't treat the last '\n' as a delimiter.  */
@@ -407,13 +414,18 @@ cut_fields (FILE *stream)
           if (found_any_selected_field
               || !(suppress_non_delimited && field_idx == 1))
             {
+              /* Make sure the output line is newline terminated.  */
               if (c == line_delim || prev_c != line_delim
                   || delim == line_delim)
-                if (putchar (line_delim) < 0)
-                  write_error ();
+                {
+                  if (putchar (line_delim) < 0)
+                    write_error ();
+                }
             }
           if (c == EOF)
             break;
+
+          /* Start processing the next input line.  */
           field_idx = 1;
           current_rp = frp;
           found_any_selected_field = false;
@@ -537,9 +549,7 @@ main (int argc, char **argv)
           break;
 
         case_GETOPT_HELP_CHAR;
-
         case_GETOPT_VERSION_CHAR (PROGRAM_NAME, AUTHORS);
-
         default:
           usage (EXIT_FAILURE);
         }