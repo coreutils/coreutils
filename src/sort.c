a2434d3e58e8ead6c4c92fd989da32fe648e1545
Paul Eggert <eggert@cs.ucla.edu>
2023-09-28 18:02:25 -0700

    sort: improve --help
    Problem reported by Jorge Stolfi (bug#66253).
    * src/sort.c (usage): Suggest looking at the manual for -n details.

--- src/sort.c
+++ src/sort.c
@@ -444,7 +444,8 @@ Ordering options:\n\
   -h, --human-numeric-sort    compare human readable numbers (e.g., 2K 1G)\n\
 "), stdout);
       fputs (_("\
-  -n, --numeric-sort          compare according to string numerical value\n\
+  -n, --numeric-sort          compare according to string numerical value;\n\
+                                see manual for which strings are supported\n\
   -R, --random-sort           shuffle, but group identical keys.  See shuf(1)\n\
       --random-source=FILE    get random bytes from FILE\n\
   -r, --reverse               reverse the result of comparisons\n\
