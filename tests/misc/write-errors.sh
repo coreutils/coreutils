61d4935802e706bd32387bf052b89286c754e2ae
Pádraig Brady <P@draigBrady.com>
2025-01-15 15:13:00 +0000

    tests: avoid slow ulimit -v behavior
    * init.cfg (ulimit_supported_): skip_ if the ulimit -v
    takes too long, which was seen with bash 5.2 on Solaris 11,
    where fork() returned EAGAIN under memory constraints,
    and bash retried for about 16 seconds.
    (get_min_ulimit_v_): Break early if skipped.
    * tests/misc/write-errors.sh: Be more conservative and
    skip on failure to determine min ulimit.

--- tests/misc/write-errors.sh
+++ tests/misc/write-errors.sh
@@ -62,7 +62,7 @@ while read writer; do
   cmd=$(printf '%s\n' "$writer" | cut -d ' ' -f1) || framework_failure_
   base_mem=$(get_min_ulimit_v_ $cmd --version) \
     && ulimit="ulimit -v $(($base_mem+8000))" \
-    || ulimit='true'
+    || skip_ 'unable to determine ulimit -v'
 
   # Check /dev/full handling
   rm -f full.err || framework_failure_
