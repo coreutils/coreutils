0bd149403d4378d3a049f3cc34780ac8d71551f7
Pádraig Brady <P@draigBrady.com>
2025-01-15 22:47:50 +0000

    tests: misc/write-errors.sh: increase memory limit
    * tests/head/head-c.sh: Use a larger VM limit to avoid
    mem allocation failures, which were seen on CheriBSD.

--- tests/misc/write-errors.sh
+++ tests/misc/write-errors.sh
@@ -61,7 +61,7 @@ while read writer; do
   # Enforce mem usage limits if possible
   cmd=$(printf '%s\n' "$writer" | cut -d ' ' -f1) || framework_failure_
   base_mem=$(get_min_ulimit_v_ $cmd --version) \
-    && ulimit="ulimit -v $(($base_mem+8000))" \
+    && ulimit="ulimit -v $(($base_mem+12000))" \
     || skip_ 'unable to determine ulimit -v'
 
   # Check /dev/full handling
