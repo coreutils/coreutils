8c027d3bfb630f57c8890e85847fdac6f54711dd
Pádraig Brady <P@draigBrady.com>
2025-09-22 18:23:31 +0100

    maint: post-release administrivia
    * NEWS: Add header line for next release.
    * .prev-version: Record previous version.
    * cfg.mk (old_NEWS_hash): Auto-update.

--- cfg.mk
+++ cfg.mk
@@ -48,7 +48,7 @@ export VERBOSE = yes
 # 4914152 9e
 export XZ_OPT = -8e
 
-old_NEWS_hash = 0e74b03eaf11d9d281aaa0e7cc544bb4
+old_NEWS_hash = 6651d3c6c61d6b5d0f95b969e5c0c2af
 
 # Add an exemption for sc_makefile_at_at_check.
 _makefile_at_at_check_exceptions = \
