1dabab70273f64cda228d244f9e262a3f77ef087
Pádraig Brady <P@draigBrady.com>
2025-10-23 16:44:14 +0100

    tests: date: test --resolution and %-N
    * tests/date/resolution.sh: New test for --resolution and %-N.
    * tests/local.mk: Reference the new test.

---
+++ tests/date/resolution.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Test date resolution interface
+
+# Copyright (C) 2025 Free Software Foundation, Inc.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+. "${srcdir=.}/tests/init.sh"; path_prepend_ ./src
+print_ver_ date
+
+# Ensure --resolution is supported
+res=$(date --resolution) || fail=1
+
+# Ensure %-N format is supported
+subsec1=$(printf "%s" "$res" | cut -d. -f2- | wc -c) || framework_failure_
+subsec2=$(date +%-N | wc -c) || framework_failure_
+test "$subsec1" = "$subsec2" || fail=1
+
+
+Exit $fail
--- tests/local.mk
+++ tests/local.mk
@@ -319,6 +319,7 @@ all_tests =					\
   tests/date/date-iran.sh			\
   tests/date/date-locale-hour.sh		\
   tests/date/reference.sh			\
+  tests/date/resolution.sh			\
   tests/date/date-sec.sh			\
   tests/date/date-thailand.sh			\
   tests/date/date-tz.sh				\
