18405cb51a372a34785e6d5051b89e667000451c
Collin Funk <collin.funk1@gmail.com>
2025-12-23 20:03:49 -0800

    tests: date: improve locale tests
    * tests/date/date-locale-hour.sh: Test that the default format of 10
    random supported locales is the same as 'locale date_fmt'.

--- tests/date/date-locale-hour.sh
+++ tests/date/date-locale-hour.sh
@@ -56,5 +56,15 @@ for loc in "$LOCALE_FR_UTF8" 'en_US.UTF-8'; do
   esac
 done
 
+# Make sure 'date' can use the format string given by 'locale date_fmt'
+# and that it uses it by default when no format string is given.
+for loc in $(locale -a | shuf -n 10); do
+  fmt=$(LC_ALL=$loc locale date_fmt)
+  if test -n "$fmt"; then
+    LC_ALL=$loc date -d '2025-10-11T13:00' +"$fmt" > $loc.exp || fail=1
+    LC_ALL=$loc date -d '2025-10-11T13:00' > $loc.out || fail=1
+    compare $loc.exp $loc.out || fail=1
+  fi
+done
 
 Exit $fail
