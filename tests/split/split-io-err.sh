0ffd9a18120fcd22d8d584a72c3dc9de756d7b95
oech3 <79379754+oech3@users.noreply.github.com>
2025-12-27 22:57:30 +0900

    tests: split: verify operation in the presence of I/O errors
    * tests/split/split-io-err.sh: A new test to verify that with I/O
    errors, we leave existing files but don't continue.
    * tests/local.mk: Reference the new test.

---
+++ tests/split/split-io-err.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# Ensure we handle i/o errors correctly in split via /dev/full
+
+# Copyright (C) 2025 Free Software Foundation, Inc.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+. "${srcdir=.}/tests/init.sh"; path_prepend_ ./src
+print_ver_ split
+
+cp -sf /dev/full xaa || skip_ '/dev/full is required'
+
+# Get the wording of the OS-dependent ENOSPC message
+returns_ 1 seq 1 >/dev/full 2>msgt || framework_failure_
+sed 's/seq: write error: //' msgt > msg || framework_failure_
+
+# Create the expected error message
+{ printf "%s" "split: xaa: " ; cat msg ; } > exp || framework_failure_
+
+# the 'split' command should fail with exit code 1
+seq 2 | returns_ 1 split -b 1 2> err || fail=1
+# split does not cleanup broken file (while csplit does)
+test -e xaa || fail=1
+# split should not continue
+test -e xab && fail=1
+
+# Ensure we got the expected error message
+compare exp err || fail=1
+
+Exit $fail
