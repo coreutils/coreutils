d7622d8629434b8d5d5064e9abca5b6f3dca9eda
Collin Funk <collin.funk1@gmail.com>
2026-06-27 16:14:34 -0700

    tests: csplit: also test a read failure without -k
    * tests/csplit/csplit.sh: Adjust the test case added by the previous
    commit to also run without the -k option.

--- tests/csplit/csplit.sh
+++ tests/csplit/csplit.sh
@@ -111,14 +111,16 @@ EOF
 compare experr err || fail=1
 
 # Test that the message to standard output isn't elided after read fails.
-rm -f xx?? || framework_failure_
 echo 0 >exp-out || framework_failure_
 cat <<\EOF >exp-err || framework_failure_
 csplit: read error: Is a directory
 EOF
-returns_ 1 csplit -k . '/^a/' >out 2>err || fail=1
-compare /dev/null xx00 || fail=1
-compare exp-out out || fail=1
-compare exp-err err || fail=1
+for opt in '' '-k'; do
+  rm -f xx?? || framework_failure_
+  returns_ 1 csplit $opt . '/^a/' >out 2>err || fail=1
+  compare /dev/null xx00 || fail=1
+  compare exp-out out || fail=1
+  compare exp-err err || fail=1
+done
 
 Exit $fail
