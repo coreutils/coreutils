224673eba8b59402eb53590a47b8cd4aec5fa146
Pádraig Brady <P@draigBrady.com>
2026-08-18 17:57:42 +0100

    build: fix logname systemd linkage
    Note there was worry about redundant linking with -lsystemd
    when getlogin() is not replaced, however that is mitigated
    in general with the --as-needed linker option (the lib-ignore
    gnulib module). Tested with:
    
      ./configure --with-systemd gl_cv_func_getlogin_works=no
    
    * NEWS: Mention the Build-related fix.
    Reported at https://bugs.debian.org/1129960

--- src/local.mk
+++ src/local.mk
@@ -344,6 +344,7 @@ src_pinky_LDADD += $(READUTMP_LIB)
 src_uptime_LDADD += $(READUTMP_LIB)
 src_users_LDADD += $(READUTMP_LIB)
 src_who_LDADD += $(READUTMP_LIB)
+src_logname_LDADD += $(READUTMP_LIB)
 
 # for strsignal
 src_kill_LDADD += $(LIBTHREAD)
