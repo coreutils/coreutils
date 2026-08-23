32f109ca8e5c2d35d407fde6716dcdd66cbe5223
Collin Funk <collin.funk1@gmail.com>
2026-08-22 19:55:33 -0700

    tests: pathchk: test the output to standard error for empty file names
    * tests/misc/pathchk.sh: Move the existing empty file name tests to a
    loop to check some additional options. Check the message printed to
    standard error.

--- tests/misc/pathchk.sh
+++ tests/misc/pathchk.sh
@@ -27,12 +27,19 @@ touch file || framework_failure_
 # but exited successfully.
 returns_ 1 pathchk file/x > /dev/null 2>&1 || fail=1
 
-# This should exit nonzero.  Through 5.3.0 it exited with status zero.
-returns_ 1 pathchk -p '' > /dev/null 2>&1 || fail=1
-
 # This tests the new -P option.
-returns_ 1 pathchk -P '' > /dev/null 2>&1 || fail=1
 returns_ 1 pathchk -P -- - > /dev/null 2>&1 || fail=1
 returns_ 1 pathchk -p -P x/- > /dev/null 2>&1 || fail=1
+
+# Test empty file names with various options.  Through 5.3.0,
+# 'pathchk -p' exited with status zero.
+cat <<\EOF >exp || framework_failure_
+pathchk: empty file name
+EOF
+for opt in -p -P '-p -P' --portability; do
+  returns_ 1 pathchk $opt '' >out 2>err || fail=1
+  compare /dev/null out || fail=1
+  compare exp err || fail=1
+done
 
 Exit $fail
