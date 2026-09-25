ae8c120c45322c2851fab790911bffaf6c453884
Pádraig Brady <P@draigBrady.com>
2026-09-25 15:20:31 +0100

    tests: date: avoid a false failure on NetBSD
    The following errors were seen on NetBSD 11.99:
    
     Setting LC_CTYPE to 'de_DE' failed setlocale(3)
     Setting LC_MESSAGES to 'de_DE' failed setlocale(3)
     Setting LC_MONETARY to 'de_DE' failed setlocale(3)
     Setting LC_NUMERIC to 'de_DE' failed setlocale(3)
     Setting LC_TIME to 'de_DE' failed setlocale(3)
    
    * tests/date/date.pl: Ignore setlocale() errors seen on NetBSD 11.99.
    Reported at https://bugs.gnu.org/81923

--- tests/date/date.pl
+++ tests/date/date.pl
@@ -173,9 +173,9 @@ my @Tests =
 
      # test for RFC-822 conformance
      ['rfc822-1', "-R -d '$d1'", {OUT=>"Sun, 19 Jan 1997 08:17:48 +0000"},
-      # Solaris 5.9's /bin/sh emits this diagnostic to stderr
-      # if you don't have support for the named locale.
-      {ERR_SUBST => q!s/^couldn't set locale correctly\n//!},
+      # Solaris 5.9's /bin/sh can emit "couldn't set locale"
+      # NetBSD 12 sh can emit "setlocale failed"
+      {ERR_SUBST => q!s/.*(?:couldn't set locale|failed setlocale).*\n//!},
       {ENV => 'LC_ALL=de_DE TZ=UTC0'}],
 
      # Relative seconds, with time.  fixed in 2.0j
